cd2dc8717fd11889a78fc18cdbb90ef2cb8c1e1f
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-10-23 22:12:20 +0200

    Function component.

--- libs/uesioapps/material/bundle/components/breadcrumbs/breadcrumbs.tsx
+++ libs/uesioapps/material/bundle/components/breadcrumbs/breadcrumbs.tsx
@@ -1,57 +1,43 @@
-import React, { ReactElement, SyntheticEvent } from "react"
+import React, { FunctionComponent, SyntheticEvent } from "react"
 
 import { hooks, material } from "@uesio/ui"
 import { BreadcrumbsProps } from "./breadcrumbsdefinition"
 
-const useStyles = material.makeStyles((theme) => {
-	return {
-		root: {
-			margin: theme.spacing(1),
-		},
-	}
-})
+const useStyles = material.makeStyles((theme) => ({
+	root: {
+		margin: theme.spacing(1),
+	},
+}))
 
-function Breadcrumbs(props: BreadcrumbsProps): ReactElement {
+const Breadcrumbs: FunctionComponent<BreadcrumbsProps> = (props) => {
 	const classes = useStyles()
 	const uesio = hooks.useUesio(props)
-	const breadcrumbsProps = {
-		className: classes.root,
-	}
-
 	const crumbs = props.definition?.crumbs
 
 	return (
-		<material.Breadcrumbs {...breadcrumbsProps}>
-			{crumbs
-				? crumbs.map((crumbDef, index) => {
-						const text = props.context.merge(crumbDef.text)
-						const href = props.context.merge(crumbDef.href)
-						const absHref =
-							href && !href.startsWith("/") ? `/${href}` : href
-						if (crumbDef.href) {
-							const linkProps = {
-								href: absHref,
-								onClick: (event: SyntheticEvent): void => {
-									event.preventDefault()
-									crumbDef.signals &&
-										uesio.signal.getHandler(
-											crumbDef.signals
-										)()
-								},
-							}
-							return (
-								<material.Link {...linkProps}>
-									{text}
-								</material.Link>
-							)
-						}
-						return (
-							<material.Typography key={index}>
-								{text}
-							</material.Typography>
-						)
-				  })
-				: []}
+		<material.Breadcrumbs className={classes.root}>
+			{crumbs?.map?.((crumbDef, index) => {
+				const text = props.context.merge(crumbDef.text)
+				const href = props.context.merge(crumbDef.href)
+				const absHref =
+					href && !href.startsWith("/") ? `/${href}` : href
+				if (crumbDef.href) {
+					const linkProps = {
+						href: absHref,
+						onClick: (event: SyntheticEvent): void => {
+							event.preventDefault()
+							crumbDef.signals &&
+								uesio.signal.getHandler(crumbDef.signals)()
+						},
+					}
+					return <material.Link {...linkProps}>{text}</material.Link>
+				}
+				return (
+					<material.Typography key={index}>
+						{text}
+					</material.Typography>
+				)
+			}) || []}
 		</material.Breadcrumbs>
 	)
 }
