3e43d3cf7ae673812de16407e44da3e3b1eab6b6
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-10-27 13:34:37 +0100

    new collection field called DateTime with usage of it (#28)
    * PR init, new datetime field and use it in the datepicker.
    
    * Dom event.

---
+++ libs/uesioapps/sample/bundle/components/datePicker/datePicker.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from "react"
+import { DatePickerProps } from "./datePickerdefinition"
+import { material } from "@uesio/ui"
+
+const useStyles = material.makeStyles(() => ({
+	container: {
+		padding: "10px",
+	},
+}))
+
+const DatePicker: FunctionComponent<DatePickerProps> = ({
+	definition: { color, label, variant },
+}) => (
+	<div className={useStyles().container}>
+		<material.TextField
+			color={color}
+			label={label}
+			type="date"
+			InputLabelProps={{
+				shrink: true,
+			}}
+			variant={variant}
+		/>
+	</div>
+)
+
+export default DatePicker
