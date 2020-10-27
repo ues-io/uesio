07461334766976cd1348513d75b4a487a79babfe
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-10-27 10:05:47 +0100

    PR init, new datetime field and use it in the datepicker.

---
+++ libs/uesioapps/sample/bundle/components/datePicker/datePickerbuilder.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react"
+import DatePicker from "./datePicker"
+import { hooks } from "@uesio/ui"
+import { DatePickerProps, DatePickerDefinition } from "./datePickerdefinition"
+
+const DatePickerBuilder: FunctionComponent<DatePickerProps> = (props) => {
+	const uesio = hooks.useUesio(props)
+	const definition = uesio.view.useDefinition(
+		props.path
+	) as DatePickerDefinition
+	return <DatePicker {...props} definition={definition}></DatePicker>
+}
+
+export default DatePickerBuilder
