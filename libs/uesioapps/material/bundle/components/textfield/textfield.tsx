5d86df91409dd9bc740706b6bf67847c74d68f8b
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-10-28 21:31:04 +0100

    Test action on linter.

--- libs/uesioapps/material/bundle/components/textfield/textfield.tsx
+++ libs/uesioapps/material/bundle/components/textfield/textfield.tsx
@@ -2,6 +2,8 @@ import React, { ChangeEvent, FunctionComponent } from "react"
 
 import { material, definition, context } from "@uesio/ui"
 
+console.log("test")
+
 type Props = {
 	label: string
 	setValue: (value: string) => void
