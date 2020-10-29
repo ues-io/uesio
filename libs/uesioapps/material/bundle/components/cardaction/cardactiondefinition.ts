df4379b3ceea349a2a1a5437abe0d329c9ba32fb
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-10-29 10:39:44 +0100

    Intnded linting error.

--- libs/uesioapps/material/bundle/components/cardaction/cardactiondefinition.ts
+++ libs/uesioapps/material/bundle/components/cardaction/cardactiondefinition.ts
@@ -37,6 +37,11 @@ const CardActionPropertyDefinition: builder.BuildPropertiesDefinition = {
 	],
 	traits: ["uesio.standalone"],
 }
+
+// intended lint provoked error
+const {} = CardActionPropertyDefinition.sections[0]
+
+
 export { CardActionProps, CardActionDefinition }
 
 export default CardActionPropertyDefinition
