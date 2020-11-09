4792937cb5f76e5a05645b4ed075cbb5726415e6
humandad <55447225+humandad@users.noreply.github.com>
2020-11-09 14:18:40 -0500

    Fix lint warning and prettier

--- apps/cli/src/metadata/bundle.ts
+++ apps/cli/src/metadata/bundle.ts
@@ -2,14 +2,14 @@ import { LoadRequestField } from "../wire/loadrequest"
 import { load } from "../wire/load"
 import { wiretable, TableColumn } from "../print/wiretable"
 import inquirer = require("inquirer")
-import {getApp, getWorkspace, getSessionId} from "../config/config"
+import { getApp, getWorkspace } from "../config/config"
 import { get } from "../request/request"
-import {authorize} from "../auth/login";
-import {throwIfBadFormat} from "../validation/version"
+import { authorize } from "../auth/login"
+import { throwIfBadFormat } from "../validation/version"
 
 //TODO: probably don't want to leave it this way. :)
-const UESIO_STUDIO_WORKSPACE = 'workspace'
-const UESIO_BUNDLE_CREATE_ENDPOINT = 'metadata/storebundle'
+const UESIO_STUDIO_WORKSPACE = "workspace"
+const UESIO_BUNDLE_CREATE_ENDPOINT = "metadata/storebundle"
 class Bundle {
 	static getCollectionName(): string {
 		return "uesio.bundles"
@@ -41,9 +41,7 @@ class Bundle {
 		wiretable(response.wires[0], response.collections, this.getColumns())
 	}
 	static async create(): Promise<void> {
-
 		const responses = await inquirer.prompt([
-
 			{
 				name: "version",
 				message: "Bundle Version (Ex: v0.0.1)",
@@ -55,16 +53,16 @@ class Bundle {
 				type: "input",
 			},
 		])
-		throwIfBadFormat(responses.version);
-		const {cookie} = await authorize()
+		throwIfBadFormat(responses.version)
+		const { cookie } = await authorize()
 
-		const [workspace , app ]= await Promise.all([getWorkspace(), getApp()])
+		const [workspace, app] = await Promise.all([getWorkspace(), getApp()])
 		const url = `${UESIO_STUDIO_WORKSPACE}/${app}/${workspace}/${UESIO_BUNDLE_CREATE_ENDPOINT}?version=${responses.version}&description=${responses.description}`
 		try {
 			const response = await get(url, cookie)
 			console.log(await response.text())
-		} catch(e) {
-			console.log(e);
+		} catch (e) {
+			console.log(e)
 		}
 	}
 }
