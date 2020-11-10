d2d5022eb64b0e8e4968a287a17ca7f4021f6856
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-10 16:12:10 +0100

    Merge conflict.

--- libs/constants/src/metadata.ts
+++ libs/constants/src/metadata.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const METADATA = {
-=======
-const metadata = {
->>>>>>> c3ae1c9... Export.
 	COLLECTION: 'collections',
 	FIELD: 'fields',
 	VIEW: 'views',
@@ -10,12 +6,6 @@ const metadata = {
 	SECRET: 'secrets',
 };
 
-<<<<<<< HEAD
 type MetadataType = keyof typeof METADATA;
 
 export { METADATA, MetadataType };
-=======
-type MetadataType = keyof typeof metadata;
-
-export { metadata, MetadataType };
->>>>>>> c3ae1c9... Export.
