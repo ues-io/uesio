84628b0ee48ed03b981e99aa2e3a3657c8081703
humandad <55447225+humandad@users.noreply.github.com>
2020-11-10 17:51:20 -0500

    Fix builds

--- apps/cli/src/commands/pack.ts
+++ apps/cli/src/commands/pack.ts
@@ -7,19 +7,8 @@ import {
 } from "../pack/pack"
 import * as webpack from "webpack"
 
-// This is really dumb, but when typescript compiles, module
-// paths are not resolved in emitted code. There's a long discussion
-// and lots of angry people in this github issue.
-// https://github.com/microsoft/TypeScript/issues/10866
-// In the end, the typescript people say it's not their job to resolve
-// modules and that the aren't going to change it.
-
-// This is a somewhat hack that resolves the module-alias for us so we can
-// use the share code in the nx way. We could possibly get rid of this
-// if we used webpack in addition to typescript to bundle up the cli commands.
-import * as moduleAlias from "module-alias"
-moduleAlias.addAlias("@uesio/constants", "../../../../libs/constants/src")
-
+// Side effect is that the @uesio/contants module works
+import "../modulealias"
 import { metadata } from "@uesio/constants"
 
 export default class Pack extends Command {
