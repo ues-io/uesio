177b0ace59564204e1345fcbe567ceeebac3f32f
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-10 08:07:05 +0100

    Share constants and types.

--- libs/constants/metadata.ts
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-    COLLECTION: "collections",
-    FIELD: "fields",
-    VIEW: "views",
-    DATASOURCE: "datasources",
-    SECRET: "secrets",
-}
-
-export type Metadata = keyof typeof metadata
