1006f11963257d744c594405a5c3d9644ca1594c
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-11 14:48:42 +0100

    Import.

--- apps/platform/webpack.config.js
+++ apps/platform/webpack.config.js
@@ -44,17 +44,4 @@ module.exports = {
 		sourceMapFilename: '[name].js.map',
 		path: path.join(__dirname, './platform'),
 	},
-
-	// When importing a module whose path matches one of the following, just
-	// assume a corresponding global variable exists and use that instead.
-	// This is important because it allows us to avoid bundling all of our
-	// dependencies, which allows browsers to cache those libraries between builds.
-	externals: {
-		react: 'React',
-		'react-dom': 'ReactDOM',
-		redux: 'Redux',
-		'react-redux': 'ReactRedux',
-		yaml: 'yaml',
-		'@material-ui/core': 'MaterialUI',
-	},
 };
