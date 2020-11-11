755f51990f671be3162160b3eae02bab8373d1fb
Jackson Stone <jacksonastone@gmail.com>
2020-11-11 08:48:41 -0500

    Silence them :

--- apps/cli/src/print/wiretable.ts
+++ apps/cli/src/print/wiretable.ts
@@ -14,14 +14,17 @@ const wiretable = (
 	columns: TableColumn[]
 ): void => {
 	const collectionMetadata = metadata[response.collection]
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const tableColumns = {} as any
 
 	columns.forEach((column) => {
 		const fieldMetadata = collectionMetadata.fields[column.id]
 		const tableColumn = {
 			header: fieldMetadata.label,
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} as any
 		if (column.type === "COLOR") {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			tableColumn.get = (row: any): string => {
 				const value = row[column.id]
 				return chalk.hex(value).bold(value)
