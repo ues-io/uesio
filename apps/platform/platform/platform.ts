a498103f60ea5ae1a341bd1d9dda9cc02f92d900
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-11-12 17:27:14 +0100

    JS compilation in platform (#72)
    * Move to typescript.
    
    * Scaffolding.
    
    * Compilation works.
    
    * Update gitignore for the static transpiled files.
    
    * Update gitignore.
    
    * Import.
    
    * Cariage return.
    
    * Config.
    
    * Debugging.
    
    * Debugging.
    
    * Debugging.
    
    * Global variable.
    
    * Compilation done.
    
    * Comment.
    
    * Make code more readable.
    
    * Cleanup.
    
    * Final touch.
    
    * Align with current implementation.

---
+++ apps/platform/platform/platform.ts
@@ -0,0 +1,189 @@
+import { metadata } from '@uesio/constants';
+
+const getPrefix = (workspace?: { app: string; name: string }) => {
+	return workspace ? `/workspace/${workspace.app}/${workspace.name}` : '/site';
+};
+
+const postJSON = (url: string, body?: object) => {
+	return fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		...(body && {
+			body: JSON.stringify(body),
+		}),
+	});
+};
+
+(window as any).monacoPublicPath = '/static/lazymonaco/';
+
+// aws-sdk requires global to exist
+(window as any).global = window;
+
+// UesioSSRLoadervar is accessible outside the generated webpack bundle
+(window as any).UesioSSRLoadervar = (mergeData: object) => {
+	// @ts-ignore
+	new uesio.loader.Loader({
+		getView: async (context: any, namespace: string, name: string) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const response = await fetch(`${prefix}/views/${namespace}/${name}`);
+			if (response.status !== 200) {
+				throw new Error('View Not Found');
+			}
+			return response.text();
+		},
+
+		saveViews: async (context: any, saveRequest: object) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const response = await postJSON(`${prefix}/views/save`, saveRequest);
+			if (response.status != 200) {
+				throw new Error('Save Failed');
+			}
+			return response.json();
+		},
+
+		getRoute: async (context: any, namespace: string, route: string) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const response = await fetch(`${prefix}/routes/${namespace}/${route}`);
+			if (response.status !== 200) {
+				throw new Error('Route Not Found');
+			}
+			return response.json();
+		},
+
+		loadData: async (context: any, requestBody: object) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const response = await postJSON(`${prefix}/wires/load`, requestBody);
+			if (response.status != 200) {
+				const error = await response.text();
+				throw new Error(error);
+			}
+			return response.json();
+		},
+
+		saveData: async (context: any, requestBody: object) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const response = await postJSON(`${prefix}/wires/save`, requestBody);
+			if (response.status != 200) {
+				const error = await response.text();
+				throw new Error(error);
+			}
+			return response.json();
+		},
+
+		callBot: async (
+			context: any,
+			namespace: string,
+			name: string,
+			params: object
+		) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const response = await postJSON(
+				`${prefix}/bots/call/${namespace}/${name}`,
+				params
+			);
+			if (response.status != 200) {
+				const error = await response.text();
+				throw new Error(error);
+			}
+			return response.json();
+		},
+
+		getFileURL: (context: any, namespace: string, name: string) => {
+			const prefix = getPrefix(context.getWorkspace());
+			return `${prefix}/files/${namespace}/${name}`;
+		},
+
+		getUserFileURL: (context: any, userfileid: string) => {
+			console.log('userfileid', userfileid);
+			console.log('type', typeof userfileid);
+			const prefix = getPrefix(context.getWorkspace());
+			return `${prefix}/userfiles/download?userfileid=${encodeURIComponent(
+				userfileid
+			)}`;
+		},
+
+		deleteUserFile: async (context: any, userfileid: string) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const url = `${prefix}/userfiles/delete?userfileid=${encodeURIComponent(
+				userfileid
+			)}`;
+			const response = await fetch(url, { method: 'POST' });
+			return response.text();
+		},
+
+		uploadFile: async (
+			context: any,
+			fileData: any,
+			name: string,
+			fileCollection: string,
+			collectionID: any,
+			recordID: any,
+			fieldID: any
+		) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const url = `${prefix}/userfiles/upload`;
+			const params = new URLSearchParams();
+			params.append('name', name);
+			params.append('filecollection', fileCollection);
+			params.append('collectionid', collectionID);
+			params.append('recordid', recordID);
+			params.append('fieldid', fieldID);
+
+			const response = await fetch(url + '?' + params.toString(), {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/octet-stream',
+				},
+				body: fileData,
+			});
+
+			return response.text();
+		},
+
+		getComponentPackURL: (
+			context: any,
+			namespace: string,
+			name: string,
+			buildMode: boolean
+		) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const buildModeSuffix = buildMode ? '/builder' : '';
+			return `${prefix}/componentpacks/${namespace}/${name}${buildModeSuffix}`;
+		},
+
+		getBuilderCoreURL: () => '/static/buildtime/uesiobuildtime.js',
+
+		getMetadataList: async (
+			context: any,
+			metadataType: metadata.MetadataType,
+			namespace: string,
+			grouping: string
+		) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const mdType = metadata.METADATA[metadataType];
+			const groupingUrl = grouping ? `/${grouping}` : '';
+			const response = await fetch(
+				`${prefix}/metadata/types/${mdType}/namespace/${namespace}/list${groupingUrl}`
+			);
+			return response.json();
+		},
+
+		getAvailableNamespaces: async (context: any) => {
+			const prefix = getPrefix(context.getWorkspace());
+			const response = await fetch(`${prefix}/metadata/namespaces`);
+			return response.json();
+		},
+
+		login: async (requestBody: object) => {
+			const response = await postJSON('/site/auth/login', requestBody);
+			return response.json();
+		},
+
+		logout: async () => {
+			const response = await postJSON('/site/auth/logout');
+			return response.json();
+		},
+	}).load(document.querySelector('#root'), mergeData);
+};
---
+++ apps/platform/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: './platform/platform.ts',
+	output: {
+		path: path.resolve(__dirname, './platform'),
+		filename: 'platform.js',
+	},
+	resolve: {
+		// Add ".ts" and ".tsx" as resolvable extensions.
+		extensions: ['.ts', '.tsx', '.js'],
+		alias: {
+			'@uesio/constants': path.resolve(
+				__dirname,
+				'../../libs/constants/src/index.ts'
+			),
+		},
+	},
+	module: {
+		rules: [
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+			},
+		],
+	},
+};
