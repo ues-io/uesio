3066e1cc1a882eb1a5dcc67038451fbcec7094b7
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-13 13:49:38 +0100

    view fot bot in collection.

--- libs/uesioapps/uesio/bundle/bots/newcollection.js
+++ libs/uesioapps/uesio/bundle/bots/newcollection.js
@@ -1,18 +1,20 @@
-function newcollection(bot) {
+const newcollection = (bot) => {
 	var collection = bot.params.get("name")
 	var datasource = bot.params.get("datasource")
 	var app = bot.params.get("app")
 	var workspace = bot.params.get("workspace")
 	var workspaceId = app + "_" + workspace
 
-	bot.save("uesio.collections", [{
-		"uesio.name": collection,
-		"uesio.datasource": datasource,
-		"uesio.collectionname": collection,
-		"uesio.workspaceid": workspaceId,
-		"uesio.idfield": app + ".id",
-		"uesio.namefield": app + ".name"
-	}])
+	bot.save("uesio.collections", [
+		{
+			"uesio.name": collection,
+			"uesio.datasource": datasource,
+			"uesio.collectionname": collection,
+			"uesio.workspaceid": workspaceId,
+			"uesio.idfield": app + ".id",
+			"uesio.namefield": app + ".name",
+		},
+	])
 	bot.save("uesio.fields", [
 		{
 			"uesio.collection": app + "." + collection,
