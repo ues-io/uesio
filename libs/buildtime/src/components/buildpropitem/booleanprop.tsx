5e3675c1028cf41d5fc84bed982a42d69105a83e
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-23 21:38:19 +0100

    leverage-typescript-in-jsx-5

--- libs/buildtime/src/components/buildpropitem/booleanprop.tsx
+++ libs/buildtime/src/components/buildpropitem/booleanprop.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react"
+import React, { FunctionComponent } from "react"
 import {
 	FormControlLabel,
 	Checkbox,
@@ -13,9 +13,8 @@ import {
 	inputProps,
 } from "./proprendererdefinition"
 
-function BooleanProp(props: PropRendererProps): ReactElement {
+const BooleanProp: FunctionComponent<PropRendererProps> = (props) => {
 	const descriptor = props.descriptor as builder.BooleanProp
-	const { definition } = props
 	const selected = props.getValue() as boolean
 
 	switch (descriptor.displaytype) {
@@ -24,11 +23,9 @@ function BooleanProp(props: PropRendererProps): ReactElement {
 				<FormControlLabel
 					control={
 						<Switch
-							{...{
-								checked: selected,
-								onChange: (event): void => {
-									props.setValue(event.target.checked)
-								},
+							checked={selected}
+							onChange={(event): void => {
+								props.setValue(event.target.checked)
 							}}
 						/>
 					}
@@ -60,11 +57,9 @@ function BooleanProp(props: PropRendererProps): ReactElement {
 					label={descriptor.label}
 					fullWidth={true}
 					onChange={(event): void => {
-						if (event.target.value == "true") {
-							props.setValue(true)
-						} else {
-							props.setValue(false)
-						}
+						event.target.value == "true"
+							? props.setValue(true)
+							: props.setValue(false)
 					}}
 				>
 					{optionslist?.map(
@@ -83,11 +78,9 @@ function BooleanProp(props: PropRendererProps): ReactElement {
 				<FormControlLabel
 					control={
 						<Checkbox
-							{...{
-								checked: selected,
-								onChange: (event): void => {
-									props.setValue(event.target.checked)
-								},
+							checked={selected}
+							onChange={(event): void => {
+								props.setValue(event.target.checked)
 							}}
 						/>
 					}
