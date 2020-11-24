294c76f14e58ece4b9c2b3eb60cfc4f2dd29497c
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-11-24 19:01:39 +0100

    Function destructuring. (#129)

--- libs/buildtime/src/components/buildpropitem/numberprop.tsx
+++ libs/buildtime/src/components/buildpropitem/numberprop.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react"
+import React, { FunctionComponent } from "react"
 import { TextField } from "@material-ui/core"
 import {
 	PropRendererProps,
@@ -7,28 +7,25 @@ import {
 	inputLabelProps,
 } from "./proprendererdefinition"
 
-function NumberProp(props: PropRendererProps): ReactElement {
-	const { descriptor } = props
-
-	// Fall back to text component
-	return (
-		<TextField
-			{...{
-				value: props.getValue(),
-				label: descriptor.label,
-				size: "small",
-				type: "number",
-				fullWidth: true,
-				style: inputStyles,
-				InputProps: inputProps,
-				InputLabelProps: inputLabelProps,
-				variant: "outlined",
-				onChange: (event): void => {
-					props.setValue(parseInt(event.target.value))
-				},
-			}}
-		/>
-	)
-}
+const NumberProp: FunctionComponent<PropRendererProps> = ({
+	descriptor,
+	setValue,
+	getValue,
+}) => (
+	<TextField
+		value={getValue()}
+		label={descriptor.label}
+		size="small"
+		type="number"
+		fullWidth={true}
+		style={inputStyles}
+		InputProps={inputProps}
+		InputLabelProps={inputLabelProps}
+		variant="outlined"
+		onChange={(event): void => {
+			setValue(parseInt(event.target.value))
+		}}
+	/>
+)
 
 export default NumberProp
