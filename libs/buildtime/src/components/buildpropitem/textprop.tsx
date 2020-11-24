ac34788b155d8515c46f3cbbc1bf559da4a09d38
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-11-24 19:03:54 +0100

    leverage typescript in jsx - No. 12 (#131)
    * leverage-typescript-in-jsx.
    
    * leverage-typescript-in-jsx.
    
    * Let comment as it was.

--- libs/buildtime/src/components/buildpropitem/textprop.tsx
+++ libs/buildtime/src/components/buildpropitem/textprop.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react"
+import React, { FunctionComponent } from "react"
 import { TextField } from "@material-ui/core"
 import {
 	PropRendererProps,
@@ -7,27 +7,25 @@ import {
 	inputLabelProps,
 } from "./proprendererdefinition"
 
-function TextProp(props: PropRendererProps): ReactElement {
-	const { descriptor } = props
-
+const TextProp: FunctionComponent<PropRendererProps> = ({
+	getValue,
+	descriptor,
+	setValue,
+}) => (
 	// Fall back to text component
-	return (
-		<TextField
-			{...{
-				value: props.getValue(),
-				label: descriptor.label,
-				size: "small",
-				fullWidth: true,
-				style: inputStyles,
-				InputProps: inputProps,
-				InputLabelProps: inputLabelProps,
-				variant: "outlined",
-				onChange: (event): void => {
-					props.setValue(event.target.value)
-				},
-			}}
-		/>
-	)
-}
+	<TextField
+		value={getValue()}
+		label={descriptor.label}
+		size="small"
+		fullWidth={true}
+		style={inputStyles}
+		InputProps={inputProps}
+		InputLabelProps={inputLabelProps}
+		variant="outlined"
+		onChange={(event): void => {
+			setValue(event.target.value)
+		}}
+	/>
+)
 
 export default TextProp
