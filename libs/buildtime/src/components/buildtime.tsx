d23a1158a120d4e1723761366ec2ed86942227ef
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-25 00:06:24 +0100

    Function parameter destructuring.

--- libs/buildtime/src/components/buildtime.tsx
+++ libs/buildtime/src/components/buildtime.tsx
@@ -6,29 +6,28 @@ import LeftToolbar from "./toolbar/lefttoolbar"
 import RightToolbar from "./toolbar/righttoolbar"
 import Canvas from "./canvas"
 
-const Buildtime: FunctionComponent<definition.BaseProps> = (props) => {
+const Buildtime: FunctionComponent<definition.BaseProps> = ({
+	context,
+	path,
+}) => {
 	const uesio = hooks.useUesio()
-	const route = props.context.getRoute()
+	const route = context.getRoute()
 
 	if (!route) {
 		return null
 	}
 
-	const view = uesio.view.useView(
-		route.viewnamespace,
-		route.viewname,
-		props.path
-	)
+	const view = uesio.view.useView(route.viewnamespace, route.viewname, path)
 
-	const addFrame = props.context.addFrame({
+	const addView = context.addFrame({
 		view: view.getId(),
 	})
 
 	return (
 		<div style={{ display: "flex" }}>
-			<LeftToolbar path="" context={addFrame} />
-			<Canvas path="" context={addFrame} />
-			<RightToolbar path="" context={addFrame} />
+			<LeftToolbar path="" context={addView} />
+			<Canvas path="" context={addView} />
+			<RightToolbar path="" context={addView} />
 		</div>
 	)
 }
