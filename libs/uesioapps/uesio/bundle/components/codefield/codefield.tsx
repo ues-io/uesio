86c5f8cad687efbb63ec1b23b142df191af60543
humandad <55447225+humandad@users.noreply.github.com>
2020-11-24 12:28:09 -0500

    Add error handling to codefield

--- libs/uesioapps/uesio/bundle/components/codefield/codefield.tsx
+++ libs/uesioapps/uesio/bundle/components/codefield/codefield.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement, useState } from "react"
-import { definition, material } from "@uesio/ui"
+import { definition, material, collection, wire, component } from "@uesio/ui"
 import LazyMonaco from "@uesio/lazymonaco"
 
 type CodeFieldDefinition = {
 	fieldId: string
 	height: string
-	language?: "yaml" | "json" | "javascript"
+	language?: CodeFieldLanguage
 	id?: string
 }
+
+type CodeFieldLanguage = "yaml" | "json" | "javascript"
 
 const tryParseJSON = (jsonString: string) => {
 	try {
@@ -21,10 +23,8 @@ const tryParseJSON = (jsonString: string) => {
 			return o
 		}
 	} catch (e) {
-		console.log("Not an Object")
+		return false
 	}
-
-	return false
 }
 
 interface Props extends definition.BaseProps {
@@ -44,10 +44,61 @@ const useStyles = material.makeStyles((theme) =>
 	})
 )
 
+function getChangeHandler(
+	fieldType: collection.FieldType,
+	language: CodeFieldLanguage,
+	record: wire.WireRecord,
+	fieldId: string,
+	setMessage: React.Dispatch<React.SetStateAction<string>>,
+	setStringValue: React.Dispatch<React.SetStateAction<string>>
+) {
+	switch (fieldType) {
+		case "MAP":
+			return (newValue: string) => {
+				if (language === "json") {
+					setStringValue(newValue)
+					const jsonValue = tryParseJSON(newValue)
+					if (jsonValue) {
+						record.update(fieldId, jsonValue)
+						setMessage("")
+						return
+					}
+					setMessage("Invalid JSON")
+					return
+				}
+				setMessage("Language not supported for maps: " + language)
+			}
+		default:
+			return (newValue: string) => {
+				record.update(fieldId, newValue)
+			}
+	}
+}
+
+function getValue(
+	fieldType: collection.FieldType,
+	language: CodeFieldLanguage,
+	value: wire.FieldValue,
+	setMessage: React.Dispatch<React.SetStateAction<string>>
+): string {
+	switch (fieldType) {
+		case "MAP":
+			if (language === "json") {
+				return JSON.stringify(value, null, "\t")
+			}
+			setMessage("Language not supported for maps: " + language)
+			return ""
+		default:
+			return (value || "") as string
+	}
+}
+
 function CodeField(props: Props): ReactElement | null {
 	const classes = useStyles(props)
 	const record = props.context.getRecord()
 	const wire = props.context.getWire()
+	const [message, setMessage] = useState("")
+	const [stringValue, setStringValue] = useState("")
 	if (!wire || !record) {
 		return null
 	}
@@ -58,42 +109,43 @@ function CodeField(props: Props): ReactElement | null {
 	const fieldMetadata = collection.getField(fieldId)
 	const fieldType = fieldMetadata.getType()
 	const value = record.getFieldValue(fieldId)
-	// TODO: Add special handline for fields of type "FILE"
-	const stringValue = fieldType == "MAP" ? JSON.stringify(value, null, "\t") : value as string
 
 	if (!fieldMetadata.isValid()) {
 		return null
 	}
 	const language = props.definition.language || "yaml"
 
+	const AlertComponent = component.registry.get("material", "alert")
+
 	return (
 		<div className={classes.root}>
 			<material.InputLabel shrink={true}>
 				{fieldMetadata.getLabel()}
 			</material.InputLabel>
+			{message && (
+				<AlertComponent
+					{...props}
+					onClose={() => setMessage("")}
+					severity="error"
+				>
+					{message}
+				</AlertComponent>
+			)}
 			<div className={classes.input}>
 				<LazyMonaco
-					{...{
-						value: stringValue,
-						language: language,
-						onChange: (newValue /*, event*/): void => {
-							// TODO: Add special handline for fields of type "FILE"
-							if (fieldType == "MAP") {
-								if (language === "json") {
-									const jsonValue = tryParseJSON(newValue)
-									if (jsonValue) {
-										record.update(
-											fieldId,
-											jsonValue
-										)
-									}
-								}
-							}
-							else {
-								record.update(fieldId, newValue)
-							}
-						},
-					}}
+					value={
+						stringValue ||
+						getValue(fieldType, language, value, setMessage)
+					}
+					language={language}
+					onChange={getChangeHandler(
+						fieldType,
+						language,
+						record,
+						fieldId,
+						setMessage,
+						setStringValue
+					)}
 				></LazyMonaco>
 			</div>
 		</div>
