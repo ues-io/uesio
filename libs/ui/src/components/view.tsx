d8876adc1440456ec3c7347b839111ce4de2218c
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-25 14:28:57 +0100

    Remove hardcoded config object.

--- libs/ui/src/components/view.tsx
+++ libs/ui/src/components/view.tsx
@@ -72,38 +72,12 @@ const View: FC<Props> = (props: Props) => {
 		neededScripts,
 		scriptResult.scripts
 	)
-
-	///
-	const PaletteOptions = {
-		primary: {
-			// light: will be calculated from palette.primary.main,
-			main: "#ff4400",
-			// dark: will be calculated from palette.primary.main,
-			// contrastText: will be calculated to contrast with palette.primary.main
-		},
-		secondary: {
-			light: "#0066ff",
-			main: "#0044ff",
-			// dark: will be calculated from palette.secondary.main,
-			contrastText: "#ffcc00",
-		},
-		// Used by `getContrastText()` to maximize the contrast between
-		// the background and the text.
-		contrastThreshold: 3,
-		// Used by the functions below to shift a color's luminance by approximately
-		// two indexes within its tonal palette.
-		// E.g., shift from Red 500 to Red 300 or Red 700.
-		tonalOffset: 0.2,
-	}
-
 	const route = uesio.route.useRoute()
 	const context = uesio.getContext()
 
 	const [themenamespace, themename] = route.theme
 		? parseKey(route.theme)
 		: ["", ""]
-
-	///
 
 	useEffect(() => {
 		const hasNewParams = viewparams !== view.source.params
