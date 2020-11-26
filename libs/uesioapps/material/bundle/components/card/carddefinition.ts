806d768eb85352b2265d489eb650eb0fe570d61e
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-11-26 07:44:01 +0100

    Remove dead code.

--- libs/uesioapps/material/bundle/components/card/carddefinition.ts
+++ libs/uesioapps/material/bundle/components/card/carddefinition.ts
@@ -1,4 +1,4 @@
-import { definition, builder, signal, styles } from "@uesio/ui"
+import { definition, signal, styles } from "@uesio/ui"
 
 import { CardActionDefinition } from "../cardaction/cardactiondefinition"
 
@@ -16,18 +16,4 @@ interface CardProps extends definition.BaseProps {
 	definition: CardDefinition
 }
 
-const CardPropertyDefinition: builder.BuildPropertiesDefinition = {
-	title: "Card",
-	defaultDefinition: () => ({}),
-	sections: [
-		{
-			title: "Display",
-			type: "PROPLIST",
-			properties: [],
-		},
-	],
-	traits: ["uesio.standalone"],
-}
 export { CardProps, CardDefinition }
-
-export default CardPropertyDefinition
