93b7ac8bf49bf730b9237c7f49e5f5a7646fa08e
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-12-02 00:39:04 +0100

    leverage-typescript-in-jsx-35 (#188)

--- libs/uesioapps/material/bundle/components/deck/deckbuilder.tsx
+++ libs/uesioapps/material/bundle/components/deck/deckbuilder.tsx
@@ -1,26 +1,15 @@
-import React, { ReactElement } from "react"
+import React, { FunctionComponent } from "react"
 import { hooks, component } from "@uesio/ui"
 import { DeckProps, DeckDefinition } from "./deckdefinition"
 import Deck from "./deck"
 
-function DeckBuilder(props: DeckProps): ReactElement {
+const DeckBuilder: FunctionComponent<DeckProps> = (props) => {
 	const uesio = hooks.useUesio(props)
 	const definition = uesio.view.useDefinition(props.path) as DeckDefinition
 	const buildView = uesio.builder.useView()
 	const isExpanded = buildView === "expandedview"
+	const { path, context } = props
 
-	const path = props.path
-	const context = props.context
-
-	const slotProps = {
-		definition,
-		listName: "components",
-		path,
-		accepts: ["uesio.standalone"],
-		context: context.addFrame({
-			noMerge: true,
-		}),
-	}
 	return (
 		<>
 			{isExpanded && (
@@ -32,7 +21,15 @@ function DeckBuilder(props: DeckProps): ReactElement {
 						backgroundColor: "#f5f5f5",
 					}}
 				>
-					<component.Slot {...slotProps} />
+					<component.Slot
+						definition={definition}
+						listName="components"
+						path={path}
+						accepts={["uesio.standalone"]}
+						context={context.addFrame({
+							noMerge: true,
+						})}
+					/>
 				</div>
 			)}
 			<Deck {...props} definition={definition} />
