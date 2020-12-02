071082ef3d039631e1c5a637f2d056b792003b07
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-02 15:58:35 +0100

    Remove file.

--- libs/ui/src/bands/theme/selector.ts
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux"
-import RuntimeState from "../../store/types/runtimestate"
-import { ThemeState } from "./types"
-
-const useTheme = (): ThemeState =>
-	useSelector((state: RuntimeState) => state.theme)
-
-export { useTheme }
