cd267dee207fb4e72cc939d91eb63e32add2c97d
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-07 23:08:30 +0100

    simplify-code-6

--- libs/ui/src/bands/collection/class.ts
+++ libs/ui/src/bands/collection/class.ts
@@ -10,27 +10,16 @@ class Collection {
 	source: PlainCollection
 	valid: boolean
 
-	getId = (): string => {
-		return this.source.name
-	}
-
-	getNamespace = (): string => {
-		return this.source.namespace
-	}
-
-	isValid = (): boolean => {
-		return this.valid
-	}
-
+	getId = (): string => this.source.name
+	getNamespace = (): string => this.source.namespace
+	isValid = (): boolean => this.valid
 	getField = (fieldName: string | null): Field => {
 		const fieldMetadata =
 			this.source && fieldName ? this.source.fields[fieldName] : null
 		return new Field(fieldMetadata)
 	}
-
-	getIdField = (): Field => {
-		return this.getField(this.source ? this.source.idField : null)
-	}
+	getIdField = (): Field =>
+		this.getField(this.source ? this.source.idField : null)
 }
 
 export default Collection
