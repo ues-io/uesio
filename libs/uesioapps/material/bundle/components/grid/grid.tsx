48d1321611a7d4fab48ee2f9a2096302c5b83d8a
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-13 17:23:26 +0100

    FunctionComponent.

--- libs/uesioapps/material/bundle/components/grid/grid.tsx
+++ libs/uesioapps/material/bundle/components/grid/grid.tsx
@@ -2,8 +2,8 @@ import React, { FunctionComponent } from "react"
 import { component, material, styles } from "@uesio/ui"
 import { GridProps } from "./griddefinition"
 
-const useStyles = material.makeStyles(() => {
-	return material.createStyles({
+const useStyles = material.makeStyles(() =>
+	material.createStyles({
 		root: (props: GridProps) => ({
 			height: props.definition.height,
 			...styles.getBackgroundStyles(
@@ -12,7 +12,7 @@ const useStyles = material.makeStyles(() => {
 			),
 		}),
 	})
-})
+)
 
 const Grid: FunctionComponent<GridProps> = (props) => {
 	const classes = useStyles(props)
