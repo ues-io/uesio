4575d493a53df12ec467dd072b8c09bfc118ce3a
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-14 10:05:56 +0100

    Action creator typing.

--- libs/ui/src/bands/bot/operations.ts
+++ libs/ui/src/bands/bot/operations.ts
@@ -2,9 +2,14 @@ import { parseKey } from "../../component/path"
 import { Context } from "../../context/context"
 import { BotParams, Platform } from "../../platform/platform"
 import { Dispatcher } from "../../store/store"
+import { AppThunk } from "../../store/types/runtimestate"
 import RuntimeState from "../../store/types/runtimestate"
 
-const call = (context: Context, bot: string, params?: BotParams) => async (
+const call = (
+	context: Context,
+	bot: string,
+	params?: BotParams
+): AppThunk<Promise<Context>> => async (
 	dispatch: Dispatcher<never>,
 	getState: () => RuntimeState,
 	platform: Platform
