1d9a18437421505bee5eeb805ae761dcbb53a641
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-15 14:48:15 +0100

    CU-74tg55_Error-handling-for-loadsave-of-wires.

---
+++ libs/ui/src/components/feedback.tsx
@@ -0,0 +1,38 @@
+import React, { FunctionComponent, useState, useEffect } from "react"
+import { BaseProps } from "../definition/definition"
+import { ComponentInternal } from "../component/component"
+
+type Props = {
+	severity?: "error" | "success" | "info" | "warning"
+} & BaseProps
+
+const Feedback: FunctionComponent<Props> = (props) => {
+	const { children, context } = props
+	const [doUnmount, setDoUnmont] = useState(false)
+
+	// componentDidMount
+	useEffect(() => {
+		setTimeout(() => setDoUnmont(true), 2000)
+	}, [])
+
+	if (doUnmount) {
+		return null
+	}
+
+	return (
+		<div style={{ position: "absolute", bottom: 10, right: 10 }}>
+			<ComponentInternal
+				{...props}
+				componentType="material.alert"
+				path=""
+				context={context}
+			>
+				<div>Component Not Found: {children}</div>
+			</ComponentInternal>
+		</div>
+	)
+}
+
+Feedback.displayName = "Feedback"
+
+export default Feedback
