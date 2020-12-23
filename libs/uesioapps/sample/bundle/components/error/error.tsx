1714972e80cb66b59757f62c2bcbf4d64db5d17d
Sakai Rubattel Toma <Sakai-san@users.noreply.github.com>
2020-12-23 17:48:27 +0100

    CU-bpxy3n_Clean-up-css-in-js_No_16_Thomas-Rubattel (#309)

--- libs/uesioapps/sample/bundle/components/error/error.tsx
+++ libs/uesioapps/sample/bundle/components/error/error.tsx
@@ -6,20 +6,17 @@ const useStyles = material.makeStyles(() =>
 	material.createStyles({
 		root: (props: ErrorProps) => ({
 			padding: "100px",
-			backgroundColor: props.definition?.color || "#2D72D9",
+			backgroundColor: props.definition.color || "#2D72D9",
 			height: "100%",
 			textAlign: "center",
 		}),
-		h1_prop: (props: ErrorProps) => ({
+		h1: (props: ErrorProps) => ({
 			fontSize: "220px",
-			color: props.definition?.font_color || "white",
+			color: props.definition.fontColor || "white",
 		}),
-		p_prop: (props: ErrorProps) => ({
-			color: props.definition?.font_color || "white",
+		p: (props: ErrorProps) => ({
+			color: props.definition.fontColor || "white",
 		}),
-		a_prop: {
-			color: "#fffee3",
-		},
 	})
 )
 
@@ -27,13 +24,12 @@ const Error: FunctionComponent<ErrorProps> = (props) => {
 	const classes = useStyles(props)
 	return (
 		<div className={classes.root}>
-			<h1 className={classes.h1_prop}>{props.definition.message}</h1>
-			<p className={classes.p_prop}> {props.definition.sub_message}</p>
-			{/* <a className={classes.a_prop} href="{props.definition.url}"> {props.definition.url} </a> */}
+			<h1 className={classes.h1}>{props.definition.message}</h1>
+			<p className={classes.p}> {props.definition.subMessage}</p>
 			<material.Button
 				color="primary"
 				variant="contained"
-				href="{props.definition.url}"
+				href={props.definition.url}
 			>
 				{props.definition.url}
 			</material.Button>
