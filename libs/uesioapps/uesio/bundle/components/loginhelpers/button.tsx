d8a101d91e44edfe76277e5805c1f4269e1075ec
thomas <thomas@thomass-MacBook-Pro.local>
2020-12-30 14:15:55 +0100

    Integer.

--- libs/uesioapps/uesio/bundle/components/loginhelpers/button.tsx
+++ libs/uesioapps/uesio/bundle/components/loginhelpers/button.tsx
@@ -9,7 +9,7 @@ const getButtonStyles = (): styles.CSSProperties => ({
 	color: "rgba(0, 0, 0, 0.54)",
 	boxShadow:
 		"rgba(0, 0, 0, 0.24) 0px 2px 2px 0px, rgba(0, 0, 0, 0.24) 0px 0px 1px 0px",
-	padding: "0",
+	padding: 0,
 	borderRadius: "2px",
 	border: "1px solid transparent",
 	fontSize: "14px",
