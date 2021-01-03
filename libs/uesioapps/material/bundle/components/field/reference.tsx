f56040cf074c3bcb5b4494973fa5358b0b83429c
humandad <55447225+humandad@users.noreply.github.com>
2021-01-02 22:31:05 -0500

    fix build

--- libs/uesioapps/material/bundle/components/field/reference.tsx
+++ libs/uesioapps/material/bundle/components/field/reference.tsx
@@ -123,10 +123,10 @@ const Reference: FunctionComponent<RendererProps> = (props) => {
 						],
 					})
 					callback(
-						result.wires[0].data.map((record) => ({
+						result.wires[0].data?.map((record) => ({
 							value: record[nameField] + "",
 							id: record[idField] + "",
-						}))
+						})) || []
 					)
 				}}
 			/>
