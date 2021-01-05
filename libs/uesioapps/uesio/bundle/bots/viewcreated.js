e1ffec886242505cc4333b56a4c8d32d5f4b10ae
jacksonCloudmasters <67640065+jacksonCloudmasters@users.noreply.github.com>
2021-01-05 13:13:58 -0600

    add bot to pre-populate value (#347)
    Co-authored-by: Jackson Stone <jacksonastone@gmail.com>

---
+++ libs/uesioapps/uesio/bundle/bots/viewcreated.js
@@ -0,0 +1,8 @@
+function onchange(bot) {
+	bot.changes.get().forEach(function (change) {
+		var defintion = change.get("uesio.definition");
+		if ((change.isNew() && !defintion) || defintion === "") {
+			change.set("uesio.definition", "# Wires connect to data in collections\nwires:\n# Components determine the layout and composition of your view\ncomponents:");
+		}
+	});
+}
