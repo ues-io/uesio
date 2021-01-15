53d7f23e6f420a30d8e6f3c020b3f70b488ab803
humandad <55447225+humandad@users.noreply.github.com>
2021-01-15 15:27:49 -0500

    Simplify Diffing and No more remounting

--- libs/buildtime/src/components/toolbar/codetoolbar/codetoolbar.tsx
+++ libs/buildtime/src/components/toolbar/codetoolbar/codetoolbar.tsx
@@ -1,15 +1,13 @@
-import React, { FunctionComponent, useRef } from "react"
+import React, { FunctionComponent, useEffect, useRef } from "react"
 import ToolbarTitle from "../toolbartitle"
 import LazyMonaco from "@uesio/lazymonaco"
 import { hooks, util, definition, styles } from "@uesio/ui"
 import yaml from "yaml"
 import CloseIcon from "@material-ui/icons/Close"
 import { makeStyles, createStyles } from "@material-ui/core"
-import md5 from "md5"
-import toPath from "lodash.topath"
-import get from "lodash.get"
 
-const MARKER_FOR_DIFF_START = "##START##"
+import { monaco } from "react-monaco-editor"
+
 const ANIMATION_DURATION = 3000
 
 const useStyles = makeStyles((theme) =>
@@ -31,90 +29,73 @@ const useStyles = makeStyles((theme) =>
 	})
 )
 
-const addMarkerAtFirstKey = (
-	addedDefinition: definition.AddDefinitionPayload
-) => {
-	const keys = Object.keys(addedDefinition.definition || {})
-	const withMarker = {
-		...addedDefinition,
-		definition: {
-			[`${keys?.[0] || ""}${MARKER_FOR_DIFF_START}`]: {
-				...(keys.length > 0
-					? // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					  // @ts-ignore next-line
-					  addedDefinition.definition?.[keys[0]]
-					: {}),
-			},
-		},
-	}
-	return withMarker
-}
-
-const splitTextByLines = (text: string) => text.split(/\r?\n/)
-
-const lookForLine = (lines: string[], wordToLookFor: string) =>
-	lines.findIndex((line) => line.indexOf(wordToLookFor) !== -1)
-
-const addedDefinitionLinesAmount = (
-	addedDefinition: definition.AddDefinitionPayload
-) => {
-	const yamlDoc = util.yaml.parse(
-		JSON.stringify(addedDefinition?.definition || {})
-	)
-	return splitTextByLines(yamlDoc.toString()).length
-}
-
-// home-made diff algorithm since unix-like diff approach did not work for our use case
-const diff = (
-	previousYamlDocInJson: definition.DefinitionMap,
-	lastAddedDefinition: definition.AddDefinitionPayload
-): [number, number] => {
-	// algorithm
-	// 0. add a marker to last added definition (drag'n drop into the canvas)
-	// 1. insert the marked definition into the previous YAML definition in JSON formatted
-	// 2. create a new YAML document with new added definition from previous step
-	// 3. transform the YAML document into a string
-	// 4. get the line of the marker in the stringified YAML document
-	// 5. compute the height of the lastAddedDefinition
-	// 6. return lines range of the changes (start with index 1 and not 0 for the monaco editor)
-
-	// step 0.
-	const withStartMarker = addMarkerAtFirstKey(lastAddedDefinition)
-	const { path, definition, index } = withStartMarker
-	const pathArray = toPath(path)
-	const children = get(previousYamlDocInJson, pathArray) || []
-
-	// step 1. insert the new definition into the children, by mutating children
-	children.splice(index, 0, definition)
-
-	// step 2.
-	const newYamlDoc = util.yaml.parse(JSON.stringify(previousYamlDocInJson))
-
-	// step 3.
-	const newYamlDoxSplitted = splitTextByLines(newYamlDoc.toString())
-
-	// step 4.
-	const startOffset = lookForLine(newYamlDoxSplitted, MARKER_FOR_DIFF_START)
-
-	// step 5.
-	const endOffset =
-		startOffset + addedDefinitionLinesAmount(lastAddedDefinition)
-
-	// step 6.
-	return [startOffset + 1, endOffset - 1]
-}
-
 const CodeToolbar: FunctionComponent<definition.BaseProps> = (props) => {
 	const classes = useStyles(props)
 	const uesio = hooks.useUesio(props)
 	const yamlDoc = uesio.view.useYAML()
-	const currentYaml = yamlDoc?.toString()
+	const currentYaml = yamlDoc?.toString() || ""
 
 	const currentAST = useRef<yaml.Document | undefined>(yamlDoc)
 	const previousYaml = currentAST.current?.toString()
-	const previousYamlInJson = currentAST.current?.toJSON()
-	const hasYamlChanged = previousYaml !== currentYaml
-	const lastAddedDefinition = uesio.view.useLastAddedDefinition()
+	currentAST.current = util.yaml.parse(currentYaml)
+
+	const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | undefined>(
+		undefined
+	)
+	const monacoRef = useRef<typeof monaco | undefined>(undefined)
+	const decorationsRef = useRef<string[] | undefined>(undefined)
+
+	const e = editorRef.current
+	const m = monacoRef.current
+
+	useEffect(() => {
+		if (e && m && previousYaml && currentYaml) {
+			// Create an off-dom diff editor
+			const diffEditor = m.editor.createDiffEditor(
+				document.createElement("div")
+			)
+
+			diffEditor.setModel({
+				original: m.editor.createModel(previousYaml, "text/plain"),
+				modified: m.editor.createModel(currentYaml, "text/plain"),
+			})
+
+			diffEditor.onDidUpdateDiff(() => {
+				const changes = diffEditor.getLineChanges()
+				if (changes?.length) {
+					decorationsRef.current = e.deltaDecorations(
+						decorationsRef.current || [],
+						changes.map((change) => ({
+							range: new m.Range(
+								change.modifiedStartLineNumber,
+								change.charChanges?.length === 0
+									? change.charChanges[0].modifiedStartColumn
+									: 1,
+								change.modifiedEndLineNumber,
+								change.charChanges?.length === 0
+									? change.charChanges[0].modifiedEndColumn
+									: 1
+							),
+							options: {
+								isWholeLine: true,
+								className: classes.highlightLines,
+							},
+						}))
+					)
+
+					// scroll to the changes
+					e.revealLineInCenter(changes[0].modifiedStartLineNumber)
+
+					// we have to remove the decoration otherwise CSS style kicks in back while clicking on the editor
+					setTimeout(() => {
+						decorationsRef.current =
+							decorationsRef.current &&
+							e.deltaDecorations(decorationsRef.current, [])
+					}, ANIMATION_DURATION)
+				}
+			})
+		}
+	})
 
 	return (
 		<>
@@ -124,11 +105,7 @@ const CodeToolbar: FunctionComponent<definition.BaseProps> = (props) => {
 				iconOnClick={(): void => uesio.builder.setRightPanel("")}
 			/>
 			<LazyMonaco
-				// force the LazyMonaco component to unmount and create a new component if hasYamlChanged is true
-				{...(hasYamlChanged && currentYaml
-					? { key: md5(currentYaml) }
-					: {})}
-				value={yamlDoc && yamlDoc.toString()}
+				value={currentYaml}
 				onChange={(newValue, event): void => {
 					const newAST = util.yaml.parse(newValue)
 					if (newAST.errors.length > 0) {
@@ -235,6 +212,8 @@ const CodeToolbar: FunctionComponent<definition.BaseProps> = (props) => {
 					}
 				}
 				editorDidMount={(editor, monaco): void => {
+					editorRef.current = editor
+					monacoRef.current = monaco
 					// Set currentAST again because sometimes monaco reformats the text
 					// (like removing trailing spaces and such)
 					currentAST.current = util.yaml.parse(editor.getValue())
@@ -276,45 +255,6 @@ const CodeToolbar: FunctionComponent<definition.BaseProps> = (props) => {
 							}
 						}
 					})
-
-					// highlight changes in the editor
-					if (
-						hasYamlChanged &&
-						previousYamlInJson &&
-						lastAddedDefinition
-					) {
-						const rangeDiff = diff(
-							previousYamlInJson,
-							lastAddedDefinition
-						)
-
-						const decorations = editor.deltaDecorations(
-							[],
-							[
-								{
-									range: new monaco.Range(
-										rangeDiff[0],
-										1,
-										rangeDiff[1],
-										1
-									),
-									options: {
-										isWholeLine: true,
-										className: classes.highlightLines,
-									},
-								},
-							]
-						)
-
-						// scroll to the changes
-						editor.revealLineInCenter(rangeDiff[1])
-
-						// we have to remove the decoration otherwise CSS style kicks in back while clicking on the editor
-						setTimeout(
-							() => editor.deltaDecorations(decorations, []),
-							ANIMATION_DURATION
-						)
-					}
 				}}
 			/>
 		</>
