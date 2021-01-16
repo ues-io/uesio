1b6ca1eb43f89fedeceea2c7fc8b52424f90415e
humandad <55447225+humandad@users.noreply.github.com>
2021-01-16 01:20:21 -0500

    fix gitignore

--- libs/uesioapps/sample/bundle/componentpacks/sample.main/runtime.entry.ts
+++
@@ -1,9 +0,0 @@
-import { component } from "@uesio/ui";
-import hello from "../../components/hello/hello";
-import error from "../../components/error/error";
-import progressgauge from "../../components/progressgauge/progressgauge";
-import datePicker from "../../components/datePicker/datePicker";
-component.registry.register("sample", "hello", hello);
-component.registry.register("sample", "error", error);
-component.registry.register("sample", "progressgauge", progressgauge);
-component.registry.register("sample", "datePicker", datePicker);
