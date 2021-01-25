72bd2e56f1c3870c42ea58ccc5aaf939fe7b6ac0
Abel <abel@thecloudmasters.com>
2021-01-25 09:52:11 +0100

    Changes

--- apps/platform/pkg/adapt/sort.go
+++ apps/platform/pkg/adapt/sort.go
@@ -51,35 +51,41 @@ func LessFunc(data interface{}, order []LoadRequestOrder) (func(i int, j int) bo
 			vi, _ := dataCast[i].GetField(singleOrder.Field)
 			vj, _ := dataCast[j].GetField(singleOrder.Field)
 
-			if vi == nil || vj == nil {
+			if vi == nil && vj == nil {
 				continue
 			}
+			if singleOrder.Desc && vi == nil {
+				return true
+			}
+			if !singleOrder.Desc && vj == nil {
+				return true
+			}
 
-			switch vi.(type) {
+			switch vit := vi.(type) {
 			case string:
-				if vi.(string) == vj.(string) {
+				if vit == vj.(string) {
 					continue
 				}
 				if singleOrder.Desc {
-					return vi.(string) > vj.(string)
+					return vit > vj.(string)
 				}
-				return vi.(string) < vj.(string)
+				return vit < vj.(string)
 			case int:
-				if vi.(int) == vj.(int) {
+				if vit == vj.(int) {
 					continue
 				}
 				if singleOrder.Desc {
-					return vi.(int) > vj.(int)
+					return vit > vj.(int)
 				}
-				return vi.(int) < vj.(int)
+				return vit < vj.(int)
 			case float64:
-				if vi.(float64) == vj.(float64) {
+				if vit == vj.(float64) {
 					continue
 				}
 				if singleOrder.Desc {
-					return vi.(float64) > vj.(float64)
+					return vit > vj.(float64)
 				}
-				return vi.(float64) < vj.(float64)
+				return vit < vj.(float64)
 			}
 
 		}
