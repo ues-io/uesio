db98e478e3a192af38e0ff2f1d24a2807453c93d
thomas <thomas@thomass-MacBook-Pro.local>
2021-02-08 12:13:51 +0100

    Renaming.

--- libs/uesioapps/material/bundle/components/utils/signals.ts
+++ libs/uesioapps/material/bundle/components/utils/signals.ts
@@ -1,12 +1,12 @@
 import { context, signal } from "@uesio/ui"
 
-const sigHandler = <S>(payload: (state: S) => S) => ({
+const sigHandler = <S>(makePayload: (state: S) => S) => ({
 	TOGGLE_MODE: {
 		dispatcher: (signal: signal.SignalDefinition, ctx: context.Context) => (
 			setState: (state: S) => void,
 			getState: () => S
 		) => {
-			setState(payload(getState()))
+			setState(makePayload(getState()))
 			return ctx
 		},
 		label: "Toggle Mode",
