b1309e788504b6045bd5d34bbc42012813dc12e4
humandad <55447225+humandad@users.noreply.github.com>
2021-02-10 15:07:10 -0500

    spacing

--- apps/platform/pkg/configstore/environment/environment.go
+++ apps/platform/pkg/configstore/environment/environment.go
@@ -15,7 +15,7 @@ var configValues = map[string]string{
 	"uesio:cognitoClientId":   os.Getenv("COGNITO_CLIENT_ID"),
 	"uesio:cognitoPoolId":     os.Getenv("COGNITO_POOL_ID"),
 	"uesio:genericFileBucket": os.Getenv("UESIO_PLATFORM_BUCKET"),
-	"uesio:googleProjectId": os.Getenv("GOOGLE_CLOUD_PROJECT"),
+	"uesio:googleProjectId":   os.Getenv("GOOGLE_CLOUD_PROJECT"),
 }
 
 // Get function
