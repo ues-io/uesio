90cac53744859d13f9a2874d67253c0b6bf6c56a
jacksonCloudmasters <67640065+jacksonCloudmasters@users.noreply.github.com>
2021-02-19 12:04:46 -0500

    Style variants - adding the metadata type and adding them to dependency loads (#428)
    * Add example in sample
    
    * Poking around and getting some boiler plate in place
    
    * Add in the loading of all component variants.
    
    Co-authored-by: Jackson Stone <jacksonastone@gmail.com>

--- libs/uesioapps/sample/bundle/components/hello/hello.tsx
+++ libs/uesioapps/sample/bundle/components/hello/hello.tsx
@@ -1,18 +1,16 @@
 import React, { FunctionComponent } from "react"
 import { HelloProps } from "./hellodefinition"
-import { material } from "@uesio/ui"
+import { styles } from "@uesio/ui"
 
-const useStyles = material.makeStyles(() =>
-	material.createStyles({
-		root: {
-			padding: "100px",
-			backgroundColor: "pink",
-		},
-	})
-)
+const useStyles = styles.getUseStyles(["root"], {
+	root: {
+		padding: "100px",
+		backgroundColor: "pink",
+	},
+})
 
 const Hello: FunctionComponent<HelloProps> = (props) => {
-	const classes = useStyles()
+	const classes = useStyles(props)
 	return (
 		<div className={classes.root}>
 			{props.definition.greeting || "Hello! World. Again."}
