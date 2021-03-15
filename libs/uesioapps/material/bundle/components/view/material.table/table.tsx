4781793959e35571e298a0c55ff6a012122df3bd
humandad <55447225+humandad@users.noreply.github.com>
2021-03-15 19:05:04 -0400

    fix build

--- libs/uesioapps/material/bundle/components/view/material.table/table.tsx
+++ libs/uesioapps/material/bundle/components/view/material.table/table.tsx
@@ -26,7 +26,7 @@ const Table: FunctionComponent<TableProps> = (props) => {
 		initialState
 	) as TableState
 
-	if (!wire || !componentState) return null
+	if (!wire || !componentState || !path) return null
 
 	const collection = wire.getCollection()
 	return (
