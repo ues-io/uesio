86390aa4aa38124d36fa88fb11ce8ec171a544a8
jacksonCloudmasters <67640065+jacksonCloudmasters@users.noreply.github.com>
2021-04-01 22:05:29 -0400

    Rough draft of team member and access of collections (#447)
    * Rough draft of team member and access of collections
    
    * Fix issue with retrieve not grabbing component pack files
    
    Co-authored-by: Jackson Stone <jacksonastone@gmail.com>

--- apps/platform/pkg/datasource/createbundle.go
+++ apps/platform/pkg/datasource/createbundle.go
@@ -7,12 +7,20 @@ import (
 	"github.com/thecloudmasters/uesio/pkg/sess"
 )
 
-func CreateBundle(namespace, sourceversion, destversion, description string, source bundlestore.BundleStore, session *sess.Session) error {
+// Only used in isolation for seeds where we allow overriding the same version with new assets
+func StoreBundleAssets(namespace, sourceversion, destversion string, source bundlestore.BundleStore, session *sess.Session) error {
 	streams, err := retrieve.RetrieveBundle(namespace, sourceversion, source, session)
 	if err != nil {
 		return err
 	}
+	dest, err := bundlestore.GetBundleStore(namespace, session.RemoveWorkspaceContext())
+	if err != nil {
+		return err
+	}
+	return dest.StoreItems(namespace, destversion, streams)
+}
 
+func CreateBundle(namespace, sourceversion, destversion, description string, source bundlestore.BundleStore, session *sess.Session) error {
 	bundle, err := meta.NewBundle(namespace, destversion, description)
 	if err != nil {
 		return err
@@ -22,9 +30,6 @@ func CreateBundle(namespace, sourceversion, destversion, description string, sou
 	if err != nil {
 		return err
 	}
-	dest, err := bundlestore.GetBundleStore(namespace, session.RemoveWorkspaceContext())
-	if err != nil {
-		return err
-	}
-	return dest.StoreItems(namespace, destversion, streams)
+
+	return StoreBundleAssets(namespace, sourceversion, destversion, source, session)
 }
