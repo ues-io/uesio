32282670b3b895f1efb232c75044e07cca317ab2
Abel <abel@thecloudmasters.com>
2021-06-28 14:03:22 +0200

    uesio and io bundles installed by default

---
+++ libs/uesioapps/studio/bundle/bots/listener/studio.newworkspace/bot.js
@@ -0,0 +1,31 @@
+function newworkspace(bot) {
+	var workspace = bot.params.get("name")
+	var app = bot.params.get("app")
+	var workspaceId = app + "_" + workspace
+
+	var IoVersion = "io_" + bot.studio.getBundleLastVersion("io")
+	var UesioVersion = "uesio_" + bot.studio.getBundleLastVersion("uesio")
+
+	bot.save("studio.workspaces", [
+		{
+			"studio.name": workspace,
+			"studio.app": app,
+			"studio.appid": app,
+		},
+	])
+
+	bot.save("studio.bundledependencies", [
+		{
+			"studio.bundle": {
+				"uesio.id": UesioVersion,
+			},
+			"studio.workspaceid": workspaceId,
+		},
+		{
+			"studio.bundle": {
+				"uesio.id": IoVersion,
+			},
+			"studio.workspaceid": workspaceId,
+		},
+	])
+}
