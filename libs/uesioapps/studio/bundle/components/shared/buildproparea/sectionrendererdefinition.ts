a165a4650825678ccd0a421a6adb33bc2a62db8a
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2021-07-01 00:11:11 +0200

    Inline styles builder #y1duf7 (#533)
    * variants in components
    
    * removed hardcoded grouping
    
    * Added function to parse the variant namespace string
    
    * Added logic to variant select fields. Also removed some repetitive code
    
    * Added support for variant grouping
    
    Refactored grouping logic into a function. With a new exception for the variant logic, the grouping code got a little chunky. A function with return statements seemed more appropriate to handle the extra logic
    
    * Keyvaluelist component
    
    Added keyvalue list component. Still placed in the wrong part of the project for now.
    
    Classes key is  added to the component definition.
    
    * definition state and styles
    
    added some styling and getting def from app state
    
    * Moved KeyValueList component
    
    * empty label was rendered empty, causing a pixel bump
    
    * Styling and optimization
    
    * Fix build
    
    * fix prettier
    
    * reuse listfield component
    
    * remove wrapper class
    
    * fix autoadd
    
    * small fix
    
    Co-authored-by: Abel <abel@thecloudmasters.com>
    Co-authored-by: humandad <55447225+humandad@users.noreply.github.com>

--- libs/uesioapps/studio/bundle/components/shared/buildproparea/sectionrendererdefinition.ts
+++ libs/uesioapps/studio/bundle/components/shared/buildproparea/sectionrendererdefinition.ts
@@ -2,6 +2,7 @@ import { definition, builder } from "@uesio/ui"
 
 interface SectionRendererProps extends definition.BaseProps {
 	section: builder.PropertySection
+	propsDef: builder.BuildPropertiesDefinition
 }
 
 export { SectionRendererProps }
