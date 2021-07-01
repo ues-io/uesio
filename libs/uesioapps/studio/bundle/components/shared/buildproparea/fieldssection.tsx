61726b69def4b5672c092b1428f7c4349f5eb27f
Wessel <wesselvdp@gmail.com>
2021-07-01 14:25:54 +0200

    Bugfix and a bit of refactoring
    Bug: when adding the first field in a new wire definition, the definition was printed out wrongly, making the page crash.
    
    Reason: The 'fields' property is not in the definition when creating a new wire.
    
    Fix: add a check to ensure the fields property is in the definition before adding fields.
    
    Also, I've separated the logic a bit from the render function making the easier to read... but yeah, that's my humble opinion :)

--- libs/uesioapps/studio/bundle/components/shared/buildproparea/fieldssection.tsx
+++ libs/uesioapps/studio/bundle/components/shared/buildproparea/fieldssection.tsx
@@ -20,17 +20,38 @@ const FieldsSection: FunctionComponent<SectionRendererProps> = (props) => {
 
 	const uesio = hooks.useUesio(props)
 	const theme = uesio.getTheme()
-	const fields = uesio.builder.useMetadataList(
-		context,
-		"FIELD",
-		namespace,
-		collectionKey
-	)
 
 	const fieldsDef = def?.fields as definition.DefinitionMap
 
 	const onDragStart = getOnDragStartToolbar(uesio)
 	const onDragEnd = getOnDragStopToolbar(uesio)
+
+	const handleClick = (fieldId: string, selected: boolean) => {
+		// Ensure the fields property is in the def
+		if (!fieldsDef) {
+			uesio.view.setDefinition(`${path}["fields"]`, {})
+		}
+		selected
+			? uesio.view.removeDefinition(`${path}["fields"]["${fieldId}"]`)
+			: uesio.view.addDefinitionPair(`${path}["fields"]`, null, fieldId)
+	}
+	const fieldArray: string[] = Object.keys(
+		uesio.builder.useMetadataList(
+			context,
+			"FIELD",
+			namespace,
+			collectionKey
+		) || {}
+	)
+
+	const fields = fieldArray.map((field) => {
+		const selected = fieldsDef && field in fieldsDef
+		return {
+			selected,
+			handleClick: () => handleClick(field, selected),
+			fieldId: field,
+		}
+	})
 
 	return (
 		<ExpandPanel
@@ -39,45 +60,27 @@ const FieldsSection: FunctionComponent<SectionRendererProps> = (props) => {
 			context={context}
 		>
 			<div onDragStart={onDragStart} onDragEnd={onDragEnd}>
-				{fields &&
-					collectionKey &&
-					Object.keys(fields).map((fieldId, index) => {
-						const fieldDef = fieldsDef?.[fieldId]
-						const selected = fieldDef !== undefined
-						const onClick = (): void =>
+				{fields.map(({ handleClick, fieldId, selected }, index) => (
+					<PropNodeTag
+						draggable={component.dragdrop.createFieldBankKey(
+							collectionKey,
+							fieldId
+						)}
+						title={fieldId}
+						icon={
+							selected ? "check_box" : "check_box_outline_blank"
+						}
+						iconColor={
 							selected
-								? uesio.view.removeDefinition(
-										`${path}["fields"]["${fieldId}"]`
-								  )
-								: uesio.view.addDefinitionPair(
-										`${path}["fields"]`,
-										null,
-										fieldId
-								  )
-						return (
-							<PropNodeTag
-								draggable={component.dragdrop.createFieldBankKey(
-									collectionKey,
-									fieldId
-								)}
-								title={fieldId}
-								icon={
-									selected
-										? "check_box"
-										: "check_box_outline_blank"
-								}
-								iconColor={
-									selected
-										? theme.definition.palette.primary
-										: undefined
-								}
-								key={index}
-								onClick={onClick}
-								selected={selected}
-								context={context}
-							/>
-						)
-					})}
+								? theme.definition.palette.primary
+								: undefined
+						}
+						key={index}
+						onClick={handleClick}
+						selected={selected}
+						context={context}
+					/>
+				))}
 			</div>
 		</ExpandPanel>
 	)
