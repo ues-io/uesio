599a39a9ee31167a2f69032811b2ce553a26dda8
Abel <abel@thecloudmasters.com>
2021-07-06 13:51:35 +0200

    Show a toast message if there is an error while saving

--- libs/ui/src/bands/notification/index.ts
+++ libs/ui/src/bands/notification/index.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 import notificationAdapter from "./adapter"
+import saveOp from "../wire/operations/save"
+import shortid from "shortid"
 
 const notificationSlice = createSlice({
 	name: "notification",
@@ -7,6 +9,20 @@ const notificationSlice = createSlice({
 	reducers: {
 		add: notificationAdapter.addOne,
 		remove: notificationAdapter.removeOne,
+	},
+	extraReducers: (builder) => {
+		builder.addCase(saveOp.rejected, (state, action) => {
+			const notId = shortid.generate()
+			state.ids.push(notId)
+			state.entities = Object.assign({}, state.entities, {
+				[notId]: Object.assign({}, state.entities[notId], {
+					id: notId,
+					severity: "error",
+					text: "ERROR",
+					details: action?.error?.message,
+				}),
+			})
+		})
 	},
 })
 
