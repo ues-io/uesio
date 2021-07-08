e24bb80db4bb266a1b9d56a266622a152457276c
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2021-07-08 19:26:01 +0200

    Component tooltips (#555)
    * Added tooltips to the builder component panel
    
    Added tooltip in components definition
    reworked the componentspanel function responsible for getting and grouping the components to work with tooltip
    
    * Update componentspanel.tsx
    
    * Better tooltip functionality  and styling for the components in the builder
    
    * feedback
    
    * Update path.ts
    
    * Prettier
    
    * prettier
    
    * simplify a little
    
    * remove unnecessary types
    
    Co-authored-by: humandad <55447225+humandad@users.noreply.github.com>

--- libs/uesioapps/io/bundle/components/view/io.box/boxdefinition.ts
+++ libs/uesioapps/io/bundle/components/view/io.box/boxdefinition.ts
@@ -10,6 +10,7 @@ interface BoxProps extends definition.BaseProps {
 
 const BoxPropertyDefinition: builder.BuildPropertiesDefinition = {
 	title: "Box",
+	description: "Used for grouping elements",
 	defaultDefinition: () => ({}),
 	properties: [],
 	sections: [],
