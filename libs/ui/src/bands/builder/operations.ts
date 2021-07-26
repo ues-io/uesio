6f66691b141d123e9f077b41d5303830706032f9
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2021-07-26 14:02:47 +0200

    Rework Save Cancel and wire create select (#594)

--- libs/ui/src/bands/builder/operations.ts
+++ libs/ui/src/bands/builder/operations.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import { Context } from "../../context/context"
+import { SaveResponseBatch } from "../../load/saveresponse"
 import { UesioThunkAPI } from "../utils"
+import { PlainWireRecord } from "../wirerecord/types"
 import { getMetadataListKey } from "./selectors"
 import { MetadataListStore, MetadataType } from "./types"
 
@@ -43,4 +45,44 @@ const getAvailableNamespaces = createAsyncThunk<
 	}
 )
 
-export default { getMetadataList, getAvailableNamespaces }
+const save = createAsyncThunk<
+	SaveResponseBatch,
+	{
+		context: Context
+	},
+	UesioThunkAPI
+>("builder/save", async ({ context }, api) => {
+	const changes: Record<string, PlainWireRecord> = {}
+	const state = api.getState().viewdef?.entities
+	const workspace = context.getWorkspace()
+
+	if (!workspace) throw new Error("No Workspace in context")
+
+	// Loop over view defs
+	if (state) {
+		for (const defKey of Object.keys(state)) {
+			const defState = state[defKey]
+			if (defState?.yaml === defState?.originalYaml) {
+				continue
+			}
+			if (defState?.yaml) {
+				changes[defKey] = {
+					"studio.definition": defState.yaml.toString(),
+					"studio.id": `${workspace.app}_${workspace.name}_${defState.name}`,
+				}
+			}
+		}
+	}
+	return api.extra.saveData(new Context(), {
+		wires: [
+			{
+				wire: "saveview",
+				collection: "studio.views",
+				changes,
+				deletes: {},
+			},
+		],
+	})
+})
+
+export default { getMetadataList, getAvailableNamespaces, save }
