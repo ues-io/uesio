fd4c36f5e00250a3e820ef2b86da2bac1e6e9027
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2021-08-10 10:49:20 +0200

    more attractive blank slate when creating new view (#632)

--- libs/uesioapps/studio/bundle/components/shared/canvas.tsx
+++ libs/uesioapps/studio/bundle/components/shared/canvas.tsx
@@ -1,5 +1,6 @@
-import { FunctionComponent, DragEvent } from "react"
+import { FunctionComponent, DragEvent, useState, useEffect } from "react"
 import { definition, component, hooks, styles } from "@uesio/ui"
+const Icon = component.registry.getUtility("io.icon")
 
 const Canvas: FunctionComponent<definition.UtilityProps> = (props) => {
 	const classes = styles.useUtilityStyles(
@@ -21,15 +22,59 @@ const Canvas: FunctionComponent<definition.UtilityProps> = (props) => {
 				padding: "0.05px", // Hack to prevent margin collapse
 				position: "relative",
 			},
+
+			noContent: {
+				display: "flex",
+				height: "100%",
+				position: "absolute",
+				inset: "15px",
+				justifyContent: "center",
+				alignItems: "center",
+
+				".icon": {
+					fontFamily: "Material Icons",
+					fontSize: "2em",
+					marginBottom: "0.5em",
+				},
+				".text": {
+					marginTop: 0,
+				},
+
+				".quote": {
+					marginTop: "2em",
+					opacity: 0.5,
+					h4: {
+						marginBottom: "0.25em",
+					},
+					p: {
+						marginTop: 0,
+						fontSize: "0.8em",
+					},
+				},
+
+				".inner": {
+					textAlign: "center",
+					border: "3px dashed rgba(238, 238, 238)",
+					padding: "0.5em 2em 2em 2em",
+					borderRadius: "2em",
+				},
+			},
 		},
 		props
 	)
-	const route = props.context.getRoute()
 
+	// Hide/show blank slate div
+	const [hasComponents, setHasComponents] = useState(false)
+	useEffect(() => {
+		setHasComponents(
+			!props.context.getViewDef()?.definition?.components?.length
+		)
+	}, [props.context])
+
+	const route = props.context.getRoute()
 	if (!route) {
 		return null
 	}
-
 	const uesio = hooks.useUesio(props)
 
 	// Handle the situation where a draggable leaves the canvas.
@@ -50,7 +95,6 @@ const Canvas: FunctionComponent<definition.UtilityProps> = (props) => {
 			}
 		}
 	}
-
 	// Handle the situation where no other slots are accepting draggable
 	// items. This clears out the current drop node so that our slot
 	// acceptance indicators go away.
@@ -61,22 +105,48 @@ const Canvas: FunctionComponent<definition.UtilityProps> = (props) => {
 	}
 
 	return (
-		<div
-			onDragLeave={onDragLeave}
-			onDragOver={onDragOver}
-			className={classes.root}
-		>
-			<div className={classes.inner}>
-				<component.View
-					context={props.context}
-					path=""
-					definition={{
-						view: route.view,
-						params: route.params,
-					}}
-				/>
+		<>
+			<div
+				onDragLeave={onDragLeave}
+				onDragOver={onDragOver}
+				className={classes.root}
+			>
+				<div className={classes.inner}>
+					<component.View
+						context={props.context}
+						path=""
+						definition={{
+							view: route.view,
+							params: route.params,
+						}}
+					/>
+
+					{/* No content yet */}
+					{hasComponents && (
+						<div className={classes.noContent}>
+							<div className="inner">
+								<Icon
+									className="icon"
+									icon={"flip_to_back"}
+									context={props.context}
+								/>
+								<h3 className="text">
+									Drag and drop any component here to get
+									started
+								</h3>
+								<div className="quote">
+									<h4>
+										What's better than a blank slate in the
+										right hands?
+									</h4>
+									<p>&mdash; Frank Underwood &mdash;</p>
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 Canvas.displayName = "Canvas"
