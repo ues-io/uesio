7e6504df03b53f05a68ba937d727bcc1197fc459
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2021-08-17 17:34:24 +0200

    Update avatardefinition.ts (#643)

--- libs/uesioapps/io/bundle/components/view/io.avatar/avatardefinition.ts
+++ libs/uesioapps/io/bundle/components/view/io.avatar/avatardefinition.ts
@@ -11,10 +11,25 @@ interface AvatarProps extends definition.BaseProps {
 
 const AvatarPropertyDefinition: builder.BuildPropertiesDefinition = {
 	title: "Avatar",
-	defaultDefinition: () => ({}),
-	properties: [],
+	defaultDefinition: () => ({
+		text: "$User{initials}",
+		image: "$User{picture}",
+	}),
+	properties: [
+		{
+			name: "text",
+			type: "TEXT",
+			label: "text",
+		},
+		{
+			name: "image",
+			type: "TEXT",
+			label: "image",
+		},
+	],
 	sections: [],
 	actions: [],
+	traits: ["uesio.standalone"],
 	type: "component",
 	classes: ["root"],
 }
