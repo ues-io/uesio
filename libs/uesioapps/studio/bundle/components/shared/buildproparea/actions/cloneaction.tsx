aaef6f1071113ec813cbd3b1588b5269e2b66b6b
Wessel <wesselvdp@gmail.com>
2021-08-19 11:00:54 +0200

    Clone action button in seperate component

--- libs/uesioapps/studio/bundle/components/shared/buildproparea/actions/cloneaction.tsx
+++ libs/uesioapps/studio/bundle/components/shared/buildproparea/actions/cloneaction.tsx
@@ -1,35 +1,19 @@
 import { FunctionComponent } from "react"
-import { hooks, builder, component } from "@uesio/ui"
 import { ActionProps } from "./actiondefinition"
 import ActionButton from "./actionbutton"
 
-const CloneAction: FunctionComponent<ActionProps> = (props) => {
-	const uesio = hooks.useUesio(props)
-	const { path = "", valueAPI, context } = props
-
-	const action = props.action as builder.CloneAction
-	if (!action) {
-		return null
-	}
-
-	const onClickHandler = (): void => {
-		const { registry } = component
-		const componentKey = action.componentKey
-		const propDef = registry.getPropertiesDefinition(componentKey)
-
-		if (propDef) {
-			valueAPI.clone(path)
-		}
-	}
-
-	return (
-		<ActionButton
-			title={action.label}
-			onClick={onClickHandler}
-			icon="copy"
-			context={props.context}
-		/>
-	)
-}
+const CloneAction: FunctionComponent<ActionProps> = ({
+	path = "",
+	valueAPI,
+	action = { label: "copy" },
+	context,
+}) => (
+	<ActionButton
+		title={action.label}
+		onClick={() => valueAPI.clone(path)}
+		icon="copy"
+		context={context}
+	/>
+)
 
 export default CloneAction
