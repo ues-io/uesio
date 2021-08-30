cb37a4aeeb95028a7ea7a369d74b9e7f3bd9b96a
Wessel <wesselvdp@gmail.com>
2021-08-30 17:42:53 +0200

    styling edits
    1. Used existing propnodetag component for the draggable variants
    2. "selected" prop for compponents is used to determine "expanded" state. Removed expanded state locally
    3. Expanded propnode tags are not draggable
    Nice to have: a11y support

--- libs/uesioapps/studio/bundle/components/shared/buildpropitem/expandablepropnodetag.tsx
+++ libs/uesioapps/studio/bundle/components/shared/buildpropitem/expandablepropnodetag.tsx
@@ -1,6 +1,5 @@
-import { FunctionComponent, useState } from "react"
-
-import { component, context, styles, hooks } from "@uesio/ui"
+import React, { FC, useState, useEffect, useRef } from "react"
+import { component, context, styles } from "@uesio/ui"
 
 type Props = {
 	title: string
@@ -11,21 +10,17 @@ type Props = {
 	draggable?: string
 	context: context.Context
 	tooltip?: string
+	// children: React.ReactNode[]
 }
 
 const Icon = component.registry.getUtility("io.icon")
 const Tile = component.registry.getUtility("io.tile")
 const IconButton = component.registry.getUtility("io.iconbutton")
 
-const onClickHandler = (): void => {
-	alert("WORKS")
-}
-
-const ExpandablePropNodeTag: FunctionComponent<Props> = (props) => {
+const ExpandablePropNodeTag: FC<Props> = (props) => {
 	const {
 		title,
 		onClick,
-		children,
 		draggable,
 		icon,
 		iconColor,
@@ -33,62 +28,78 @@ const ExpandablePropNodeTag: FunctionComponent<Props> = (props) => {
 		selected,
 		context,
 	} = props
-
+	const children = props.children as React.ReactNode[]
+	const expandBox = useRef<HTMLDivElement>(null)
 	const classes = styles.useStyles(
 		{
 			root: {
 				cursor: draggable ? "grab" : "inherit",
-				"&:hover .tooltip": {
+				"&:hover .icon": {
 					opacity: 0.3,
 				},
 				border: "1px solid #eee",
 				borderRadius: "4px",
-				".tooltip": {
+				marginBottom: "4px",
+
+				".icon": {
 					cursor: "initial",
 					opacity: 0,
-					textTransform: "initial",
-					transition: "opacity 0.125s ease",
+					transition: "opacity 0.125s ease, transform 0.3s ease",
 					"&:hover": {
 						opacity: 1,
 					},
+					transform: `rotate(${selected ? "18" : ""}0deg)`,
 				},
 			},
 			title: {
 				textTransform: "uppercase",
 			},
+
+			badgeContainer: {
+				display: "flex",
+				padding: "0 8px",
+				opacity: selected ? "1" : "0",
+				flexFlow: "row wrap",
+				maxHeight: selected ? "300px" : "0px",
+				transition: "opacity 0.125s ease, max-height 0.3s ease",
+			},
 		},
 		props
 	)
 
-	const [expanded, setExpanded] = useState(false)
-
-	const triggerState = () => {
-		!expanded ? setExpanded(true) : setExpanded(false)
-	}
-
+	const expandable = children && children.length > 0
 	return (
 		<div
 			className={classes.root}
-			draggable={!expanded && !!draggable}
+			draggable={!selected && !!draggable}
 			data-type={draggable}
 		>
 			<Tile
 				variant="io.tile.studio.expandablepropnodetag"
-				avatar={<Icon icon={icon} context={context} />}
+				avatar={
+					<Icon
+						icon={selected && expandable ? "invert_colors" : icon}
+						context={context}
+					/>
+				}
 				context={context}
 				onClick={onClick}
 			>
 				<span className={classes.title}>{title}</span>
-				<IconButton
-					size="small"
-					icon="expand"
-					label={"Expand"}
-					className="tooltip"
-					context={context}
-					onClick={triggerState}
-				/>
+				{expandable && (
+					<IconButton
+						size="small"
+						icon="expand_more"
+						label={"Expand"}
+						className="icon"
+						context={context}
+					/>
+				)}
 			</Tile>
-			{expanded && children && <div> {children} </div>}
+
+			<div ref={expandBox} className={classes.badgeContainer}>
+				{selected && expandable && children}
+			</div>
 		</div>
 	)
 }
