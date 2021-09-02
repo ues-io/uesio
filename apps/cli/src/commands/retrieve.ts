6f6f316c7b1770e48f25b5ad01979b23405e9076
Abel Jimenez Molla <abel@ues.io>
2021-09-02 13:36:13 +0200

    Retrieve from CLI

---
+++ apps/cli/src/commands/retrieve.ts
@@ -0,0 +1,50 @@
+import { Command } from "@oclif/command"
+import { get } from "../request/request"
+import { getApp, getWorkspace } from "../config/config"
+import { authorize } from "../auth/login"
+import * as fs from "fs"
+import unzipper from "unzipper"
+import chalk from "chalk"
+
+export default class Retrieve extends Command {
+	static description = "retrieve metadata items"
+	static flags = {}
+	static args = []
+
+	async run(): Promise<void> {
+		const app = await getApp()
+		const workspace = await getWorkspace()
+		const user = await authorize()
+		const tmp = "tmp.zip"
+
+		console.log(
+			`Retrieving app: ${chalk.magenta(app)} from workspace ${chalk.green(
+				workspace
+			)}.`
+		)
+
+		fs.rmdir("bundle", { recursive: true }, () => {
+			console.log("Local bundle deleted!")
+		})
+
+		const response = await get(
+			`workspace/${app}/${workspace}/metadata/retrieve`,
+			user.cookie
+		)
+
+		response.body.pipe(fs.createWriteStream(tmp)).on("close", function () {
+			fs.createReadStream(tmp)
+				.pipe(
+					unzipper.Extract({
+						path: "bundle",
+					})
+				)
+				.on("close", function () {
+					console.log("New bundle extracted!")
+					fs.unlink("./" + tmp, () => {
+						console.log("erased temporary data")
+					})
+				})
+		})
+	}
+}
