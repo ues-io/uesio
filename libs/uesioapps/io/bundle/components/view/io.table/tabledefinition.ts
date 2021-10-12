c622f5710c72a87ed3cdf9b237a97c83ddfd0bbe
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2021-10-12 00:37:05 -0400

    Refactor table component (#742)
    * refactor table component
    
    * fix stuph
    
    * cleanup

--- libs/uesioapps/io/bundle/components/view/io.table/tabledefinition.ts
+++ libs/uesioapps/io/bundle/components/view/io.table/tabledefinition.ts
@@ -2,29 +2,16 @@ import { definition, builder, signal, component } from "@uesio/ui"
 
 type TableMode = "READ" | "EDIT"
 
-type TableClasses = Record<
-	| "root"
-	| "table"
-	| "headerCell"
-	| "header"
-	| "cell"
-	| "row"
-	| "rowDeleted"
-	| "rowNumberCell"
-	| "rowNumber",
-	string
->
-
 type TableState = {
 	mode: TableMode
 }
 
-type TableDefinition = {
+interface TableDefinition extends definition.BaseDefinition {
 	id: string
 	wire: string
 	mode: TableMode
 	columns: definition.DefinitionList
-	rowactions: RowAction[]
+	rowactions?: RowAction[]
 	rownumbers: boolean
 }
 
@@ -99,13 +86,6 @@ const TablePropertyDefinition: builder.BuildPropertiesDefinition = {
 	type: "component",
 	classes: ["root"],
 }
-export {
-	TableProps,
-	TableState,
-	TableDefinition,
-	ColumnDefinition,
-	TableClasses,
-	RowAction,
-}
+export { TableProps, TableState, TableDefinition, ColumnDefinition, RowAction }
 
 export default TablePropertyDefinition
