3816656b59a9b8c0da248c1debc5d0d44ff74d12
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2021-11-15 16:26:59 +0100

    Lab table (#783)
    * added table files
    
    * table isolation
    
    * table isolation
    
    * Previous lab/table pr simplified
    
    1. Actions bar actions are not functional yet, merely the placement is working here.
    2. The buildwrapper of fields in a cell is missing, this is intentional but I just realised this is actually inconvenient when you're building out the table by duplicating columns.
    
    * fixed dropping fields in the lab table
    
    * Update table.tsx
    
    hide fieldlabels in table
    
    * table with single column can't setDragCol
    
    * missing key prop
    
    * linter fixes

---
+++ libs/uesioapps/lab/bundle/components/view/lab.table/tablebuilder.tsx
@@ -0,0 +1,37 @@
+import { FunctionComponent } from "react"
+import { TableProps } from "./tabledefinition"
+import Table from "./table"
+import { styles, component, hooks } from "@uesio/ui"
+import WireHelper from "../../utility/lab.wirehelper/wirehelper"
+
+const BuildWrapper = component.registry.getUtility("studio.buildwrapper")
+
+const TableBuilder: FunctionComponent<TableProps> = (props) => {
+	const { context, path = "", definition } = props
+	const uesio = hooks.useUesio(props)
+
+	const wire = uesio.wire.useWire(definition.wire)
+
+	const [dragType, dragItem, dragPath] = uesio.builder.useDragNode()
+	const classes = styles.useStyles(
+		{
+			root: {},
+			header: {},
+		},
+		{
+			context,
+		}
+	)
+
+	return (
+		<BuildWrapper {...props} classes={classes}>
+			{!wire ? (
+				<WireHelper {...props} />
+			) : (
+				<Table {...props} isDragging={!!dragType && !!dragItem} />
+			)}
+		</BuildWrapper>
+	)
+}
+
+export default TableBuilder
