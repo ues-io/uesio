0ce31b3d5b9edb9d2e49439623c2db0a84492c89
Abel Jimenez Molla <abel@ues.io>
2021-11-19 11:17:36 +0100

    generic reducers

--- libs/ui/src/bands/componentvariant/index.ts
+++ libs/ui/src/bands/componentvariant/index.ts
@@ -1,12 +1,54 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import loadOp from "../viewdef/operations/load"
-import { getNodeAtPath, newDoc, parse } from "../../yamlutils/yamlutils"
+import {
+	getNodeAtPath,
+	newDoc,
+	parse,
+	setNodeAtPath,
+} from "../../yamlutils/yamlutils"
 import componentVariantAdapter from "./adapter"
-import { getFullPathParts, parseVariantKey } from "../../component/path"
+import { getFullPathParts, parseVariantKey, toPath } from "../../component/path"
 import { ComponentVariant } from "./types"
-import { Scalar, YAMLMap } from "yaml"
-import { setDefinition, SetDefinitionPayload } from "../builder"
-import { setDef } from "./reducers"
+import yaml, { Scalar, YAMLMap } from "yaml"
+import {
+	addDefinition,
+	AddDefinitionPayload,
+	moveDefinition,
+	MoveDefinitionPayload,
+	removeDefinition,
+	RemoveDefinitionPayload,
+	setDefinition,
+	SetDefinitionPayload,
+	setYaml,
+	YamlUpdatePayload,
+} from "../builder"
+import { addDef, moveDef, removeDef, setDef } from "./reducers"
+import { setWith } from "lodash"
+
+const updateYaml = (state: ComponentVariant, payload: YamlUpdatePayload) => {
+	const { path, yaml: yamlDoc } = payload
+	const pathArray = toPath(path)
+	const definition = yamlDoc.toJSON()
+
+	// Set the definition JS Object from the yaml
+	setWith(state, ["definition", ...pathArray], definition)
+	if (!state.originalYaml) {
+		state.originalYaml = yamlDoc
+	}
+
+	if (!state.yaml) {
+		state.yaml = yamlDoc
+		return
+	}
+
+	if (state.yaml === state.originalYaml) {
+		state.originalYaml = parse(state.originalYaml.toString())
+	}
+	if (!path) return (state.yaml = new yaml.Document(state.yaml.toJSON()))
+
+	// We actually don't want components using useYaml to rerender
+	setNodeAtPath(path, state.yaml.contents, yamlDoc.contents)
+}
 
 const componentVariantSlice = createSlice({
 	name: "componentVariant",
@@ -58,6 +100,74 @@ const componentVariantSlice = createSlice({
 						setDef(entityState, {
 							path: localPath,
 							definition: payload.definition,
+						})
+				}
+			}
+		)
+		builder.addCase(
+			addDefinition,
+			(state, { payload }: PayloadAction<AddDefinitionPayload>) => {
+				const [metadataType, metadataItem, localPath] =
+					getFullPathParts(payload.path)
+				if (metadataType === "componentvariant") {
+					const entityState = state.entities[metadataItem]
+					entityState &&
+						addDef(entityState, {
+							path: localPath,
+							definition: payload.definition,
+							index: payload.index,
+						})
+				}
+			}
+		)
+		builder.addCase(
+			moveDefinition,
+			(state, { payload }: PayloadAction<MoveDefinitionPayload>) => {
+				const [toType, toItem, toPath] = getFullPathParts(
+					payload.toPath
+				)
+				const [fromType, fromItem, fromPath] = getFullPathParts(
+					payload.fromPath
+				)
+				if (
+					toType === "componentvariant" &&
+					fromType === "componentvariant" &&
+					toItem === fromItem
+				) {
+					const entityState = state.entities[toItem]
+					entityState &&
+						moveDef(entityState, {
+							fromPath,
+							toPath,
+						})
+				}
+			}
+		)
+		builder.addCase(
+			removeDefinition,
+			(state, { payload }: PayloadAction<RemoveDefinitionPayload>) => {
+				const [metadataType, metadataItem, localPath] =
+					getFullPathParts(payload.path)
+				if (metadataType === "componentvariant") {
+					const entityState = state.entities[metadataItem]
+					entityState &&
+						removeDef(entityState, {
+							path: localPath,
+						})
+				}
+			}
+		)
+		builder.addCase(
+			setYaml,
+			(state, { payload }: PayloadAction<YamlUpdatePayload>) => {
+				const [metadataType, metadataItem, localPath] =
+					getFullPathParts(payload.path)
+				if (metadataType === "componentvariant") {
+					const entityState = state.entities[metadataItem]
+					entityState &&
+						updateYaml(entityState, {
+							path: localPath,
+							yaml: payload.yaml,
 						})
 				}
 			}
--- libs/ui/src/bands/componentvariant/reducers.ts
+++ libs/ui/src/bands/componentvariant/reducers.ts
@@ -1,34 +1,34 @@
-import { setWith, toPath } from "lodash"
-import { Definition, YamlDoc } from "../../definition/definition"
-import { parse, setNodeAtPath } from "../../yamlutils/yamlutils"
-import { SetDefinitionPayload } from "../builder"
+import {
+	AddDefinitionPayload,
+	MoveDefinitionPayload,
+	RemoveDefinitionPayload,
+	SetDefinitionPayload,
+} from "../builder"
 import { ComponentVariant } from "./types"
-
-const getNewNode = (yaml: YamlDoc, definition: Definition) => {
-	//Keep this line on top; 0 is false in JS, but we want to write it to YAML
-	if (definition === 0) {
-		return yaml.createNode(definition)
-	}
-
-	if (!definition) {
-		return null
-	}
-
-	return yaml.createNode(definition)
-}
+import {
+	moveDef as genericMoveDef,
+	addDef as genericAddDef,
+	setDef as genericSetDef,
+	removeDef as genericRemoveDef,
+} from "../../store/reducers"
 
 const setDef = (state: ComponentVariant, payload: SetDefinitionPayload) => {
-	const { path, definition } = payload
-	const pathArray = toPath(path)
-
-	// Set the definition JS Object
-	setWith(state, ["definition", ...pathArray], definition)
-	if (state.yaml) {
-		// create a new document so components using useYaml will rerender
-		state.yaml = parse(state.yaml.toString())
-		const newNode = getNewNode(state.yaml, definition)
-		setNodeAtPath(path, state.yaml.contents, newNode)
-	}
+	genericSetDef(state, payload)
 }
 
-export { setDef }
+const addDef = (state: ComponentVariant, payload: AddDefinitionPayload) => {
+	genericAddDef(state, payload)
+}
+
+const removeDef = (
+	state: ComponentVariant,
+	payload: RemoveDefinitionPayload
+) => {
+	genericRemoveDef(state, payload)
+}
+
+const moveDef = (state: ComponentVariant, payload: MoveDefinitionPayload) => {
+	genericMoveDef(state, payload)
+}
+
+export { removeDef, addDef, moveDef, setDef }
