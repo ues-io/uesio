b958b9c40d6a23ae22ebb4975a823bcb4f68afec
Abel Jimenez Molla <abel@ues.io>
2021-12-01 10:50:29 +0100

    Language label Backend Side

---
+++ apps/platform/pkg/meta/label.go
@@ -0,0 +1,113 @@
+package meta
+
+import (
+	"github.com/humandad/yaml"
+)
+
+// Label struct
+type Label struct {
+	ID        string     `yaml:"-" uesio:"uesio.id"`
+	Name      string     `yaml:"name" uesio:"studio.name"`
+	Namespace string     `yaml:"-" uesio:"-"`
+	Workspace *Workspace `yaml:"-" uesio:"studio.workspace"`
+	itemMeta  *ItemMeta  `yaml:"-" uesio:"-"`
+	CreatedBy *User      `yaml:"-" uesio:"uesio.createdby"`
+	Owner     *User      `yaml:"-" uesio:"uesio.owner"`
+	UpdatedBy *User      `yaml:"-" uesio:"uesio.updatedby"`
+	UpdatedAt int64      `yaml:"-" uesio:"uesio.updatedat"`
+	CreatedAt int64      `yaml:"-" uesio:"uesio.createdat"`
+}
+
+// GetCollectionName function
+func (l *Label) GetCollectionName() string {
+	return l.GetBundleGroup().GetName()
+}
+
+// GetCollection function
+func (l *Label) GetCollection() CollectionableGroup {
+	var lc LabelCollection
+	return &lc
+}
+
+// GetConditions function
+func (l *Label) GetConditions() map[string]string {
+	return map[string]string{
+		"studio.name": l.Name,
+	}
+}
+
+// GetBundleGroup function
+func (l *Label) GetBundleGroup() BundleableGroup {
+	var lc LabelCollection
+	return &lc
+}
+
+// GetKey function
+func (l *Label) GetKey() string {
+	return l.Namespace + "." + l.Name
+}
+
+// GetPath function
+func (l *Label) GetPath() string {
+	return l.GetKey() + ".yaml"
+}
+
+// GetPermChecker function
+func (l *Label) GetPermChecker() *PermissionSet {
+	return nil
+}
+
+// SetField function
+func (l *Label) SetField(fieldName string, value interface{}) error {
+	return StandardFieldSet(l, fieldName, value)
+}
+
+// GetField function
+func (l *Label) GetField(fieldName string) (interface{}, error) {
+	return StandardFieldGet(l, fieldName)
+}
+
+// GetNamespace function
+func (l *Label) GetNamespace() string {
+	return l.Namespace
+}
+
+// SetNamespace function
+func (l *Label) SetNamespace(namespace string) {
+	l.Namespace = namespace
+}
+
+// SetWorkspace function
+func (l *Label) SetWorkspace(workspace string) {
+	l.Workspace = &Workspace{
+		ID: workspace,
+	}
+}
+
+// Loop function
+func (l *Label) Loop(iter func(string, interface{}) error) error {
+	return StandardItemLoop(l, iter)
+}
+
+// Len function
+func (l *Label) Len() int {
+	return StandardItemLen(l)
+}
+
+// GetItemMeta function
+func (l *Label) GetItemMeta() *ItemMeta {
+	return l.itemMeta
+}
+
+// SetItemMeta function
+func (l *Label) SetItemMeta(itemMeta *ItemMeta) {
+	l.itemMeta = itemMeta
+}
+
+func (l *Label) UnmarshalYAML(node *yaml.Node) error {
+	err := validateNodeName(node, l.Name)
+	if err != nil {
+		return err
+	}
+	return node.Decode(l)
+}
---
+++ apps/platform/pkg/meta/labelcollection.go
@@ -0,0 +1,80 @@
+package meta
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/thecloudmasters/uesio/pkg/meta/loadable"
+)
+
+// LabelCollection slice
+type LabelCollection []Label
+
+func (lc *LabelCollection) Filter(iter func(item loadable.Item) (bool, error)) error {
+	return nil
+}
+
+// GetName function
+func (lc *LabelCollection) GetName() string {
+	return "studio.labels"
+}
+
+// GetFields function
+func (lc *LabelCollection) GetFields() []string {
+	return StandardGetFields(&Label{})
+}
+
+// NewItem function
+func (lc *LabelCollection) NewItem() loadable.Item {
+	*lc = append(*lc, Label{})
+	return &(*lc)[len(*lc)-1]
+}
+
+// NewBundleableItemWithKey function
+func (lc *LabelCollection) NewBundleableItemWithKey(key string) (BundleableItem, error) {
+	keyArray := strings.Split(key, ".")
+	if len(keyArray) != 2 {
+		return nil, errors.New("Invalid Label Key: " + key)
+	}
+	*lc = append(*lc, Label{
+		Namespace: keyArray[0],
+		Name:      keyArray[1],
+	})
+	return &(*lc)[len(*lc)-1], nil
+}
+
+// GetKeyFromPath function
+func (lc *LabelCollection) GetKeyFromPath(path string, conditions BundleConditions) (string, error) {
+	return StandardKeyFromPath(path, conditions)
+}
+
+// GetItem function
+func (lc *LabelCollection) GetItem(index int) loadable.Item {
+	return &(*lc)[index]
+}
+
+// Loop function
+func (lc *LabelCollection) Loop(iter loadable.GroupIterator) error {
+	for index := range *lc {
+		err := iter(lc.GetItem(index), index)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Len function
+func (lc *LabelCollection) Len() int {
+	return len(*lc)
+}
+
+// GetItems function
+func (lc *LabelCollection) GetItems() interface{} {
+	return *lc
+}
+
+// Slice function
+func (lc *LabelCollection) Slice(start int, end int) {
+
+}
