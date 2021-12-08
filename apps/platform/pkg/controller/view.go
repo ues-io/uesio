132780357c998f88e7f4c25057b12cf3b5145502
Abel Jimenez Molla <abel@ues.io>
2021-12-08 10:36:23 +0100

    order and add translation to runtime

--- apps/platform/pkg/controller/view.go
+++ apps/platform/pkg/controller/view.go
@@ -125,6 +125,7 @@ func addmap(a map[string]string, b map[string]string) {
 func getReferenceLabels(translations meta.TranslationCollection, session *sess.Session) map[string]string {
 
 	originalNamespace := session.GetContextAppName()
+	println(originalNamespace)
 	labelRefs := map[string]string{}
 	index := -1
 
@@ -139,6 +140,41 @@ func getReferenceLabels(translations meta.TranslationCollection, session *sess.S
 	}
 
 	return labelRefs
+}
+
+func getTranslatedLabels(session *sess.Session) (meta.LabelCollection, error) {
+
+	userLanguage := session.GetUserInfo().Language
+	println(userLanguage)
+
+	var labels meta.LabelCollection
+	err := bundle.LoadAllFromAny(&labels, nil, session)
+	if err != nil {
+		return nil, errors.New("Failed to load labels: " + err.Error())
+	}
+
+	var translations meta.TranslationCollection
+	if userLanguage != "" {
+		err = bundle.LoadAllFromAny(&translations, meta.BundleConditions{
+			"studio.language": userLanguage,
+		}, session)
+
+		if err != nil {
+			return nil, errors.New("Failed to load translations: " + err.Error())
+		}
+	}
+
+	labelRefs := getReferenceLabels(translations, session)
+
+	for i := range labels {
+		//check if we have any translation for that label
+		if val, ok := labelRefs[labels[i].GetKey()]; ok {
+			labels[i].Value = val
+		}
+	}
+
+	return labels, nil
+
 }
 
 func getBuilderDependencies(session *sess.Session) (*ViewDependencies, error) {
@@ -159,26 +195,11 @@ func getBuilderDependencies(session *sess.Session) (*ViewDependencies, error) {
 		return nil, errors.New("Failed to load studio variants: " + err.Error())
 	}
 
-	userLanguage := session.GetUserInfo().Language
+	labels, err := getTranslatedLabels(session)
 
-	var labels meta.LabelCollection
-	err = bundle.LoadAllFromAny(&labels, nil, session)
 	if err != nil {
-		return nil, errors.New("Failed to load labels: " + err.Error())
+		return nil, errors.New("Failed to get translated labels: " + err.Error())
 	}
-
-	var translations meta.TranslationCollection
-	if userLanguage != "" {
-		err = bundle.LoadAllFromAny(&translations, meta.BundleConditions{
-			"studio.language": userLanguage,
-		}, session)
-
-		if err != nil {
-			return nil, errors.New("Failed to load translations: " + err.Error())
-		}
-	}
-
-	labelRefs := getReferenceLabels(translations, session)
 
 	deps := ViewDependencies{
 		ComponentPacks:    map[string]bool{},
@@ -213,10 +234,6 @@ func getBuilderDependencies(session *sess.Session) (*ViewDependencies, error) {
 
 	for i := range labels {
 		label := labels[i]
-		//check if we have any translation for that label
-		if val, ok := labelRefs[label.GetKey()]; ok {
-			label.Value = val
-		}
 		deps.Labels[label.GetKey()] = &label
 	}
 
@@ -332,11 +349,18 @@ func getViewDependencies(view *meta.View, session *sess.Session) (*ViewDependenc
 		return nil, err
 	}
 
+	labels, err := getTranslatedLabels(session)
+
+	if err != nil {
+		return nil, errors.New("Failed to get translated labels: " + err.Error())
+	}
+
 	deps := ViewDependencies{
 		ComponentPacks:    map[string]bool{},
 		ComponentVariants: map[string]*meta.ComponentVariant{},
 		ConfigValues:      map[string]string{},
 		FeatureFlags:      map[string]*FeatureFlagResponse{},
+		Labels:            map[string]*meta.Label{},
 	}
 
 	packs := map[string]meta.ComponentPackCollection{}
@@ -359,6 +383,11 @@ func getViewDependencies(view *meta.View, session *sess.Session) (*ViewDependenc
 	for i := range ffr {
 		featureFlag := ffr[i]
 		deps.FeatureFlags[featureFlag.Name] = &featureFlag
+	}
+
+	for i := range labels {
+		label := labels[i]
+		deps.Labels[label.GetKey()] = &label
 	}
 
 	return &deps, nil
