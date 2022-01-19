881b466d3d9313bb2c20e6ffe6b6cd393649f79a
Wessel <wesselvdp@gmail.com>
2022-01-19 12:26:06 +0100

    Update fieldset.tsx

--- libs/uesioapps/io/bundle/components/utility/io.fieldset/fieldset.tsx
+++ libs/uesioapps/io/bundle/components/utility/io.fieldset/fieldset.tsx
@@ -3,10 +3,11 @@ import { definition, styles, context, collection, wire } from "@uesio/ui"
 
 interface Props extends definition.UtilityProps {
 	fieldLabel: string
+	disabled: boolean
 }
 
-const RadioButtons: FC<Props> = (props) => {
-	const { children, fieldLabel } = props
+const Fieldset: FC<Props> = (props) => {
+	const { children, fieldLabel, disabled } = props
 
 	const classes = styles.useUtilityStyles(
 		{
@@ -24,11 +25,11 @@ const RadioButtons: FC<Props> = (props) => {
 	)
 
 	return (
-		<fieldset className={classes.fieldset} {...props}>
+		<fieldset className={classes.fieldset} disabled={disabled}>
 			<legend className={classes.legend}>{fieldLabel}</legend>
 			{children}
 		</fieldset>
 	)
 }
 
-export default RadioButtons
+export default Fieldset
