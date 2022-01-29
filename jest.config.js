d11ae554bc2b1e00839795675f1be5d02a26fe58
humandad <55447225+humandad@users.noreply.github.com>
2022-01-29 10:13:53 -0500

    upgrade to nx 13

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
+const { getJestProjects } = require("@nrwl/jest")
+
 module.exports = {
-	projects: [
-		"<rootDir>/libs/ui",
-		"<rootDir>/libs/uesioapps/uesio",
-		"<rootDir>/libs/uesioapps/sample",
-		"<rootDir>/libs/uesioapps/crm",
-		"<rootDir>/libs/loginhelpers",
-		"<rootDir>/libs/vendor",
-		"<rootDir>/apps/cli",
-	],
+	projects: [...getJestProjects()],
 }
