ef9faa7f0c3f320ebdbe894932fa5895fe641f1e
humandad <55447225+humandad@users.noreply.github.com>
2022-04-08 13:35:24 -0400

    remove after save of collections

--- apps/platform/pkg/datasource/systembot_after_collection.go
+++ apps/platform/pkg/datasource/systembot_after_collection.go
@@ -1,75 +1,80 @@
 package datasource
 
 import (
-	"strings"
-
 	"github.com/thecloudmasters/uesio/pkg/adapt"
 	"github.com/thecloudmasters/uesio/pkg/sess"
 )
 
 func runCollectionAfterSaveBot(request *adapt.SaveOp, connection adapt.Connection, session *sess.Session) error {
 
-	var workspaceID string
+	// I'm actually not sure we want to do this at all.
+	// Maybe this functionality could be part of a generator.
+	// This will cause all deployments to auto-create name fields,
+	// Which for some collections, this is not what we want.
+	return nil
+	/*
+		var workspaceID string
 
-	fieldChanges := adapt.Collection{}
-	collectionChanges := adapt.Collection{}
+		fieldChanges := adapt.Collection{}
+		collectionChanges := adapt.Collection{}
 
-	err := request.LoopChanges(func(change *adapt.ChangeItem) error {
+		err := request.LoopChanges(func(change *adapt.ChangeItem) error {
 
-		err := checkWorkspaceID(&workspaceID, change)
+			err := checkWorkspaceID(&workspaceID, change)
+			if err != nil {
+				return err
+			}
+
+			name, err := change.GetFieldAsString("uesio/studio.name")
+			if err != nil {
+				return err
+			}
+
+			//Creates Default Name Field for collection
+			if change.IsNew {
+
+				//TO-DO Nice way of doing
+				idParts := strings.Split(workspaceID, "_")
+				fieldCollection := idParts[0] + "." + name
+				fieldItem := adapt.Item{
+					"uesio/studio.name":       "name",
+					"uesio/studio.type":       "TEXT",
+					"uesio/studio.label":      "Name",
+					"uesio/studio.collection": fieldCollection,
+					"uesio/studio.workspace": map[string]interface{}{
+						"uesio/core.id": workspaceID,
+					},
+				}
+
+				fieldChanges = append(fieldChanges, fieldItem)
+
+				namefield := idParts[0] + ".name"
+				collectionItem := adapt.Item{
+					"uesio/studio.namefield": namefield,
+					"uesio/core.id":          change.IDValue,
+				}
+
+				collectionChanges = append(collectionChanges, collectionItem)
+			}
+
+			return nil
+		})
 		if err != nil {
 			return err
 		}
 
-		name, err := change.GetFieldAsString("uesio/studio.name")
-		if err != nil {
-			return err
-		}
-
-		//Creates Default Name Field for collection
-		if change.IsNew {
-
-			//TO-DO Nice way of doing
-			idParts := strings.Split(workspaceID, "_")
-			fieldCollection := idParts[0] + "." + name
-			fieldItem := adapt.Item{
-				"uesio/studio.name":       "name",
-				"uesio/studio.type":       "TEXT",
-				"uesio/studio.label":      "Name",
-				"uesio/studio.collection": fieldCollection,
-				"uesio/studio.workspace": map[string]interface{}{
-					"uesio/core.id": workspaceID,
-				},
-			}
-
-			fieldChanges = append(fieldChanges, fieldItem)
-
-			namefield := idParts[0] + ".name"
-			collectionItem := adapt.Item{
-				"uesio/studio.namefield": namefield,
-				"uesio/core.id":          change.IDValue,
-			}
-
-			collectionChanges = append(collectionChanges, collectionItem)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
-	return SaveWithOptions([]SaveRequest{
-		{
-			Collection: "uesio/studio.field",
-			Wire:       "defaultnamefield",
-			Changes:    &fieldChanges,
-		},
-		{
-			Collection: "uesio/studio.collection",
-			Wire:       "defaultnamefieldcoll",
-			Changes:    &collectionChanges,
-		},
-	}, session, GetConnectionSaveOptions(connection))
+		return SaveWithOptions([]SaveRequest{
+			{
+				Collection: "uesio/studio.field",
+				Wire:       "defaultnamefield",
+				Changes:    &fieldChanges,
+			},
+			{
+				Collection: "uesio/studio.collection",
+				Wire:       "defaultnamefieldcoll",
+				Changes:    &collectionChanges,
+			},
+		}, session, GetConnectionSaveOptions(connection))
+	*/
 
 }
