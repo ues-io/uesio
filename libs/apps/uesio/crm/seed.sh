aaeaff300767c4a16f1960f56b8a4e9c1d7baddc
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2022-04-13 09:34:40 +0200

    CRM from plural to singular (#1112)

--- libs/apps/uesio/crm/seed.sh
+++ libs/apps/uesio/crm/seed.sh
@@ -1,2 +1,2 @@
-npm run uesio upsert -- -f data/accounts.csv -c uesio/crm.accounts -u uesio/crm.externalid
+npm run uesio upsert -- -f data/accounts.csv -c uesio/crm.account -u uesio/crm.externalid
 npm run uesio upsert -- -f data/contacts.csv -s data/contacts.spec.json
