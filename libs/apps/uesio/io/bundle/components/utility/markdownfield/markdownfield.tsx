b19af6f45f3550ca1bb3493216c958dcf8c545c3
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2022-04-20 23:10:08 +0200

    Markdown and CMS app V1 (#1113)
    * Markdown and CMS app V1
    
    * Fix npm
    
    * remove markdown field type
    
    * files bla bla
    
    * remove un used stuff
    
    * Update article_details.yaml
    
    * Fix components
    
    * last step
    
    * remove draft and switch to using comments
    
    * simplify
    
    * Fix new files
    
    Co-authored-by: humandad <55447225+humandad@users.noreply.github.com>

---
+++ libs/apps/uesio/io/bundle/components/utility/markdownfield/markdownfield.tsx
@@ -0,0 +1,73 @@
+import { ChangeEvent, FunctionComponent } from "react"
+import { definition, styles, context, collection } from "@uesio/ui"
+import ReactMarkdown from "react-markdown"
+import remarkGfm from "remark-gfm"
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter"
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism"
+
+import bash from "react-syntax-highlighter/dist/esm/languages/prism/bash"
+import yaml from "react-syntax-highlighter/dist/esm/languages/prism/yaml"
+
+SyntaxHighlighter.registerLanguage("bash", bash)
+SyntaxHighlighter.registerLanguage("yaml", yaml)
+
+interface MarkDownFieldProps extends definition.UtilityProps {
+	setValue: (value: string) => void
+	value: string | null
+	fieldMetadata: collection.Field
+	mode?: context.FieldMode
+}
+
+const MarkDownField: FunctionComponent<MarkDownFieldProps> = (props) => {
+	const { setValue, value, mode } = props
+	const readonly = mode === "READ"
+	const classes = styles.useUtilityStyles(
+		{
+			input: {
+				resize: "none",
+			},
+			readonly: {},
+		},
+		props
+	)
+
+	const commonProps = {
+		value: value || "",
+		className: styles.cx(classes.input, readonly && classes.readonly),
+		disabled: readonly,
+		onChange: (
+			event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+		) => setValue(event.target.value),
+	}
+
+	return readonly ? (
+		<ReactMarkdown
+			children={value || ""}
+			remarkPlugins={[remarkGfm]}
+			components={{
+				code({ node, inline, className, children, ...props }) {
+					const match = /language-(\w+)/.exec(className || "")
+					return !inline && match ? (
+						<SyntaxHighlighter
+							children={String(children).replace(/\n$/, "")}
+							style={materialDark}
+							language={match[1]}
+							PreTag="div"
+							{...props}
+						/>
+					) : (
+						<code className={className} {...props}>
+							{children}
+						</code>
+					)
+				},
+			}}
+		/>
+	) : (
+		<textarea {...commonProps} rows={40} cols={40} />
+	)
+}
+
+export { MarkDownFieldProps }
+
+export default MarkDownField
