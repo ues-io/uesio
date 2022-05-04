6062a923a4e25f156e7084064b2cc053a15b3887
Abel Jimenez Molla <abel@ues.io>
2022-05-04 16:23:58 +0200

    some steps

--- libs/apps/uesio/core/bundle/bots/generator/field/bot.js
+++ libs/apps/uesio/core/bundle/bots/generator/field/bot.js
@@ -1,4 +1,21 @@
 function field(bot) {
+	function help(template, parameters) {
+		var params = parameters.reduce((prev, key) => {
+			const value = bot.params.get(key)
+
+			return value
+				? {
+						...prev,
+						[key]: value,
+				  }
+				: prev
+		}, null)
+
+		if (!params) return null
+
+		return bot.mergeYamlTemplate(params, template)
+	}
+
 	var name = bot.params.get("name")
 	var collectionKey = bot.params.get("collection")
 	var type = bot.params.get("type")
@@ -15,17 +32,21 @@ function field(bot) {
 		"common.yaml"
 	)
 
-	//number
-	var number_definition = bot.params.get("number")
-		? bot.mergeYamlTemplate(
-				{
-					number: bot.params.get("number"),
-				},
-				"number.yaml"
-		  )
-		: null
+	var number_definition = help("number.yaml", ["number_decimals"])
+	var reference_definition = help("reference.yaml", ["ref_collection"])
+	var selectlist_definition = help("selectlist.yaml", ["select_list"])
+	var autonumber_definition = help("autonumber.yaml", [
+		"autonumber_prefix",
+		"autonumber_leadingzeros",
+	])
 
-	var field = [common_definition, number_definition]
+	var field = [
+		common_definition,
+		number_definition,
+		reference_definition,
+		selectlist_definition,
+		autonumber_definition,
+	]
 		.filter((val) => val)
 		.join("")
 
