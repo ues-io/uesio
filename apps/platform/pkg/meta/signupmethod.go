d4c8cc09768a16c9154cd2c6f0c542f716423d4c
Abel Jimenez Molla <abel@ues.io>
2022-05-05 10:37:45 +0200

    Signup Method

---
+++ apps/platform/pkg/meta/signupmethod.go
@@ -0,0 +1,115 @@
+package meta
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/humandad/yaml"
+)
+
+func NewSignupMethod(key string) (*SignupMethod, error) {
+	namespace, name, err := ParseKey(key)
+	if err != nil {
+		return nil, errors.New("Bad Key for SignupMethod: " + key)
+	}
+	return &SignupMethod{
+		Name:      name,
+		Namespace: namespace,
+	}, nil
+}
+
+type SignupMethod struct {
+	ID          string     `yaml:"-" uesio:"uesio/core.id"`
+	Name        string     `yaml:"name" uesio:"uesio/studio.name"`
+	Namespace   string     `yaml:"-" uesio:"-"`
+	Type        string     `yaml:"type" uesio:"uesio/studio.type"`
+	Credentials string     `yaml:"credentials" uesio:"uesio/studio.credentials"`
+	Workspace   *Workspace `yaml:"-" uesio:"uesio/studio.workspace"`
+	itemMeta    *ItemMeta  `yaml:"-" uesio:"-"`
+	CreatedBy   *User      `yaml:"-" uesio:"uesio/core.createdby"`
+	Owner       *User      `yaml:"-" uesio:"uesio/core.owner"`
+	UpdatedBy   *User      `yaml:"-" uesio:"uesio/core.updatedby"`
+	UpdatedAt   int64      `yaml:"-" uesio:"uesio/core.updatedat"`
+	CreatedAt   int64      `yaml:"-" uesio:"uesio/core.createdat"`
+	Public      bool       `yaml:"public,omitempty" uesio:"uesio/studio.public"`
+}
+
+func (sm *SignupMethod) GetCollectionName() string {
+	return sm.GetBundleGroup().GetName()
+}
+
+func (sm *SignupMethod) GetCollection() CollectionableGroup {
+	var smc SignupMethodCollection
+	return &smc
+}
+
+func (sm *SignupMethod) GetDBID(workspace string) string {
+	return fmt.Sprintf("%s_%s", workspace, sm.Name)
+}
+
+func (sm *SignupMethod) GetBundleGroup() BundleableGroup {
+	var smc SignupMethodCollection
+	return &smc
+}
+
+func (sm *SignupMethod) GetKey() string {
+	return fmt.Sprintf("%s.%s", sm.Namespace, sm.Name)
+}
+
+func (sm *SignupMethod) GetPath() string {
+	return sm.Name + ".yaml"
+}
+
+func (sm *SignupMethod) GetPermChecker() *PermissionSet {
+	return nil
+}
+
+func (sm *SignupMethod) SetField(fieldName string, value interface{}) error {
+	return StandardFieldSet(sm, fieldName, value)
+}
+
+func (sm *SignupMethod) GetField(fieldName string) (interface{}, error) {
+	return StandardFieldGet(sm, fieldName)
+}
+
+func (sm *SignupMethod) GetNamespace() string {
+	return sm.Namespace
+}
+
+func (sm *SignupMethod) SetNamespace(namespace string) {
+	sm.Namespace = namespace
+}
+
+func (sm *SignupMethod) SetWorkspace(workspace string) {
+	sm.Workspace = &Workspace{
+		ID: workspace,
+	}
+}
+
+func (sm *SignupMethod) Loop(iter func(string, interface{}) error) error {
+	return StandardItemLoop(sm, iter)
+}
+
+func (sm *SignupMethod) Len() int {
+	return StandardItemLen(sm)
+}
+
+func (sm *SignupMethod) GetItemMeta() *ItemMeta {
+	return sm.itemMeta
+}
+
+func (sm *SignupMethod) SetItemMeta(itemMeta *ItemMeta) {
+	sm.itemMeta = itemMeta
+}
+
+func (sm *SignupMethod) UnmarshalYAML(node *yaml.Node) error {
+	err := validateNodeName(node, sm.Name)
+	if err != nil {
+		return err
+	}
+	return node.Decode(sm)
+}
+
+func (sm *SignupMethod) IsPublic() bool {
+	return sm.Public
+}
