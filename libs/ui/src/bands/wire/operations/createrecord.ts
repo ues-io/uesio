27bb754c2a34ab06b2d021782046859987dcf398
Wessel <wesselvdp@gmail.com>
2022-06-15 13:28:05 +0200

    Update createrecord.ts

--- libs/ui/src/bands/wire/operations/createrecord.ts
+++ libs/ui/src/bands/wire/operations/createrecord.ts
@@ -12,9 +12,7 @@ export default (
 	(dispatch, getState) => {
 		const viewId = context.getViewId()
 		if (!viewId) return context
-		console.log("1")
 		const viewDef = context.getViewDef()
-		console.log("2")
 		if (!viewDef) return context
 
 		const recordId = nanoid()
@@ -22,27 +20,22 @@ export default (
 		const wireId = getFullWireId(viewId, wirename)
 		const wire = state.wire.entities[wireId]
 		if (!wire) return context
-		const record = getDefaultRecord(
-			context,
-			state.wire.entities,
-			state.collection.entities,
-			viewId,
-			wire.def,
-			wire.collection
-		)
-		console.log("3", record)
+
 		dispatch(
 			createRecord({
 				recordId,
-				record,
+				record: getDefaultRecord(
+					context,
+					state.wire.entities,
+					state.collection.entities,
+					viewId,
+					wire.def,
+					wire.collection
+				),
 				entity: `${viewId}:${wirename}`,
 				prepend: !!prepend,
 			})
 		)
-		console.log("4", {
-			record: recordId,
-			wire: wirename,
-		})
 		return context.addFrame({
 			record: recordId,
 			wire: wirename,
