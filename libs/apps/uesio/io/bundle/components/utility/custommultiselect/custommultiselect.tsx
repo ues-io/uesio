b618e71272228c42b86f4dbc7e5f32726bab1284
Abel Jimenez Molla <abel@ues.io>
2022-06-28 13:19:50 +0200

    Multi Select

---
+++ libs/apps/uesio/io/bundle/components/utility/custommultiselect/custommultiselect.tsx
@@ -0,0 +1,217 @@
+import { useState, FunctionComponent, ReactNode, MouseEvent } from "react"
+import { useCombobox, useMultipleSelection, useSelect } from "downshift"
+import { definition, styles, component } from "@uesio/ui"
+import { usePopper } from "react-popper"
+import { IconUtilityProps } from "../icon/icon"
+
+type CustomMultiSelectProps<T> = {
+	value: T
+	setValue: (value: T[]) => void
+	litems: T[]
+	itemToString: (item: T) => string
+	itemRenderer: (
+		item: T,
+		index: number,
+		highlightedIndex: number
+	) => ReactNode
+	tagRenderer: (item: T) => ReactNode
+} & definition.BaseProps
+
+const Icon = component.getUtility<IconUtilityProps>("uesio/io.icon")
+
+const CustomMultiSelect: FunctionComponent<CustomMultiSelectProps<unknown>> = (
+	props
+) => {
+	const {
+		value,
+		litems,
+		setValue,
+		itemToString,
+		itemRenderer = (item) => <div>{itemToString(item)}</div>,
+		tagRenderer = (item) => <div>{itemToString(item)}</div>,
+		context,
+	} = props
+
+	const classes = styles.useUtilityStyles(
+		{
+			root: {},
+			input: {},
+			readonly: {},
+			menu: {
+				backgroundColor: "white",
+				fontSize: "10pt",
+				border: "1px solid #ccc",
+				borderRadius: "4px",
+				overflow: "hidden",
+				boxShadow: "0 0 4px #00000033",
+			},
+			menuitem: {
+				cursor: "pointer",
+			},
+			editbutton: {
+				color: "#444",
+				border: "none",
+				outline: "none",
+				padding: "6px 10px 6px 0",
+				backgroundColor: "transparent",
+				fontSize: "initial",
+				cursor: "pointer",
+			},
+			selecteditem: {
+				display: "inline-block",
+				// border: "0.5px solid black",
+				backgroundColor: "#f5f5f5",
+				padding: "6px 10px",
+				marginRight: "8px",
+				borderRadius: "5px",
+				verticalAlign: "middle",
+			},
+		},
+		props
+	)
+
+	const {
+		getDropdownProps,
+		addSelectedItem,
+		removeSelectedItem,
+		selectedItems,
+	} = useMultipleSelection({ initialSelectedItems: value as string[] })
+
+	function getFilter(selectedItems: string[]) {
+		return function filter(elem: string) {
+			return selectedItems.indexOf(elem) < 0
+		}
+	}
+
+	const items = litems.filter(getFilter(selectedItems))
+
+	const {
+		isOpen,
+		getMenuProps,
+		getToggleButtonProps,
+		highlightedIndex,
+		getItemProps,
+		inputValue,
+	} = useSelect({
+		itemToString,
+		items,
+		selectedItem: null,
+		stateReducer: (state, actionAndChanges) => {
+			const { changes, type } = actionAndChanges
+			switch (type) {
+				case useSelect.stateChangeTypes.MenuKeyDownEnter:
+				case useSelect.stateChangeTypes.MenuKeyDownSpaceButton:
+				case useSelect.stateChangeTypes.ItemClick:
+					return {
+						...changes,
+						isOpen: true, // keep the menu open after selection.
+					}
+			}
+			return changes
+		},
+		onStateChange: ({ type, selectedItem }) => {
+			switch (type) {
+				case useSelect.stateChangeTypes.MenuKeyDownEnter:
+				case useSelect.stateChangeTypes.MenuKeyDownSpaceButton:
+				case useSelect.stateChangeTypes.ItemClick:
+					if (selectedItem) {
+						addSelectedItem(selectedItem as string) //TO-DO
+					}
+					break
+				default:
+					break
+			}
+		},
+		onSelectedItemChange: (changes) => {
+			const selectedItem = changes.selectedItem as string
+			const lcopy = [...selectedItems]
+			lcopy.push(selectedItem)
+			setValue(lcopy)
+		},
+	})
+
+	const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null)
+	const [popperEl, setPopperEl] = useState<HTMLDivElement | null>(null)
+	const popper = usePopper(anchorEl, popperEl, {
+		placement: "bottom-start",
+	})
+
+	return (
+		<div ref={setAnchorEl}>
+			<div>
+				<div>
+					{selectedItems.map(function renderSelectedItem(
+						selectedItem,
+						index
+					) {
+						return (
+							<div
+								className={classes.selecteditem}
+								key={`${selectedItem}-${index}`}
+							>
+								{tagRenderer(selectedItem)}
+								<button
+									className={classes.editbutton}
+									type="button"
+									onClick={(e) => {
+										e.stopPropagation()
+										removeSelectedItem(selectedItem)
+										setValue(
+											selectedItems.filter(
+												(elem) => elem === selectedItem
+											)
+										)
+									}}
+								>
+									<Icon icon="close" context={context} />
+								</button>
+							</div>
+						)
+					})}
+					<button
+						className={classes.editbutton}
+						type="button"
+						{...getToggleButtonProps(
+							getDropdownProps({ preventKeyAction: isOpen })
+						)}
+					>
+						<Icon icon="expand_more" context={context} />
+					</button>
+				</div>
+			</div>
+			<div
+				ref={setPopperEl}
+				style={{
+					...popper.styles.popper,
+					zIndex: "1",
+					...(!isOpen && { visibility: "hidden" }),
+				}}
+				{...popper.attributes.popper}
+				className={classes.menu}
+			>
+				<div {...getMenuProps()}>
+					{isOpen &&
+						items
+							.filter((item) =>
+								itemToString(item).includes(inputValue)
+							)
+							.map((item, index) => (
+								<div
+									className={classes.menuitem}
+									key={index}
+									{...getItemProps({ item, index })}
+								>
+									{itemRenderer(
+										item,
+										index,
+										highlightedIndex
+									)}
+								</div>
+							))}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CustomMultiSelect
---
+++ libs/apps/uesio/studio/bundle/components/utility/multimetadatapicker/multimetadatapicker.tsx
@@ -0,0 +1,160 @@
+import { FunctionComponent } from "react"
+import { definition, component, hooks, metadata, styles } from "@uesio/ui"
+
+interface MultiMetadataPickerProps extends definition.UtilityProps {
+	value: string[]
+	setValue: (value: string[]) => void
+	metadataType: metadata.MetadataType
+	label: string
+	labelPosition?: string
+	grouping?: string
+	defaultNamespace?: string
+	selectVariant?: string
+	fieldWrapperVariant?: string
+}
+
+const CustomMultiSelect = component.getUtility("uesio/io.custommultiselect")
+const FieldWrapper = component.getUtility("uesio/io.fieldwrapper")
+
+const MultiMetadataPicker: FunctionComponent<MultiMetadataPickerProps> = (
+	props
+) => {
+	const {
+		value,
+		setValue,
+		label,
+		labelPosition,
+		metadataType,
+		context,
+		grouping,
+		fieldWrapperVariant,
+	} = props
+	const uesio = hooks.useUesio(props)
+
+	if (!context.getWorkspace() && !context.getSiteAdmin()) {
+		throw new Error("Must provide either siteadmin or workspace context")
+	}
+
+	const classes = styles.useUtilityStyles(
+		{
+			itemwrapper: {
+				padding: "6px",
+				color: "white",
+				display: "inline-block",
+			},
+			notfound: {
+				textTransform: "capitalize",
+				color: "#999",
+			},
+			namespacetag: {
+				display: "inline-block",
+				backgroundColor: "#ddd",
+				padding: "6px 10px",
+				marginRight: "8px",
+				borderRadius: "14px",
+				fontSize: "8pt",
+				verticalAlign: "middle",
+			},
+			highlighteditem: {
+				backgroundColor: "#eee",
+			},
+			nametag: {
+				display: "inline-block",
+				fontSize: "9pt",
+				verticalAlign: "middle",
+				color: "#333",
+			},
+		},
+		props
+	)
+
+	const metadata = uesio.builder.useMetadataList(
+		context,
+		metadataType,
+		"",
+		grouping
+	)
+
+	const items: string[] = metadata
+		? Object.keys(metadata).map((key) => key)
+		: []
+
+	const itemToString = (item: string) => (item ? item : "")
+
+	const tag = (ns: string, name: string, background: string) => (
+		<>
+			<div
+				className={classes.namespacetag}
+				style={{
+					...(background && { backgroundColor: background }),
+				}}
+			>
+				{ns}
+			</div>
+			<div className={classes.nametag}>{name}</div>
+		</>
+	)
+
+	const renderer = (item: string, highlighted: boolean) => {
+		if (!item)
+			return (
+				<div
+					className={styles.cx(classes.itemwrapper, classes.notfound)}
+				>
+					{tag(
+						`No ${metadataType.toLowerCase()} Selected`,
+						"",
+						"#eee"
+					)}
+				</div>
+			)
+		const [ns, name] = component.path.parseKey(item)
+		const metadataInfo = metadata?.[item]
+
+		return (
+			<div
+				className={styles.cx(
+					classes.itemwrapper,
+					highlighted && classes.highlighteditem
+				)}
+			>
+				{tag(ns, name, metadataInfo?.color || "#eee")}
+			</div>
+		)
+	}
+
+	return (
+		<FieldWrapper
+			labelPosition={labelPosition}
+			variant={fieldWrapperVariant}
+			label={label}
+			context={context}
+		>
+			<CustomMultiSelect
+				litems={items}
+				value={value ? value : undefined}
+				itemToString={itemToString}
+				itemRenderer={(
+					item: string,
+					index: number,
+					highlightedIndex: number
+				) => {
+					if (!item) {
+						return null
+					}
+					const highlighted = index === highlightedIndex
+					return renderer(item, highlighted)
+				}}
+				tagRenderer={renderer}
+				context={context}
+				setValue={(item: string[]) => {
+					setValue(item ? item : [])
+				}}
+			/>
+		</FieldWrapper>
+	)
+}
+
+MultiMetadataPicker.displayName = "MultiMetadataPicker"
+
+export default MultiMetadataPicker
