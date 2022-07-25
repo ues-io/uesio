0df7d3cf28ad647e6e33eab82274d41b2e7f8ad8
Abel Jimenez Molla <abel@ues.io>
2022-07-25 10:01:08 +0200

    Signal errors proposal

--- libs/ui/src/context/context.ts
+++ libs/ui/src/context/context.ts
@@ -400,6 +400,23 @@ class Context {
 			: {}
 
 	getErrors = () => this.stack.find((frame) => frame?.errors)?.errors
+	addError = (error: string) => {
+		const itemIndex = this.stack.findIndex((frame) => frame?.errors)
+		if (itemIndex >= 0) this.stack[itemIndex].errors?.push(error)
+		return this.addFrame({ errors: [error] })
+	}
+	removeError = (error: string) => {
+		console.log("Removing", error)
+
+		const itemIndex = this.stack.findIndex((frame) => frame?.errors)
+		if (itemIndex >= 0) {
+			this.stack[itemIndex].errors = this.stack[itemIndex].errors?.filter(
+				(item) => item !== error
+			)
+		}
+
+		return this
+	}
 
 	getViewStack = () =>
 		this.stack
--- libs/ui/src/signals/signals.ts
+++ libs/ui/src/signals/signals.ts
@@ -11,7 +11,7 @@ import panelSignals from "../bands/panel/signals"
 import notificationSignals from "../bands/notification/signals"
 import { additionalContext } from "../component/component"
 import debounce from "lodash/debounce"
-import { getErrorString } from "../bands/utils"
+import { getErrorString } from "../utilexports"
 
 const registry: Record<string, SignalDescriptor> = {
 	...botSignals,
@@ -38,16 +38,32 @@ const run = (signal: SignalDefinition, context: Context) => {
 const runMany = async (signals: SignalDefinition[], context: Context) => {
 	for (const signal of signals) {
 		try {
-			// Keep adding to context as each signal is run
+			const prevError = context.getErrors()
 			context = await run(signal, context)
-		} catch (error) {
-			const message = getErrorString(error)
-			if (signal.onerror?.signals) {
-				runMany(
-					signal.onerror.signals,
-					context.addFrame({ errors: [message] })
-				)
+
+			if (prevError) {
+				context = context.removeError(prevError[0])
 			}
+			const myselfErrors = context.getErrors()
+
+			if (myselfErrors && myselfErrors.length > 0) {
+				if (signal.onerror?.signals) {
+					await runMany(signal.onerror.signals, context)
+				} else {
+					await runMany(
+						[{ signal: "notification/ADD_ERRORS" }],
+						context
+					)
+				}
+				break
+			}
+		} catch (error) {
+			//The specific operation does not have a try catch.
+			const message = getErrorString(error)
+			await runMany(
+				[{ signal: "notification/ADD_ERRORS" }],
+				context.addFrame({ errors: [message] })
+			)
 			break
 		}
 	}
