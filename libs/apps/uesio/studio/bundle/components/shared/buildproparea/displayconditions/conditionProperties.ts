20ec0df2cb4d1b23763f60de940459d41645fb38
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2022-07-28 23:52:50 +0200

    Display condition properties (#1370)
    * WIP Display condition properties
    
    * add new param property type
    
    * Update popper.tsx
    
    Co-authored-by: humandad <55447225+humandad@users.noreply.github.com>

--- libs/apps/uesio/studio/bundle/components/shared/buildproparea/displayconditions/conditionProperties.ts
+++ libs/apps/uesio/studio/bundle/components/shared/buildproparea/displayconditions/conditionProperties.ts
@@ -11,16 +11,47 @@ export const properties: builder.PropDescriptor[] = [
 				value: "",
 			},
 			{
-				label: "Param is set",
-				value: "paramIsSet",
-			},
-			{
 				label: "Field equals",
 				value: "fieldValue",
 			},
 			{
+				label: "Param is set",
+				value: "paramIsSet",
+			},
+			{
 				label: "Param equals",
 				value: "paramValue",
+			},
+			{
+				label: "Has No Value",
+				value: "hasNoValue",
+			},
+			{
+				label: "Has Value",
+				value: "hasValue",
+			},
+			{
+				label: "Collection Context",
+				value: "collectionContext",
+			},
+			{
+				label: "Feature Flag",
+				value: "featureFlag",
+			},
+			{
+				label: "Field Mode",
+				value: "fieldMode",
+			},
+		],
+	},
+	{
+		name: "wire",
+		type: "WIRE",
+		label: "wire",
+		display: [
+			{
+				property: "type",
+				value: "fieldValue",
 			},
 		],
 	},
@@ -34,16 +65,43 @@ export const properties: builder.PropDescriptor[] = [
 		display: [
 			{
 				property: "type",
-				values: ["fieldValue"],
+				value: "fieldValue",
+			},
+		],
+	},
+	{
+		name: "operator",
+		type: "SELECT",
+		label: "Operator",
+		options: [
+			{
+				label: "Select an option",
+				value: "",
+			},
+			{
+				label: "EQUALS",
+				value: "EQUALS",
+			},
+			{
+				label: "NOT_EQUALS",
+				value: "NOT_EQUALS",
+			},
+		],
+		display: [
+			{
+				type: "INCLUDES",
+				property: "type",
+				values: ["fieldValue", "paramValue"],
 			},
 		],
 	},
 	{
 		name: "param",
-		type: "TEXT",
+		type: "PARAM",
 		label: "Param",
 		display: [
 			{
+				type: "INCLUDES",
 				property: "type",
 				values: ["paramValue", "paramIsSet"],
 			},
@@ -55,8 +113,57 @@ export const properties: builder.PropDescriptor[] = [
 		label: "Value",
 		display: [
 			{
+				type: "INCLUDES",
 				property: "type",
-				values: ["paramValue", "fieldValue"],
+				values: ["paramValue", "fieldValue", "hasNoValue", "hasValue"],
+			},
+		],
+	},
+	{
+		type: "METADATA",
+		name: "collection",
+		metadataType: "COLLECTION",
+		label: "Collection",
+		display: [
+			{
+				property: "type",
+				value: "collectionContext",
+			},
+		],
+	},
+	{
+		name: "name",
+		type: "TEXT", //TO-DO FFlag type
+		label: "Name",
+		display: [
+			{
+				property: "type",
+				value: "featureFlag",
+			},
+		],
+	},
+	{
+		name: "mode",
+		type: "SELECT",
+		label: "Mode",
+		options: [
+			{
+				label: "Select an option",
+				value: "",
+			},
+			{
+				label: "READ",
+				value: "READ",
+			},
+			{
+				label: "EDIT",
+				value: "EDIT",
+			},
+		],
+		display: [
+			{
+				property: "type",
+				value: "fieldMode",
 			},
 		],
 	},
