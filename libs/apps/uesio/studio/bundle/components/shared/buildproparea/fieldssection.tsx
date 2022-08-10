af6a9c76f26788effdee6acde866d82870c59cba
Wessel <wesselvdp@gmail.com>
2022-08-10 11:15:53 +0200

    Only selected fields in the fields section

--- libs/apps/uesio/studio/bundle/components/shared/buildproparea/fieldssection.tsx
+++ libs/apps/uesio/studio/bundle/components/shared/buildproparea/fieldssection.tsx
@@ -1,38 +1,16 @@
-import React, {
-	FunctionComponent,
-	DragEvent,
-	useState,
-	SyntheticEvent,
-	useEffect,
-	ChangeEvent,
-} from "react"
+import { FC, DragEvent, useState, SyntheticEvent, ChangeEvent } from "react"
 import { SectionRendererProps } from "./sectionrendererdefinition"
 import { hooks, component, definition } from "@uesio/ui"
-import PropNodeTag from "../buildpropitem/propnodetag"
+import PropNodeTag from "../buildpropitem/propnodetagnew"
 
 const TitleBar = component.getUtility("uesio/io.titlebar")
 
-const useExpand = (
-	initialExpanded?: boolean,
-	onExpand?: () => void,
-	onClose?: () => void
-): [boolean, React.Dispatch<React.SetStateAction<boolean>>] => {
-	const [isExpanded, setIsExpanded] = useState(!!initialExpanded)
+type FieldProp = { fieldId: string; fields: FieldProp[] }
+type FieldDef = null | { fields: FieldDef }
 
-	useEffect(() => {
-		if (isExpanded) {
-			console.log("expanded")
-		}
-		if (!isExpanded) {
-			console.log("folded")
-		}
-	}, [isExpanded])
-
-	console.log({ isExpanded })
-	return [isExpanded, setIsExpanded]
-}
-
-const FieldsSection: FunctionComponent<SectionRendererProps> = (props) => {
+const Button = component.getUtility("uesio/io.button")
+const Icon = component.getUtility("uesio/io.icon")
+const FieldsSection: FC<SectionRendererProps> = (props) => {
 	const { path, context, valueAPI } = props
 	const wireDef = valueAPI.get(path) as definition.DefinitionMap | undefined
 	const collectionKey = wireDef?.collection as string | undefined
@@ -82,6 +60,34 @@ const FieldsSection: FunctionComponent<SectionRendererProps> = (props) => {
 				field.toLowerCase().includes(searchTerm.toLocaleLowerCase())
 		  )
 
+	const handleField = ([key, value]: [string, FieldDef]): FieldProp => ({
+		fieldId: key,
+		fields:
+			value && value.fields
+				? Object.entries(value.fields).map((el) => handleField(el))
+				: [],
+	})
+
+	const formattedFields = Object.entries(wireDef?.fields || {}).map((el) =>
+		handleField(el)
+	)
+
+	const FieldRenderer: FC<FieldProp> = ({ fieldId, fields }) => (
+		<PropNodeTag
+			draggable={`${collectionKey}:${fieldId}`}
+			key={fieldId}
+			context={context}
+		>
+			<p>{fieldId}</p>
+			{fields?.map((el) => (
+				<FieldRenderer
+					key={fieldId}
+					fieldId={el.fieldId}
+					fields={el.fields}
+				/>
+			))}
+		</PropNodeTag>
+	)
 	return (
 		<>
 			<TitleBar
@@ -89,60 +95,59 @@ const FieldsSection: FunctionComponent<SectionRendererProps> = (props) => {
 				title={""}
 				context={context}
 				actions={
-					<input
-						value={searchTerm}
-						style={{
-							outline: "none",
-							padding: "4px",
-							fontSize: "9pt",
-							border: "none",
-							background: "#eee",
-							borderRadius: "4px",
-						}}
-						onChange={(event: ChangeEvent<HTMLInputElement>) => {
-							handleChange(event.target.value)
-						}}
-						onClick={(event: SyntheticEvent): void => {
-							event.stopPropagation()
-						}}
-						type="search"
-						placeholder="Search..."
-					/>
+					<>
+						<input
+							value={searchTerm}
+							style={{
+								outline: "none",
+								padding: "4px",
+								fontSize: "9pt",
+								border: "none",
+								background: "#eee",
+								borderRadius: "4px",
+							}}
+							onChange={(
+								event: ChangeEvent<HTMLInputElement>
+							) => {
+								handleChange(event.target.value)
+							}}
+							onClick={(event: SyntheticEvent): void => {
+								event.stopPropagation()
+							}}
+							type="search"
+							placeholder="Search..."
+						/>
+						<Button
+							context={context}
+							variant="uesio/studio.actionbutton"
+							icon={
+								<Icon
+									context={context}
+									icon="library_add"
+									variant="uesio/studio.actionicon"
+								/>
+							}
+							label="Add fields"
+							onClick={() => null}
+						/>
+					</>
 				}
 			/>
 			<div onDragStart={onDragStart} onDragEnd={onDragEnd}>
 				{collectionKey &&
-					results &&
-					results.map((fieldId, index) => {
-						const fieldDef = fieldsDef?.[fieldId]
-						const selected = fieldDef !== undefined
-						const onClick = (): void => {
-							const setPath = `${path}["fields"]["${fieldId}"]`
-							selected
-								? valueAPI.remove(setPath)
-								: valueAPI.set(setPath, null)
-						}
-						return (
-							<PropNodeTag
-								draggable={`${collectionKey}:${fieldId}`}
-								title={fieldId}
-								icon={
-									selected
-										? "check_box"
-										: "check_box_outline_blank"
-								}
-								iconColor={
-									selected
-										? theme.definition.palette.primary
-										: undefined
-								}
-								key={index}
-								onClick={onClick}
-								selected={selected}
-								context={context}
-							/>
-						)
-					})}
+					formattedFields.map(({ fieldId, fields }) => (
+						// const onClick = (): void => {
+						// 	const setPath = `${path}["fields"]["${fieldId}"]`
+						// 	selected
+						// 		? valueAPI.remove(setPath)
+						// 		: valueAPI.set(setPath, null)
+						// }
+						<FieldRenderer
+							key={fieldId}
+							fieldId={fieldId}
+							fields={fields as any}
+						/>
+					))}
 			</div>
 		</>
 	)
