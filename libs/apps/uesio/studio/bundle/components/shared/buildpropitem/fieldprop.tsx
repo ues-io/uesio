a09cd1281f3a8c3260fd16921a543517c7c924d6
Wessel <wesselvdp@gmail.com>
2022-08-23 13:09:46 +0200

    Update fieldprop.tsx

--- libs/apps/uesio/studio/bundle/components/shared/buildpropitem/fieldprop.tsx
+++ libs/apps/uesio/studio/bundle/components/shared/buildpropitem/fieldprop.tsx
@@ -1,17 +1,26 @@
 import { FC } from "react"
-import { builder, component, wire, util } from "@uesio/ui"
+import { builder, component, wire, util, hooks } from "@uesio/ui"
 import SelectProp from "./selectprop"
 
 const FieldsProp: FC<builder.PropRendererProps> = (props) => {
 	const { valueAPI, path } = props
 	const descriptor = props.descriptor as builder.FieldProp
+
 	const parentPath = component.path.getParentPath(path || "")
 	const wirePath = parentPath + `["${descriptor.wireField}"]`
+
 	const wireId = valueAPI.get(wirePath)
 
 	const wireDef = (valueAPI.get(
 		'["wires"][' + valueAPI.get(wirePath) + "]"
 	) || {}) as wire.WireDefinition
+
+	const options = util.getWireFieldSelectOptions(wireDef) || []
+	const getOptionsLabel = () => {
+		if (!wireId) return "Select a wire first"
+		if (!options.length) return `No fields in ${wireId}`
+		return "Select a field"
+	}
 
 	return (
 		<SelectProp
@@ -21,12 +30,10 @@ const FieldsProp: FC<builder.PropRendererProps> = (props) => {
 				type: "SELECT",
 				options: [
 					{
-						label: wireId
-							? "Select a field"
-							: "Select a wire first",
+						label: getOptionsLabel(),
 						value: "",
 					},
-					...(util.getWireFieldSelectOptions(wireDef) || []),
+					...options,
 				],
 			}}
 		/>
