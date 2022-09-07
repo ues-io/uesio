19191419f7caa938c7da38a55e0c36328e291753
Abel Jimenez Molla <abel@ues.io>
2022-09-07 15:41:26 +0200

    more usage stuff

--- apps/platform/pkg/cmd/worker.go
+++ apps/platform/pkg/cmd/worker.go
@@ -23,7 +23,11 @@ func worker(cmd *cobra.Command, args []string) {
 	logger.Log("Running uesio worker", logger.INFO)
 
 	for {
-		usage.RunJob()
+		err := usage.RunJob()
+		if err != nil {
+			logger.Log("Job failed reason: "+err.Error(), logger.ERROR)
+		}
+		logger.Log("Job completed without any issues", logger.INFO)
 		time.Sleep(5 * time.Second)
 	}
 
--- apps/platform/pkg/usage/usage.go
+++ apps/platform/pkg/usage/usage.go
@@ -12,11 +12,36 @@ import (
 	"github.com/thecloudmasters/uesio/pkg/cache"
 	"github.com/thecloudmasters/uesio/pkg/datasource"
 	"github.com/thecloudmasters/uesio/pkg/logger"
+	"github.com/thecloudmasters/uesio/pkg/meta"
 )
+
+func getUser(userid string) (*meta.User, error) {
+
+	if userid == "GUEST" {
+		return &meta.User{
+			FirstName: "Guest",
+			LastName:  "User",
+			UniqueKey: "guest",
+			Username:  "guest",
+		}, nil
+	}
+
+	session, err := auth.GetStudioAdminSession()
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := auth.GetUserByID(userid, session, nil)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+
+}
 
 func RunJob() error {
 
-	logger.Log("RunJob", logger.INFO)
+	logger.Log("Job Running", logger.INFO)
 
 	conn := cache.GetRedisConn()
 	defer conn.Close()
@@ -37,26 +62,21 @@ func RunJob() error {
 		return fmt.Errorf("Error Getting Usage Event: " + err.Error())
 	}
 
-	_, err = conn.Do("DEL", keyArgs...)
-	if err != nil {
-		return fmt.Errorf("Error Getting Usage Event: " + err.Error())
-	}
-
-	_, err = conn.Do("DEL", "USAGE_KEYS")
-	if err != nil {
-		return fmt.Errorf("Error Getting Usage Event: " + err.Error())
-	}
-
 	changes := adapt.Collection{}
 	for i, key := range keys {
 		keyParts := strings.Split(key, ":")
 		if len(keyParts) != 9 {
 			return fmt.Errorf("Error Getting Usage Event: " + err.Error())
 		}
+
+		user, err := getUser(keyParts[4])
+		if err != nil {
+			return err
+		}
 		usageItem := adapt.Item{}
+		usageItem.SetField("uesio/core.user", user)
 		usageItem.SetField("uesio/core.tenanttype", keyParts[1])
 		usageItem.SetField("uesio/core.tenantid", keyParts[2]+":"+keyParts[3])
-		usageItem.SetField("uesio/core.user", keyParts[4])
 		usageItem.SetField("uesio/core.day", keyParts[5])
 		usageItem.SetField("uesio/core.actiontype", keyParts[6])
 		usageItem.SetField("uesio/core.metadatatype", keyParts[7])
@@ -91,7 +111,17 @@ func RunJob() error {
 		if err != nil {
 			return errors.New("Failed to update usage events: " + err.Error())
 		}
-	}
 
+		_, err = conn.Do("DEL", keyArgs...)
+		if err != nil {
+			return fmt.Errorf("Error Getting Usage Event: " + err.Error())
+		}
+
+		_, err = conn.Do("DEL", "USAGE_KEYS")
+		if err != nil {
+			return fmt.Errorf("Error Getting Usage Event: " + err.Error())
+		}
+
+	}
 	return nil
 }
