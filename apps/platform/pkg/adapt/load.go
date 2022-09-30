3b053beb4e0f7576ad12b03ae0f4e07446363382
humandad <55447225+humandad@users.noreply.github.com>
2022-09-30 15:40:44 -0400

    preloading oops

--- apps/platform/pkg/adapt/load.go
+++ apps/platform/pkg/adapt/load.go
@@ -57,7 +57,6 @@ func (op *LoadOp) UnmarshalJSONObject(dec *gojay.Decoder, key string) error {
 	case "collection":
 		// Do some extra stuff
 		op.Collection = &Collection{}
-		op.Preloaded = true
 		return dec.String(&op.CollectionName)
 	case "name":
 		return dec.String(&op.WireName)
@@ -111,7 +110,6 @@ func (op *LoadOp) UnmarshalYAML(node *yaml.Node) error {
 	op.Fields = fields
 	op.Conditions = conditions
 	op.Order = order
-	op.Preloaded = true
 	return nil
 
 }
