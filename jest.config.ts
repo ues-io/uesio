909ebcb9e804097ec7c8e7b5e38f95b9a4e8e876
Wessel <wesselvdp@gmail.com>
2022-10-04 14:48:10 +0200

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,4 @@ const { getJestProjects } = require("@nrwl/jest")
 
 export default {
 	projects: [...getJestProjects()],
-	setupFiles: ["whatwg-fetch"],
-	setupFilesAfterEnv: [".<rootDir>/setup-jest.js"],
 }
