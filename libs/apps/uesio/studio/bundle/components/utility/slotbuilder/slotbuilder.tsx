5740f5f35647edb6131a432e831e6bb769a25e74
Wessel <wesselvdp@gmail.com>
2022-10-04 12:38:39 +0200

    Update slotbuilder.tsx

--- libs/apps/uesio/studio/bundle/components/utility/slotbuilder/slotbuilder.tsx
+++ libs/apps/uesio/studio/bundle/components/utility/slotbuilder/slotbuilder.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent, useEffect, useRef } from "react"
 import { isDropAllowed } from "../../shared/dragdrop"
 
 const SlotBuilder: FunctionComponent<component.SlotUtilityProps> = (props) => {
-	const { accepts, definition, listName, path, direction } = props
+	const { accepts, definition, listName, path, direction, label } = props
 	const ref = useRef<HTMLDivElement>(null)
 	const uesio = hooks.useUesio(props)
 	const listDef = (definition?.[listName] || []) as definition.DefinitionList
@@ -18,15 +18,27 @@ const SlotBuilder: FunctionComponent<component.SlotUtilityProps> = (props) => {
 		dragPath
 	)
 
+	const isHovering = dropPath === `${listPath}["0"]`
 	const classes = styles.useStyles(
 		{
+			label: {
+				opacity: 0,
+				fontSize: "0.7em",
+				transition: "all 0.125s ease",
+				...(isHovering && {
+					opacity: 0.5,
+				}),
+			},
 			placeholder: {
-				...(dropPath === `${listPath}["0"]` && {
+				border: "1px solid rgba(0, 0, 0, 0)",
+				transition: "all 0.125s ease",
+				paddingLeft: "5px",
+				minWidth: "40px",
+				minHeight: "40px",
+				...(isHovering && {
 					border: "1px dashed #ccc",
 					backgroundColor: "#e5e5e5",
 				}),
-				minWidth: "40px",
-				minHeight: "40px",
 			},
 		},
 		props
@@ -40,13 +52,15 @@ const SlotBuilder: FunctionComponent<component.SlotUtilityProps> = (props) => {
 			parentElem.setAttribute("data-path", listPath)
 			parentElem.setAttribute("data-insertindex", size + "")
 		}
-	}, [])
+	}, [path])
 
 	return (
 		<>
 			<div ref={ref} style={{ display: "contents" }} />
 			{size === 0 && isDropAllowed(accepts, fullDragPath) && (
-				<div className={classes.placeholder} />
+				<div className={classes.placeholder}>
+					{label && <span className={classes.label}>{label}</span>}
+				</div>
 			)}
 			{props.children}
 		</>
