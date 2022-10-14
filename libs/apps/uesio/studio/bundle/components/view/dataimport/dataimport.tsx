46e27c7cb2089f59cc15ff2e22481e3b6ce6989b
Wessel <wesselvdp@gmail.com>
2022-10-14 11:16:51 +0200

    styling + component consolidation

--- libs/apps/uesio/studio/bundle/components/view/dataimport/dataimport.tsx
+++ libs/apps/uesio/studio/bundle/components/view/dataimport/dataimport.tsx
@@ -1,7 +1,6 @@
-import { FunctionComponent, useState } from "react"
-import { definition, hooks } from "@uesio/ui"
-
-import ImportBody from "./importbody"
+import { FunctionComponent, useState, useEffect } from "react"
+import { definition, hooks, collection, util, component } from "@uesio/ui"
+import ImportBodyItem from "./importbodyitem"
 import ImportButton from "./importbutton"
 
 type DataImportDefinition = {
@@ -18,7 +17,7 @@ interface State {
 	csvFields: string[]
 	file: File | null
 }
-
+const Button = component.getUtility("uesio/io.button")
 const DataImport: FunctionComponent<Props> = (props) => {
 	const { context, definition } = props
 	const uesio = hooks.useUesio(props)
@@ -38,22 +37,140 @@ const DataImport: FunctionComponent<Props> = (props) => {
 		setUploaded({ success, csvFields, file })
 	}
 
-	const collection = uesio.collection.useCollection(context, collectionId)
-	if (!collection) return null
+	const collectionInstance = uesio.collection.useCollection(
+		context,
+		collectionId
+	)
+	const collectionFields = Object.keys(
+		collectionInstance?.source.fields || {}
+	)
 
-	return !uploaded.success ? (
-		<ImportButton changeUploaded={changeUploaded} context={context} />
-	) : (
-		<ImportBody
-			definition={{
-				collectionId: "",
-				namespace: "",
-			}}
-			collection={collection}
-			csvFields={uploaded.csvFields}
-			file={uploaded.file}
-			context={context}
-		/>
+	const getInitialMatch = (
+		csvFields: string[],
+		collectionFields: string[]
+	): Record<string, definition.ImportMapping> =>
+		csvFields.reduce((mapping, key) => {
+			const field = collectionInstance?.getField(key) || ""
+			return !field || !collectionFields.includes(key)
+				? mapping
+				: {
+						...mapping,
+						[field.getId()]: {
+							columnname: key,
+							type: "IMPORT",
+						},
+				  }
+		}, {})
+
+	const [spec, setSpec] = useState<definition.ImportSpec>({
+		jobtype: "IMPORT",
+		collection: collectionInstance?.getFullName() || "",
+		filetype: "CSV",
+		mappings: getInitialMatch(uploaded.csvFields, collectionFields),
+	})
+
+	useEffect(() => {
+		uploaded.success &&
+			setSpec({
+				...spec,
+				collection: collectionInstance?.getFullName() || "",
+				mappings: getInitialMatch(uploaded.csvFields, collectionFields),
+			})
+	}, [uploaded.success])
+
+	if (!collectionInstance) return null
+
+	const addBlankSelectOption = collection.addBlankSelectOption
+	const csvOptions = addBlankSelectOption(
+		uploaded.csvFields.map((key) => ({
+			value: key,
+			label: key,
+		}))
+	)
+
+	const upload = async (file: File) => {
+		const tenant = context.getTenant()
+		if (!tenant) throw new Error("Invalid context for collection list")
+		const tenantType = context.getTenantType()
+		const jobResponse = await uesio.collection.createJob(context, spec)
+		if (!jobResponse.id) return
+
+		try {
+			await uesio.collection.importData(context, file, jobResponse.id)
+		} catch (error) {
+			const message = util.getErrorString(error)
+			uesio.notification.addError("Import error: " + message, context)
+			return
+		}
+
+		uesio.signal.run(
+			{
+				signal: "route/REDIRECT",
+				path: `/app/${tenant.app}/${tenantType}/${
+					tenant.name
+				}/data/${collectionInstance.getNamespace()}/${collectionInstance.getId()}`,
+			},
+			context
+		)
+	}
+
+	return (
+		<>
+			<div style={{ display: "flex", gap: "5px", marginBottom: "1em" }}>
+				<div style={{ flex: 1 }}>
+					<ImportButton
+						changeUploaded={changeUploaded}
+						context={context}
+						type={uploaded.success ? "button" : "area"}
+					/>
+				</div>
+				{uploaded.success && (
+					<Button
+						context={context}
+						variant={"uesio/io.primary"}
+						onClick={() => {
+							uploaded.file && upload(uploaded.file)
+						}}
+						label={"start import"}
+					/>
+				)}
+			</div>
+
+			{uploaded.success &&
+				collectionFields.map((fieldName, i) => {
+					const field = collectionInstance.getField(fieldName)
+					if (!field) return null
+					return (
+						<ImportBodyItem
+							key={`${fieldName}.${i}`}
+							index={i + 1}
+							context={context}
+							csvOptions={csvOptions}
+							mapping={spec.mappings[fieldName]}
+							removeMapping={() => {
+								const { [fieldName]: remove, ...rest } =
+									spec.mappings
+								setSpec({
+									...spec,
+									mappings: {
+										...rest,
+									},
+								})
+							}}
+							setMapping={(mapping) =>
+								setSpec({
+									...spec,
+									mappings: {
+										...spec.mappings,
+										[fieldName]: mapping,
+									},
+								})
+							}
+							field={field}
+						/>
+					)
+				})}
+		</>
 	)
 }
 
