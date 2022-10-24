cd95dafd04e9e1e385fc70177a3c608464d90ab2
Abel Jimenez Molla <abel@ues.io>
2022-10-24 16:04:05 +0200

    check if env is missing and if it has a value

--- apps/platform/pkg/configstore/environment/environment.go
+++ apps/platform/pkg/configstore/environment/environment.go
@@ -2,6 +2,7 @@ package environment
 
 import (
 	"errors"
+	"log"
 	"os"
 
 	"github.com/thecloudmasters/uesio/pkg/logger"
@@ -10,24 +11,37 @@ import (
 type ConfigStore struct {
 }
 
+func CheckEnv(key string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		log.Fatal(errors.New("Missing environment variable: " + key))
+	}
+
+	if value == "" {
+		logger.Log("environment variable: "+key+" does not have value", logger.ERROR)
+	}
+
+	return value
+}
+
 var configValues = map[string]string{
-	"uesio/core:cognito_client_id":                os.Getenv("COGNITO_CLIENT_ID"),
-	"uesio/core:cognito_pool_id":                  os.Getenv("COGNITO_POOL_ID"),
-	"uesio/core:google_project_id":                os.Getenv("GOOGLE_CLOUD_PROJECT"),
-	"uesio/core:mock_auth":                        os.Getenv("UESIO_MOCK_AUTH"),
-	"uesio/core:platform_authsource_type":         os.Getenv("UESIO_PLATFORM_AUTHSOURCE_TYPE"),
-	"uesio/core:platform_datasource_type":         os.Getenv("UESIO_PLATFORM_DATASOURCE_TYPE"),
-	"uesio/core:platform_filesource_type":         os.Getenv("UESIO_PLATFORM_FILESOURCE_TYPE"),
-	"uesio/core:platform_bundlestore_type":        os.Getenv("UESIO_PLATFORM_BUNDLESTORE_TYPE"),
-	"uesio/core:platform_authsource_credentials":  os.Getenv("UESIO_PLATFORM_AUTHSOURCE_CREDENTIALS"),
-	"uesio/core:platform_datasource_credentials":  os.Getenv("UESIO_PLATFORM_DATASOURCE_CREDENTIALS"),
-	"uesio/core:platform_filesource_credentials":  os.Getenv("UESIO_PLATFORM_FILESOURCE_CREDENTIALS"),
-	"uesio/core:platform_bundlestore_credentials": os.Getenv("UESIO_PLATFORM_BUNDLESTORE_CREDENTIALS"),
-	"uesio/core:aws_region":                       os.Getenv("AWS_REGION"),
-	"uesio/core:userfiles_bucket_name":            os.Getenv("UESIO_USERFILES_BUCKET_NAME"),
-	"uesio/core:db_host":                          os.Getenv("UESIO_DB_HOST"),
-	"uesio/core:db_port":                          os.Getenv("UESIO_DB_PORT"),
-	"uesio/core:bundlestore_bucket":               os.Getenv("UESIO_BUNDLES_BUCKET_NAME"),
+	"uesio/core:cognito_client_id":                CheckEnv("COGNITO_CLIENT_ID"),
+	"uesio/core:cognito_pool_id":                  CheckEnv("COGNITO_POOL_ID"),
+	"uesio/core:google_project_id":                CheckEnv("GOOGLE_CLOUD_PROJECT"),
+	"uesio/core:mock_auth":                        CheckEnv("UESIO_MOCK_AUTH"),
+	"uesio/core:platform_authsource_type":         CheckEnv("UESIO_PLATFORM_AUTHSOURCE_TYPE"),
+	"uesio/core:platform_datasource_type":         CheckEnv("UESIO_PLATFORM_DATASOURCE_TYPE"),
+	"uesio/core:platform_filesource_type":         CheckEnv("UESIO_PLATFORM_FILESOURCE_TYPE"),
+	"uesio/core:platform_bundlestore_type":        CheckEnv("UESIO_PLATFORM_BUNDLESTORE_TYPE"),
+	"uesio/core:platform_authsource_credentials":  CheckEnv("UESIO_PLATFORM_AUTHSOURCE_CREDENTIALS"),
+	"uesio/core:platform_datasource_credentials":  CheckEnv("UESIO_PLATFORM_DATASOURCE_CREDENTIALS"),
+	"uesio/core:platform_filesource_credentials":  CheckEnv("UESIO_PLATFORM_FILESOURCE_CREDENTIALS"),
+	"uesio/core:platform_bundlestore_credentials": CheckEnv("UESIO_PLATFORM_BUNDLESTORE_CREDENTIALS"),
+	"uesio/core:aws_region":                       CheckEnv("AWS_REGION"),
+	"uesio/core:userfiles_bucket_name":            CheckEnv("UESIO_USERFILES_BUCKET_NAME"),
+	"uesio/core:db_host":                          CheckEnv("UESIO_DB_HOST"),
+	"uesio/core:db_port":                          CheckEnv("UESIO_DB_PORT"),
+	"uesio/core:bundlestore_bucket":               CheckEnv("UESIO_BUNDLES_BUCKET_NAME"),
 }
 
 func (cs *ConfigStore) Get(key string) (string, error) {
