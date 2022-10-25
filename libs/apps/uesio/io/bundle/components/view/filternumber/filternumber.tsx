1ab03cc1a366acb344320c0a82e47148f9353466
Wessel <wesselvdp@gmail.com>
2022-10-25 15:59:27 +0200

    Smoother input
    Input is no longer visually delayed

--- libs/apps/uesio/io/bundle/components/view/filternumber/filternumber.tsx
+++ libs/apps/uesio/io/bundle/components/view/filternumber/filternumber.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useState, useEffect } from "react"
 
 import { Props } from "./filternumberdefinition"
 import { component, hooks, wire } from "@uesio/ui"
@@ -12,6 +12,15 @@ const FilterNumber: FC<Props> = (props) => {
 	const { context, definition, wire, path = "" } = props
 	const uesio = hooks.useUesio(props)
 	const operator = definition?.operator as Operator
+	const [value, setValue] = useState<number | null>(
+		Number(
+			(
+				wire.getCondition(
+					path + `["${operator}"]`
+				) as wire.ValueConditionState | null
+			)?.value
+		)
+	)
 
 	const onChange = (value: number | null, operator: Operator) => {
 		const id = props.path + `["${operator}"]`
@@ -40,25 +49,16 @@ const FilterNumber: FC<Props> = (props) => {
 		]
 		uesio.signal.runMany(signals, context)
 	}
-
-	const getDisplayValue = () => {
-		const value = (
-			wire.getCondition(
-				path + `["${operator}"]`
-			) as wire.ValueConditionState | null
-		)?.value
-		if (typeof value !== "number") return ""
-		return String(value)
-	}
-
 	const debouncedRequest = debounce(onChange, 250)
+
+	useEffect(() => {
+		debouncedRequest(value, operator)
+	}, [value])
 
 	return (
 		<TextField
-			value={getDisplayValue()}
-			setValue={(value: number | null): void =>
-				debouncedRequest(value ? Number(value) : value, operator)
-			}
+			value={value}
+			setValue={(value: number | null): void => setValue(value)}
 			context={context}
 			variant="uesio/io.textfield:uesio/studio.propfield"
 		/>
