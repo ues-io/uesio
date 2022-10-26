f9f7268742fad0719663bf6b82d6b1822a0c34aa
Wessel <wesselvdp@gmail.com>
2022-10-26 17:48:35 +0200

    oops

--- apps/platform/pkg/adapt/postgresio/conditions.go
+++ apps/platform/pkg/adapt/postgresio/conditions.go
@@ -119,7 +119,7 @@ func processValueCondition(condition adapt.LoadRequestCondition, collectionMetad
 		if fieldMetadata.Type != "MULTISELECT" {
 			return errors.New("Operator HAS_ALL only works with fieldType MULTI_SELECT")
 		}
-		builder.addQueryPart(fmt.Sprintf("%s ?| %s", fieldName, builder.addValue(condition.Value)))
+		builder.addQueryPart(fmt.Sprintf("%s ?& %s", fieldName, builder.addValue(condition.Value)))
 
 	case "NOT_EQ":
 		builder.addQueryPart(fmt.Sprintf("%s is distinct from %s", fieldName, builder.addValue(condition.Value)))
