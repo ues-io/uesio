04cab1418682368b2be711bc9f32ebac59b2d693
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2022-11-11 15:11:06 +0100

    LICENSES Styling (#1695)

--- apps/platform/pkg/datasource/systembot_after_bundledependency.go
+++ apps/platform/pkg/datasource/systembot_after_bundledependency.go
@@ -22,7 +22,6 @@ func runBundleDependencyAfterSaveBot(request *adapt.SaveOp, connection adapt.Con
 	visited := map[string]bool{}
 	request.LoopInserts(func(change *adapt.ChangeItem) error {
 
-		//CRM will use CMS --> "uesio/crm:dev:uesio/cms"
 		applicensed, app, err := parseKey(change.UniqueKey)
 		if err != nil {
 			return err
@@ -35,9 +34,6 @@ func runBundleDependencyAfterSaveBot(request *adapt.SaveOp, connection adapt.Con
 		}
 
 		visited[pairKey] = true
-
-		//query CMS license tempalte?
-		//Price and autoactive needs to be queried here
 
 		newDeps = append(newDeps, &adapt.Item{
 			"uesio/studio.app": map[string]interface{}{
