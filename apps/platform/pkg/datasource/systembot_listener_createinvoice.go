84b53ef75a2c4d6e925dad54f684094c576479b0
Abel Jimenez Molla <abel@ues.io>
2022-11-23 12:39:43 +0100

    accumulate

--- apps/platform/pkg/datasource/systembot_listener_createinvoice.go
+++ apps/platform/pkg/datasource/systembot_listener_createinvoice.go
@@ -129,36 +129,74 @@ func runCreateInvoiceListenerBot(params map[string]interface{}, connection adapt
 	}
 
 	type record struct {
-		total int
-		price int
+		total int64
+		price float64
 	}
 
-	var mymap map[string]record
+	accumulate := make(map[string]record, lpic.Len())
 
 	//merge lpic and usage
 	lpic.Loop(func(item meta.Item, _ string) error {
-		actiontype, err := item.GetField("uesio/studio.actiontype")
+		pricingActiontype, err := item.GetField("uesio/studio.actiontype")
 		if err != nil {
 			return err
 		}
 
-		actiontypeAsString, ok := actiontype.(string)
+		pricingActiontypeStr, ok := pricingActiontype.(string)
 		if !ok {
 			return errors.New("actiontype must be a string")
 		}
 
-		mymap[actiontypeAsString] = record{total: 2, price: 2}
+		if _, ok := accumulate[pricingActiontypeStr]; !ok {
+			//NEW record & get the price for that record
+			price, err := item.GetField("uesio/studio.price")
+			if err != nil {
+				return err
+			}
 
-		//UPLOAD_BYTES
+			pricefloat64, ok := price.(float64)
+			if !ok {
+				return errors.New("price must be a number")
+			}
 
-		//licensesIds = append(licensesIds, uniquekeyAsString)
+			accumulate[pricingActiontypeStr] = record{price: pricefloat64, total: 0}
+		}
 
 		return nil
 	})
 
-	//query all the sites of the app
+	usage.Loop(func(item meta.Item, _ string) error {
+		UsageActiontype, err := item.GetField("uesio/studio.actiontype")
+		if err != nil {
+			return err
+		}
 
-	//use maps
+		UsageActiontypeStr, ok := UsageActiontype.(string)
+		if !ok {
+			return errors.New("actiontype must be a string")
+		}
+
+		if lrecord, ok := accumulate[UsageActiontypeStr]; ok {
+
+			total, err := item.GetField("uesio/studio.total")
+			if err != nil {
+				return err
+			}
+
+			totalint64, ok := total.(int64)
+			if !ok {
+				return errors.New("total must be a number")
+			}
+
+			lrecord.total = lrecord.total + totalint64
+			accumulate[UsageActiontypeStr] = lrecord
+
+		}
+
+		return nil
+	})
+
+	//println(accumulate) + licenses montly price
 
 	// return CreateBundle(appID, workspace.Name, bundle, wsbs, session)
 
