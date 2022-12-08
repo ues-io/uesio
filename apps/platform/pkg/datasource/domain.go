c29c05165f78292b41e9a49d4c004999281427e6
humandad <55447225+humandad@users.noreply.github.com>
2022-12-08 12:51:25 -0500

    Small stripe improvements

---
+++ apps/platform/pkg/datasource/domain.go
@@ -0,0 +1,45 @@
+package datasource
+
+import (
+	"fmt"
+
+	"github.com/thecloudmasters/uesio/pkg/adapt"
+	"github.com/thecloudmasters/uesio/pkg/meta"
+	"github.com/thecloudmasters/uesio/pkg/sess"
+)
+
+func QueryDomainFromSite(siteID string) (*meta.SiteDomain, error) {
+	var sd meta.SiteDomain
+	err := PlatformLoadOne(
+		&sd,
+		&PlatformLoadOptions{
+			Fields: []adapt.LoadRequestField{
+				{
+					ID: "uesio/studio.domain",
+				},
+				{
+					ID: "uesio/studio.type",
+				},
+			},
+			Conditions: []adapt.LoadRequestCondition{
+				{
+					Field: "uesio/studio.site",
+					Value: siteID,
+				},
+			},
+			BatchSize: 1,
+		},
+		sess.GetStudioAnonSession(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &sd, nil
+}
+
+func GetHostFromDomain(domain *meta.SiteDomain, site *meta.Site) string {
+	if domain.Type == "subdomain" {
+		return fmt.Sprintf("https://%s.%s", domain.Domain, site.Domain)
+	}
+	return fmt.Sprintf("https://%s", domain.Domain)
+}
