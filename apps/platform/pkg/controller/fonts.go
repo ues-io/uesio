3e1e053c3c0dfafbfabf980185ae469f9c700642
Zach McElrath <zmcelrath87@gmail.com>
2023-01-05 13:48:51 -0500

    refactor: Serve fonts from directory-sandboxed FileServer

--- apps/platform/pkg/controller/fonts.go
+++ apps/platform/pkg/controller/fonts.go
@@ -2,11 +2,18 @@ package controller
 
 import (
 	"net/http"
+	"os"
 	"path/filepath"
 
-	"github.com/gorilla/mux"
+	"github.com/thecloudmasters/uesio/pkg/logger"
 )
 
-func Fonts(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, filepath.Join("fonts", mux.Vars(r)["filename"]))
+func Fonts() http.Handler {
+	cwd, err := os.Getwd()
+	if err != nil {
+		logger.LogError(err)
+		panic("Failed to obtain working directory")
+	}
+	fontServer := http.FileServer(http.Dir(filepath.Join(cwd, "fonts")))
+	return http.StripPrefix("/fonts", fontServer)
 }
