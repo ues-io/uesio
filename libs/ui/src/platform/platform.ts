8762a485d015fc922ece0287867f72db221493b1
Abel Jimenez Molla <abel@ues.io>
2023-01-05 17:19:51 +0100

    context for forgot password

--- libs/ui/src/platform/platform.ts
+++ libs/ui/src/platform/platform.ts
@@ -427,12 +427,14 @@ const platform = {
 		return respondJSON(response)
 	},
 	forgotPassword: async (
+		context: Context,
 		authSource: string,
 		requestBody: Record<string, string>
 	): Promise<void> => {
+		const prefix = getPrefix(context)
 		const [namespace, name] = parseKey(authSource)
 		const response = await postJSON(
-			`/site/auth/${namespace}/${name}/forgotpassword`,
+			`${prefix}/auth/${namespace}/${name}/forgotpassword`,
 			requestBody
 		)
 
