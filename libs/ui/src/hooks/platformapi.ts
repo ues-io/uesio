2fd8d053280db293b16d66059ec4bd07157194a4
Zach McElrath <zmcelrath87@gmail.com>
2023-01-06 16:31:02 -0500

    fix: Prepend static assets path to Monaco URLs

--- libs/ui/src/hooks/platformapi.ts
+++ libs/ui/src/hooks/platformapi.ts
@@ -1,5 +1,11 @@
 import { platform } from "../platform/platform"
-
 const loadData = platform.loadData
 
-export { loadData }
+// Hack for Monaco loader to be able to load assets from custom paths
+interface UesioWindow extends Window {
+    uesioStaticAssetsPath: string;
+}
+
+const getStaticAssetsPath = () => (window as unknown as UesioWindow).uesioStaticAssetsPath;
+
+export { loadData, getStaticAssetsPath }
