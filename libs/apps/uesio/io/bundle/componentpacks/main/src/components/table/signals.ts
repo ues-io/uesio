bc1743563996b170e39f28500dc70f89d911a190
Wessel <wesselvdp@gmail.com>
2023-01-12 17:01:34 +0100

    Update signals.ts

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/table/signals.ts
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/table/signals.ts
@@ -1,9 +1,11 @@
 import { signal } from "@uesio/ui"
-import { toggleMode } from "../../shared/mode"
+import { toggleMode, setEditMode, setReadMode } from "../../shared/mode"
 import { nextPage, prevPage } from "../../shared/pagination"
 
 const signals: Record<string, signal.ComponentSignalDescriptor> = {
 	TOGGLE_MODE: toggleMode,
+	SET_EDIT_MODE: setEditMode,
+	SET_READ_MODE: setReadMode,
 	NEXT_PAGE: nextPage,
 	PREV_PAGE: prevPage,
 }
