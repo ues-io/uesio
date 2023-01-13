49d5cb5e3caa634fe5a4c5ddb75948878ba6d31b
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-01-13 15:43:35 +0100

    first try (#1856)

---
+++ libs/ui/src/bands/collection/operations/createjob.ts
@@ -0,0 +1,15 @@
+import { Context } from "../../../context/context"
+import { platform } from "../../../platform/platform"
+import { Spec } from "../../../definition/definition"
+
+const createJob = async (collection: string, context: Context) => {
+	const spec: Spec = {
+		jobtype: "EXPORT",
+		collection,
+		filetype: "CSV",
+	}
+	await platform.createJob(context, spec)
+	return context
+}
+
+export default createJob
