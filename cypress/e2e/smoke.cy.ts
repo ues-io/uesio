39a4125a34b61a6dffb634ce7e1a654358bc08b2
Zach McElrath <zmcelrath87@gmail.com>
2023-02-01 22:49:37 -0500

    test: Add Cypress E2E tests, run on master build

---
+++ cypress/e2e/smoke.cy.ts
@@ -0,0 +1,105 @@
+/// <reference types="cypress" />
+
+import { getAppBasePath, getWorkspaceBasePath } from "../support/paths"
+
+describe("Uesio Sanity Smoke Tests", () => {
+	const username = Cypress.env("automation_username")
+
+	const appName = "e2e" + new Date().getTime()
+	const workspaceName = "test"
+	const workspaceBasePath = getWorkspaceBasePath(appName, workspaceName)
+
+	before(() => {
+		cy.loginWithAppAndWorkspace(appName, workspaceName)
+	})
+
+	context("Create Collection and Fields", () => {
+		it("creates collection and fields", () => {
+			cy.visitRoute(workspaceBasePath)
+			cy.get('[id$="uesio/io.tile:collections"]').click()
+			cy.url().should("contain", `${workspaceBasePath}/collections`)
+			// Test the hotkey for creating a new collection
+			cy.hotkey("{meta}n")
+			// Fill out the form to create a new collection
+			cy.typeInInput("new-collection-label", "Animal")
+			cy.typeInInput("new-collection-plural-label", "Animals")
+			cy.typeInInput("new-collection-name", "animal")
+			cy.clickButton("save-new-collection")
+			// Verify we get taken to the collection detail
+			cy.url().should(
+				"contain",
+				`${workspaceBasePath}/collections/${username}/${appName}/animal`
+			)
+			cy.getByIdSuffix("table", "fields").scrollIntoView()
+			// Create a CHECKBOX field
+			cy.hotkey("{meta}n")
+			cy.typeInInput("new-field-name", "is_extinct")
+			cy.typeInInput("new-field-label", "Is Extinct")
+			cy.changeSelectValue("new-field-type", "CHECKBOX")
+			cy.clickButton("save-new-field")
+			// verify the field was created
+			cy.get('table[id$="fields"]>tbody')
+				.children("tr")
+				.should("have.length", 1)
+			cy.get('table[id$="fields"]>tbody>tr')
+				.first()
+				.children("td")
+				.eq(0)
+				.find("input")
+				.first()
+				.should("have.value", "is_extinct")
+			cy.get('table[id$="fields"]>tbody>tr')
+				.first()
+				.children("td")
+				.eq(1)
+				.find("input")
+				.first()
+				.should("have.value", "Check Box")
+			cy.get('table[id$="fields"]>tbody>tr')
+				.first()
+				.children("td")
+				.eq(2)
+				.find("input")
+				.first()
+				.should("have.value", "Is Extinct")
+			// Create a NUMBER field
+			cy.hotkey("{meta}n")
+			cy.typeInInput("new-field-name", "estimated_population")
+			cy.typeInInput("new-field-label", "Estimated Population")
+			cy.changeSelectValue("new-field-type", "NUMBER")
+			cy.typeInInput("new-field-number-decimals", "0")
+			cy.hotkey("{meta}+s")
+			// verify the field was created
+			cy.get('table[id$="fields"]>tbody')
+				.children("tr")
+				.should("have.length", 2)
+			cy.get('table[id$="fields"]>tbody>tr')
+				.eq(1)
+				.children("td")
+				.eq(0)
+				.find("input")
+				.first()
+				.should("have.value", "estimated_population")
+			cy.get('table[id$="fields"]>tbody>tr')
+				.eq(1)
+				.children("td")
+				.eq(1)
+				.find("input")
+				.first()
+				.should("have.value", "Number")
+			cy.get('table[id$="fields"]>tbody>tr')
+				.eq(1)
+				.children("td")
+				.eq(2)
+				.find("input")
+				.first()
+				.should("have.value", "Estimated Population")
+		})
+	})
+
+	after(() => {
+		cy.visitRoute(getAppBasePath(appName))
+		cy.clickButton("delete-app")
+		cy.clickButton("confirm-delete-app")
+	})
+})
---
+++ cypress/support/commands.ts
@@ -0,0 +1,148 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This file creates various custom commands
+// can be called on the cy object, e.g. cy.login()
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { getAppBasePath, getWorkspaceBasePath } from "./paths"
+
+const baseUrl = Cypress.env("studio_base_url")
+const useMockLogin = Cypress.env("use_mock_login")
+const automationUsername = Cypress.env("automation_username")
+const automationPassword = Cypress.env("automation_password")
+const idEndsWithSelector = (el: string, idSuffix: string) =>
+	`${el}[id$="${idSuffix}"]`
+
+// Logs in to the app
+// and creates an App and Workspace to use for subsequent tests
+Cypress.Commands.add(
+	"loginWithAppAndWorkspace",
+	(appName: string, workspaceName: string) => {
+		// Caching session when logging in via page visit
+		cy.session("automationSession1", () => {
+			login()
+			createApp(appName)
+			createWorkspaceInApp(workspaceName, appName)
+		})
+	}
+)
+
+// Just logs in to the app
+Cypress.Commands.add("login", () => {
+	cy.session("automationSession2", login)
+})
+
+// Gets an element of a given type whose id ends with a given string
+Cypress.Commands.add(
+	"getByIdSuffix",
+	(elementType: string, idSuffix: string) => {
+		cy.get(idEndsWithSelector(elementType, idSuffix))
+	}
+)
+
+// Gets an input element whose id ends with a given string, and types a string into it
+Cypress.Commands.add("typeInInput", (idSuffix: string, value: string) => {
+	cy.get(idEndsWithSelector("input", idSuffix)).type(value)
+})
+
+// Changes the value of a <select>
+Cypress.Commands.add(
+	"changeSelectValue",
+	(selectElementIdSuffix: string, value: string) => {
+		cy.get(idEndsWithSelector("select", selectElementIdSuffix)).select(
+			value
+		)
+	}
+)
+
+// Gets a button element whose id ends with a given string
+Cypress.Commands.add("clickButton", (idSuffix: string) => {
+	cy.get(idEndsWithSelector("button", idSuffix)).click()
+})
+
+// Enters a global hotkey
+Cypress.Commands.add("hotkey", (hotkey: string) => {
+	cy.get("body").type(hotkey)
+})
+
+// Use this for all Uesio URL navigation, as it ensures that the Host header is set
+// while access the correct URL.  This ensures Uesio gets fed the subdomains it needs (e.g. "studio")
+// even though the URL we are visiting can be "localhost:3000" or "app:3000" (in docker network)
+Cypress.Commands.add("visitRoute", (route: string) => {
+	cy.visit(`${baseUrl}${route.startsWith("/") ? route : "/" + route}`, {
+		headers: {},
+	})
+})
+
+declare global {
+	namespace Cypress {
+		interface Chainable {
+			loginWithAppAndWorkspace(
+				appName: string,
+				workspaceName: string
+			): Chainable<void>
+			login(): Chainable<void>
+			visitRoute(route: string): Chainable<void>
+			getByIdSuffix(
+				elementType: string,
+				idSuffix: string
+			): Chainable<void>
+			typeInInput(inputIdSuffix: string, value: string): Chainable<void>
+			clickButton(idSuffix: string): Chainable<void>
+			hotkey(hotkey: string): Chainable<void>
+			changeSelectValue(
+				selectElementIdSuffix: string,
+				value: string
+			): Chainable<void>
+			//   drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+			//   dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+			//   visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+		}
+	}
+}
+function createWorkspaceInApp(workspaceName: string, appName: string) {
+	cy.clickButton("uesio/io.button:add-workspace")
+	cy.typeInInput("workspace-name", workspaceName)
+	cy.clickButton("uesio/io.button:save-workspace")
+	cy.url().should("contain", getWorkspaceBasePath(appName, workspaceName))
+}
+
+function createApp(appName: string) {
+	cy.clickButton("uesio/studio.home($root):uesio/io.button:add-app")
+	cy.typeInInput("new-app-name", appName)
+	cy.typeInInput("new-app-description", "E2E Test App")
+	cy.clickButton("save-new-app")
+	cy.url().should("contain", getAppBasePath(appName))
+}
+
+const login = () => {
+	cy.visitRoute("login")
+	if (useMockLogin) {
+		cy.clickButton("uesio/core.loginmock:mock-login-uesio")
+	} else {
+		cy.typeInInput("username", automationUsername)
+		cy.typeInInput("password", automationPassword)
+		cy.clickButton("sign-in")
+	}
+	cy.url().should("contain", "/home")
+}
