d356de962e3adeedfcd17b3b4ee054413dd502c2
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2023-02-06 12:25:41 -0500

    Move param specific code out of core (#1953)

--- libs/ui/src/hooks/wireapi.ts
+++ libs/ui/src/hooks/wireapi.ts
@@ -9,54 +9,9 @@ import Wire from "../bands/wire/class"
 import loadWiresOp from "../bands/wire/operations/load"
 import initWiresOp from "../bands/wire/operations/initialize"
 import { Context } from "../context/context"
-import { ViewOnlyField, WireDefinition } from "../definition/wire"
+import { WireDefinition } from "../definition/wire"
 import { useEffect } from "react"
-import { ParamDefinition } from "../definition/param"
-import WireRecord from "../bands/wirerecord/class"
-import { ID_FIELD } from "../bands/collection/types"
 import { dispatch } from "../store/store"
-
-const getWireFieldFromParamDef = (def: ParamDefinition): ViewOnlyField => {
-	switch (def.type) {
-		case "RECORD":
-			return {
-				label: def.prompt || def.name,
-				required: !!def.required,
-				type: "REFERENCE" as const,
-				reference: {
-					collection: def.collection,
-				},
-			}
-		case "METADATAMULTI":
-			return {
-				label: def.prompt || def.name,
-				required: !!def.required,
-				type: "LIST" as const,
-			}
-		default:
-			return {
-				label: def.prompt || def.name,
-				required: !!def.required,
-				type: "TEXT" as const,
-			}
-	}
-}
-
-const getValueForParam = (def: ParamDefinition, record: WireRecord) => {
-	const fieldKey = def.name
-	switch (def.type) {
-		case "RECORD":
-			return (
-				record.getFieldValue<string>(`${fieldKey}->${ID_FIELD}`) || ""
-			)
-		case "METADATAMULTI": {
-			const values = record.getFieldValue<string[]>(fieldKey) || []
-			return values.join(",")
-		}
-		default:
-			return record.getFieldValue<string>(fieldKey) || ""
-	}
-}
 
 // Wraps our store's useWire result (POJO) in a nice Wire class
 // with convenience methods to make the api easier to consume for end users.
@@ -127,29 +82,4 @@ const loadWires = loadWiresOp
 
 const initWires = initWiresOp
 
-const getWireFieldsFromParams = (params: ParamDefinition[] | undefined) => {
-	if (!params) return {}
-	return Object.fromEntries(
-		params.map((def) => [def.name, getWireFieldFromParamDef(def)])
-	)
-}
-
-const getParamValues = (
-	params: ParamDefinition[] | undefined,
-	record: WireRecord
-) => {
-	if (!params) return {}
-	return Object.fromEntries(
-		params.map((def) => [def.name, getValueForParam(def, record)])
-	)
-}
-
-export {
-	useWire,
-	useDynamicWire,
-	useWires,
-	loadWires,
-	initWires,
-	getWireFieldsFromParams,
-	getParamValues,
-}
+export { useWire, useDynamicWire, useWires, loadWires, initWires }
