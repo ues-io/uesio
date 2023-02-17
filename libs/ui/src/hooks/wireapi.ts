310d3f44026a50cbae9aa3e66b2665b5cd3ca78d
humandad <55447225+humandad@users.noreply.github.com>
2023-02-17 17:25:12 -0500

    Fix dynamic wire definition changes

--- libs/ui/src/hooks/wireapi.ts
+++ libs/ui/src/hooks/wireapi.ts
@@ -4,14 +4,18 @@ import {
 	useWire as uWire,
 	useWires as uWires,
 	removeOne as removeWire,
+	init,
 } from "../bands/wire"
 import Wire from "../bands/wire/class"
 import loadWiresOp from "../bands/wire/operations/load"
-import initWiresOp from "../bands/wire/operations/initialize"
+import initWiresOp, {
+	initExistingWire,
+} from "../bands/wire/operations/initialize"
 import { Context } from "../context/context"
 import { WireDefinition } from "../definition/wire"
 import { useEffect } from "react"
 import { dispatch } from "../store/store"
+import { PlainCollectionMap } from "../bands/collection/types"
 
 // Wraps our store's useWire result (POJO) in a nice Wire class
 // with convenience methods to make the api easier to consume for end users.
@@ -38,13 +42,24 @@ const useDynamicWire = (
 	const wire = useWire(wireName, context)
 	useEffect(() => {
 		if (!wireDef || !wireName) return
-		initWires(context, {
+		initWiresOp(context, {
 			[wireName]: wireDef,
 		})
-		loadWires(context, [wireName])
+		loadWiresOp(context, [wireName])
 		return () => {
 			remove(wireName, context)
 		}
+	}, [wireName])
+
+	useEffect(() => {
+		if (!wire || !wireDef) return
+		const collections: PlainCollectionMap = {}
+		const initializedWires = initExistingWire(
+			wire.source,
+			wireDef,
+			collections
+		)
+		dispatch(init([[initializedWires], collections]))
 	}, [wireName, JSON.stringify(wireDef)])
 	return wire
 }
