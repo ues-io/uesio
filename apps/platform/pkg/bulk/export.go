c86edbb90bf98c1c409daa05da71f5b19d8d4d86
humandad <55447225+humandad@users.noreply.github.com>
2023-02-21 11:31:21 -0500

    better organization

--- apps/platform/pkg/bulk/export.go
+++ apps/platform/pkg/bulk/export.go
@@ -4,8 +4,9 @@ import (
 	"archive/zip"
 	"bytes"
 	"errors"
+	"fmt"
 	"io"
-	"strconv"
+	"strings"
 	"time"
 
 	"github.com/thecloudmasters/uesio/pkg/adapt"
@@ -13,6 +14,7 @@ import (
 	"github.com/thecloudmasters/uesio/pkg/fileadapt"
 	"github.com/thecloudmasters/uesio/pkg/filesource"
 	"github.com/thecloudmasters/uesio/pkg/meta"
+	"github.com/thecloudmasters/uesio/pkg/retrieve"
 	"github.com/thecloudmasters/uesio/pkg/sess"
 )
 
@@ -36,92 +38,28 @@ func loadData(op *adapt.LoadOp, session *sess.Session) error {
 	return loadData(op, session)
 }
 
-func generateFileName(collectionName string) string {
-	y, m, d := time.Now().Date()
-	hour, min, sec := time.Now().Clock()
-	dateTime := strconv.Itoa(y) + "_" + m.String() + "_" + strconv.Itoa(d) + "_" + strconv.Itoa(hour) + ":" + strconv.Itoa(min) + ":" + strconv.Itoa(sec)
-
-	return collectionName + "_" + dateTime + ".zip"
-}
-
 func NewExportBatch(job meta.BulkJob, session *sess.Session) (*meta.BulkBatch, error) {
 
 	spec := job.Spec
-	fileFormat := spec.FileType
-
-	metadataResponse, err := getBatchMetadata(spec.Collection, session)
-	if err != nil {
-		return nil, err
-	}
-
-	//Load operation
-	fields := []adapt.LoadRequestField{}
-	collectionMetadata, err := metadataResponse.GetCollection(spec.Collection)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, fieldMetadata := range collectionMetadata.Fields {
-		// For reference fields, lets just request the id for now
-		if adapt.IsReference(fieldMetadata.Type) {
-			fields = append(fields, adapt.LoadRequestField{
-				ID: fieldMetadata.GetFullName(),
-				Fields: []adapt.LoadRequestField{
-					{
-						ID: adapt.ID_FIELD,
-					},
-				},
-			})
-			continue
-		}
-		fields = append(fields, adapt.LoadRequestField{
-			ID: fieldMetadata.GetFullName(),
-		})
-	}
-
-	var collection Exportable
-
-	if fileFormat == "CSV" {
-		collection = NewCSVExportCollection(collectionMetadata)
-	}
-
-	if collection == nil {
-		return nil, errors.New("Cannot process that file type: " + fileFormat)
-	}
-
-	op := &adapt.LoadOp{
-		WireName:       "uesio_data_export",
-		CollectionName: spec.Collection,
-		Collection:     collection,
-		Fields:         fields,
-		Query:          true,
-	}
-
-	err = loadData(op, session)
-	if err != nil {
-		return nil, err
-	}
 
 	batch := meta.BulkBatch{
 		Status:    "started",
 		BulkJobID: job.ID,
 	}
 
-	err = datasource.PlatformSaveOne(&batch, nil, nil, session)
+	err := datasource.PlatformSaveOne(&batch, nil, nil, session)
 	if err != nil {
 		return nil, err
 	}
 
+	tenantID := strings.ReplaceAll(session.GetTenantID(), "/", "_")
+	fileName := fmt.Sprintf("uesio_export_%s_%s.zip", tenantID, time.Now().Format(time.RFC3339))
+
 	details := &fileadapt.FileDetails{
-		Path:         generateFileName(spec.Collection),
+		Path:         fileName,
 		CollectionID: "uesio/core.bulkbatch",
 		RecordID:     batch.ID,
 		FieldID:      "uesio/core.result",
-	}
-
-	data, err := collection.GetData()
-	if err != nil {
-		return nil, err
 	}
 
 	buf := new(bytes.Buffer)
@@ -129,33 +67,9 @@ func NewExportBatch(job meta.BulkJob, session *sess.Session) (*meta.BulkBatch, e
 	// Create a new zip archive.
 	zipwriter := zip.NewWriter(buf)
 
-	file, err := zipwriter.Create("records.csv")
+	err = exportCollection(retrieve.NewWriterCreator(zipwriter.Create), spec, session)
 	if err != nil {
 		return nil, err
-	}
-	_, err = io.Copy(file, data)
-	if err != nil {
-		return nil, err
-	}
-
-	// Check to see if we found any file fields
-	fileIDs := collection.GetFileFieldIDs()
-
-	if len(fileIDs) > 0 {
-		for _, fileID := range fileIDs {
-			filedata, filemetadata, err := filesource.Download(fileID, session)
-			if err != nil {
-				return nil, err
-			}
-			file, err := zipwriter.Create(filemetadata.GetRelativePath())
-			if err != nil {
-				return nil, err
-			}
-			_, err = io.Copy(file, filedata)
-			if err != nil {
-				return nil, err
-			}
-		}
 	}
 
 	zipwriter.Close()
@@ -185,4 +99,95 @@ func NewExportBatch(job meta.BulkJob, session *sess.Session) (*meta.BulkBatch, e
 	}
 
 	return &batch, nil
+}
+
+func exportCollection(create retrieve.WriterCreator, spec *meta.JobSpec, session *sess.Session) error {
+	metadataResponse, err := getBatchMetadata(spec.Collection, session)
+	if err != nil {
+		return err
+	}
+
+	//Load operation
+	fields := []adapt.LoadRequestField{}
+	collectionMetadata, err := metadataResponse.GetCollection(spec.Collection)
+	if err != nil {
+		return err
+	}
+
+	for _, fieldMetadata := range collectionMetadata.Fields {
+		// For reference fields, lets just request the id for now
+		if adapt.IsReference(fieldMetadata.Type) {
+			fields = append(fields, adapt.LoadRequestField{
+				ID: fieldMetadata.GetFullName(),
+				Fields: []adapt.LoadRequestField{
+					{
+						ID: adapt.ID_FIELD,
+					},
+				},
+			})
+			continue
+		}
+		fields = append(fields, adapt.LoadRequestField{
+			ID: fieldMetadata.GetFullName(),
+		})
+	}
+
+	var collection Exportable
+
+	if spec.FileType == "CSV" {
+		collection = NewCSVExportCollection(collectionMetadata)
+	}
+
+	if collection == nil {
+		return errors.New("Cannot process that file type: " + spec.FileType)
+	}
+
+	op := &adapt.LoadOp{
+		WireName:       "uesio_data_export",
+		CollectionName: spec.Collection,
+		Collection:     collection,
+		Fields:         fields,
+		Query:          true,
+	}
+
+	err = loadData(op, session)
+	if err != nil {
+		return err
+	}
+
+	data, err := collection.GetData()
+	if err != nil {
+		return err
+	}
+
+	file, err := create(fmt.Sprintf("%s.csv", strings.ReplaceAll(spec.Collection, "/", "_")))
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, data)
+	if err != nil {
+		return err
+	}
+
+	// Check to see if we found any file fields
+	fileIDs := collection.GetFileFieldIDs()
+
+	if len(fileIDs) > 0 {
+		for _, fileID := range fileIDs {
+			filedata, filemetadata, err := filesource.Download(fileID, session)
+			if err != nil {
+				return err
+			}
+			file, err := create(filemetadata.GetRelativePath())
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(file, filedata)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
