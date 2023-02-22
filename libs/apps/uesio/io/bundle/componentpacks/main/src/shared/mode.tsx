43760b085ccd8f1f132fc5d2752956ce903474c1
Zach McElrath <zmcelrath87@gmail.com>
2023-02-22 14:38:27 -0500

    implement component specific params

--- libs/apps/uesio/io/bundle/componentpacks/main/src/shared/mode.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/shared/mode.tsx
@@ -8,22 +8,16 @@ const toggleMode: signal.ComponentSignalDescriptor<ModeState> = {
 	dispatcher: (state) => {
 		state.mode = state.mode === "READ" || !state.mode ? "EDIT" : "READ"
 	},
-	label: "Toggle Mode",
-	properties: () => [],
 }
 const setReadMode: signal.ComponentSignalDescriptor<ModeState> = {
 	dispatcher: (state) => {
 		state.mode = "READ"
 	},
-	label: "Read Mode",
-	properties: () => [],
 }
 const setEditMode: signal.ComponentSignalDescriptor<ModeState> = {
 	dispatcher: (state) => {
 		state.mode = "EDIT"
 	},
-	label: "Edit Mode",
-	properties: () => [],
 }
 
 const useMode = (id: string, initialMode: context.FieldMode | undefined) =>
