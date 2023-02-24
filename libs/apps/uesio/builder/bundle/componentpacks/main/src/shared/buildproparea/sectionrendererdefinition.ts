c972ae97bfad6f3ed5058889496333caadf342e5
humandad <55447225+humandad@users.noreply.github.com>
2023-02-24 12:21:36 -0500

    Remove unused builder code

--- libs/apps/uesio/builder/bundle/componentpacks/main/src/shared/buildproparea/sectionrendererdefinition.ts
+++
@@ -1,8 +0,0 @@
-import { definition, builder } from "@uesio/ui"
-
-interface SectionRendererProps extends definition.BaseProps {
-	section: builder.PropertySection
-	propsDef: builder.BuildPropertiesDefinition
-}
-
-export { SectionRendererProps }
