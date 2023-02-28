bc88ceb23768fb781da70d71e7305a46986baeef
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-02-28 15:02:43 +0100

    feat: Builder support for Filter Component (#2062)

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/filter/filter.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/filter/filter.tsx
@@ -80,29 +80,4 @@ const Filter: definition.UC<FilterDefinition> = (props) => {
 	)
 }
 
-/*
-const FilterPropertyDefinition: builder.BuildPropertiesDefinition = {
-	title: "Filter",
-	description: "Just a Filter",
-	link: "https://docs.ues.io/",
-	defaultDefinition: () => ({}),
-	properties: [
-		{
-			name: "wire",
-			type: "WIRE",
-			label: "Wire",
-		},
-		{
-			name: "fieldId",
-			type: "TEXT",
-			label: "id",
-		},
-	],
-	sections: [],
-	actions: [],
-	type: "component",
-	classes: ["root"],
-}
-*/
-
 export default Filter
