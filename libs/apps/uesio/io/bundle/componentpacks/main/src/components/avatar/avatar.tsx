47e6401bf3ae512e063ca21358e6901ae1c48b3f
Abel Jimenez Molla <abel@ues.io>
2023-03-02 09:59:16 +0100

    Cleanup for Avatar & Code components (remove unused props)

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/avatar/avatar.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/avatar/avatar.tsx
@@ -25,34 +25,4 @@ const Avatar: definition.UC<AvatarDefinition> = (props) => {
 	)
 }
 
-/*
-const AvatarPropertyDefinition: builder.BuildPropertiesDefinition = {
-	title: "Avatar",
-	description: "Display an image or initials to represent a record.",
-	link: "https://docs.ues.io/",
-	defaultDefinition: () => ({
-		text: "$User{initials}",
-		image: "$User{picture}",
-	}),
-	properties: [
-		{
-			name: "text",
-			type: "TEXT",
-			label: "text",
-		},
-		{
-			name: "image",
-			type: "TEXT",
-			label: "image",
-		},
-	],
-	sections: [],
-	actions: [],
-	traits: ["uesio.standalone"],
-	type: "component",
-	classes: ["root"],
-	category: "CONTENT",
-}
-*/
-
 export default Avatar
--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/code/code.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/code/code.tsx
@@ -3,7 +3,6 @@ import { default as IOCodeField } from "../../utilities/codefield/codefield"
 
 type CodeFieldDefinition = {
 	language?: CodeFieldLanguage
-	label?: string
 	file: string
 }
 
@@ -42,65 +41,5 @@ const CodeField: definition.UC<CodeFieldDefinition> = (props) => {
 		/>
 	)
 }
-/*
-const CodePropertyDefinition: builder.BuildPropertiesDefinition = {
-	title: "Code",
-	description: "Code editor",
-	link: "https://docs.ues.io/",
-	defaultDefinition: () => ({}),
-	properties: [
-		{
-			name: "language",
-			type: "SELECT",
-			label: "Language",
-			options: [
-				{
-					value: "",
-					label: "",
-				},
-				{
-					value: "yaml",
-					label: "yaml",
-				},
-				{
-					value: "json",
-					label: "json",
-				},
-				{
-					value: "javascript",
-					label: "javascript",
-				},
-				{
-					value: "typescript",
-					label: "typescript",
-				},
-				{
-					value: "html",
-					label: "html",
-				},
-				{
-					value: "css",
-					label: "css",
-				},
-			],
-		},
-		{
-			name: "label",
-			type: "TEXT",
-			label: "label",
-		},
-		{
-			name: "file",
-			type: "METADATA",
-			metadataType: "FILE",
-			label: "File",
-		},
-	],
-	sections: [],
-	actions: [],
-	//traits: ["uesio.standalone"],
-	type: "component",
-	classes: ["root"],
-} */
 
 export default CodeField
