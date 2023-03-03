72fbf77de56dda29ce7c53d64e6386bdecd299cb
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-03-03 13:26:35 +0100

    feat: Bring Grid Back to builder (#2087)

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/grid/grid.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/grid/grid.tsx
@@ -58,63 +58,6 @@ const Grid: definition.UC<GridDefinition> = (props) => {
 	)
 }
 
-/*
-
-const GridPropertyDefinition: builder.BuildPropertiesDefinition = {
-	title: "Grid",
-	description:
-		"Layout areas of your page using low-level CSS grid functionality.",
-	link: "https://docs.ues.io/",
-	defaultDefinition: () => ({}),
-	properties: [
-		{
-			name: "templateColumns",
-			type: "TEXT",
-			label: "Template Columns",
-		},
-		{
-			name: "columnGap",
-			type: "TEXT",
-			label: "Column Gap",
-		},
-		{
-			name: "templateRows",
-			type: "TEXT",
-			label: "Template Rows",
-		},
-		{
-			name: "rowGap",
-			type: "TEXT",
-			label: "Row Gap",
-		},
-	],
-	sections: [],
-	actions: [],
-	classes: ["root"],
-	traits: ["uesio.standalone"],
-	handleFieldDrop: (dragNode, dropNode, dropIndex) => {
-		const [metadataType, metadataItem] =
-			component.path.getFullPathParts(dragNode)
-
-		if (metadataType === "field") {
-			const [, , fieldNamespace, fieldName] =
-				component.path.parseFieldKey(metadataItem)
-			uesio.builder.addDefinition(
-				dropNode,
-				{
-					"uesio/io.field": {
-						fieldId: `${fieldNamespace}.${fieldName}`,
-					},
-				},
-				dropIndex
-			)
-		}
-	},
-	type: "component",
-	category: "LAYOUT",
-}
-*/
-
 Grid.displayName = "Grid"
 
 export { GridDefinition }
