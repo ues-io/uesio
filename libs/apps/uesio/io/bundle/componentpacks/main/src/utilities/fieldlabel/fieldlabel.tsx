38a770c1baa7077825ac251065a14e6ba809dd7e
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2023-03-03 13:28:10 +0100

    feat: Merges for io image and fieldlabel (#2084)

--- libs/apps/uesio/io/bundle/componentpacks/main/src/utilities/fieldlabel/fieldlabel.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/utilities/fieldlabel/fieldlabel.tsx
@@ -6,7 +6,7 @@ interface FieldLabelProps extends definition.UtilityProps {
 }
 
 const FieldLabel: FunctionComponent<FieldLabelProps> = (props) => {
-	const { label } = props
+	const { label, context } = props
 	const classes = styles.useUtilityStyles(
 		{
 			root: {},
@@ -14,7 +14,7 @@ const FieldLabel: FunctionComponent<FieldLabelProps> = (props) => {
 		props,
 		"uesio/io.fieldlabel"
 	)
-	return <div className={classes.root}>{label}</div>
+	return <div className={classes.root}>{context.merge(label)}</div>
 }
 
 export default FieldLabel
