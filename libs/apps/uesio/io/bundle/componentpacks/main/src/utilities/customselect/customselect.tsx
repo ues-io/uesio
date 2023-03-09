ee7c7296ed8e8a60e0bcc911206c5a7d38aadd08
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2023-03-09 10:21:01 -0500

    refactor: Add Menu Component, remove downshift library (#2109)

--- libs/apps/uesio/io/bundle/componentpacks/main/src/utilities/customselect/customselect.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/utilities/customselect/customselect.tsx
@@ -1,19 +1,7 @@
-import { ReactNode, useRef, useState } from "react"
-import { definition, styles } from "@uesio/ui"
+import { ReactNode } from "react"
+import { definition, styles, metadata } from "@uesio/ui"
 import Icon from "../icon/icon"
-import {
-	autoPlacement,
-	useFloating,
-	autoUpdate,
-	offset,
-	FloatingPortal,
-	useListNavigation,
-	useInteractions,
-	useDismiss,
-	useClick,
-	useRole,
-	FloatingFocusManager,
-} from "@floating-ui/react"
+import Menu from "../menu/menu"
 
 type CustomSelectProps<T> = {
 	onSelect: (item: T) => void
@@ -24,6 +12,7 @@ type CustomSelectProps<T> = {
 	onSearch?: (search: string) => void
 	searchFilter?: (item: T, search: string) => boolean
 	itemRenderer: (item: T) => ReactNode
+	menuVariant?: metadata.MetadataKey
 }
 
 const CustomSelect: definition.UtilityComponent<CustomSelectProps<unknown>> = (
@@ -39,65 +28,32 @@ const CustomSelect: definition.UtilityComponent<CustomSelectProps<unknown>> = (
 		onUnSelect,
 		itemRenderer,
 		context,
+		menuVariant,
 	} = props
 
 	const classes = styles.useUtilityStyles(
 		{
 			root: {},
-			menu: {},
-			menuitem: {},
 			notfound: {},
 			editbutton: {},
 			selecteditemwrapper: {},
-			highlighted: {},
-			searchbox: {},
 		},
 		props,
 		"uesio/io.customselectfield"
 	)
 
-	const [isOpen, setIsOpen] = useState(false)
-	const [searchText, setSearchText] = useState("")
-
-	const floating = useFloating({
-		open: isOpen,
-		onOpenChange: setIsOpen,
-		placement: "bottom-start",
-		middleware: [
-			offset(2),
-			autoPlacement({ allowedPlacements: ["top-start", "bottom-start"] }),
-		],
-		whileElementsMounted: autoUpdate,
-	})
-
-	const { x, y, strategy, refs } = floating
-
-	const listRef = useRef<(HTMLDivElement | null)[]>([])
-
-	const [activeIndex, setActiveIndex] = useState<number | null>(null)
-
-	const dismiss = useDismiss(floating.context)
-	const click = useClick(floating.context)
-	const role = useRole(floating.context, { role: "listbox" })
-
-	const listNavigation = useListNavigation(floating.context, {
-		listRef,
-		activeIndex,
-		onNavigate: setActiveIndex,
-		focusItemOnOpen: false,
-	})
-
-	const { getReferenceProps, getFloatingProps, getItemProps } =
-		useInteractions([click, dismiss, role, listNavigation])
-
 	return (
-		<>
-			<div
-				tabIndex={0}
-				className={classes.root}
-				ref={refs.setReference}
-				{...getReferenceProps()}
-			>
+		<Menu
+			onSelect={onSelect}
+			getItemKey={getItemKey}
+			itemRenderer={itemRenderer}
+			items={items}
+			onSearch={onSearch}
+			searchFilter={searchFilter}
+			context={context}
+			variant={menuVariant}
+		>
+			<div className={classes.root}>
 				<div>
 					{!selectedItems.length && (
 						<div className={classes.notfound}>Nothing Selected</div>
@@ -131,82 +87,7 @@ const CustomSelect: definition.UtilityComponent<CustomSelectProps<unknown>> = (
 					<Icon icon="expand_more" context={context} />
 				</button>
 			</div>
-			<FloatingPortal>
-				{isOpen && (
-					<FloatingFocusManager
-						context={floating.context}
-						modal={false}
-					>
-						<div
-							ref={refs.setFloating}
-							style={{
-								position: strategy,
-								top: y ?? 0,
-								left: x ?? 0,
-							}}
-							className={classes.menu}
-							{...getFloatingProps()}
-						>
-							<div>
-								{(onSearch || searchFilter) && (
-									<input
-										type="text"
-										value={searchText}
-										autoFocus
-										className={classes.searchbox}
-										placeholder="Search..."
-										onChange={(e) => {
-											onSearch?.(e.target.value)
-											setSearchText(e.target.value)
-										}}
-									/>
-								)}
-								{items
-									.filter((item) => {
-										if (!searchFilter) return true
-										if (!searchText) return true
-										return searchFilter(item, searchText)
-									})
-									.map((item, index) => (
-										<div
-											className={styles.cx(
-												classes.menuitem,
-												activeIndex === index &&
-													classes.highlighted
-											)}
-											key={getItemKey(item)}
-											tabIndex={
-												activeIndex === index ? 0 : -1
-											}
-											ref={(node) => {
-												listRef.current[index] = node
-											}}
-											role="option"
-											{...getItemProps({
-												// Handle pointer select.
-												onClick() {
-													onSelect(item)
-													setIsOpen(false)
-												},
-												// Handle keyboard select.
-												onKeyDown(event) {
-													if (event.key === "Enter") {
-														event.preventDefault()
-														onSelect(item)
-														setIsOpen(false)
-													}
-												},
-											})}
-										>
-											{itemRenderer(item)}
-										</div>
-									))}
-							</div>
-						</div>
-					</FloatingFocusManager>
-				)}
-			</FloatingPortal>
-		</>
+		</Menu>
 	)
 }
 
