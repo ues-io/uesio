5f7043e0ca08b9c4f688cd97a6cd522bb4015f91
Wessel <wesselvdp@gmail.com>
2023-03-22 13:21:10 +0100

    Update bot.ts

--- libs/apps/uesio/web/bundle/bots/listener/addlead/bot.ts
+++ libs/apps/uesio/web/bundle/bots/listener/addlead/bot.ts
@@ -1,12 +1,6 @@
 import { AfterSaveBotApi } from "uesio/bots"
 function addlead(bot: AfterSaveBotApi) {
 	const fields = ["firstname", "lastname", "email", "role"]
-	const labels: {
-		firstname: "first name"
-		lastname: "last name"
-		email: "email"
-		role: "role"
-	}
 
 	const values = fields.reduce(
 		(prev, key) => ({ ...prev, [key]: bot.params.get(key) }),
@@ -14,6 +8,12 @@ function addlead(bot: AfterSaveBotApi) {
 	)
 
 	// TODO: callbots can't throw errors yet. Uncomment when they can
+	// const labels = {
+	// 	firstname: "first name",
+	// 	lastname: "last name",
+	// 	email: "email",
+	// 	role: "role",
+	// }
 	// for (const f of values) {
 	// 	if (!values[f]) bot.addError(`missing ${labels[f]}`)
 	// }
@@ -29,7 +29,7 @@ function addlead(bot: AfterSaveBotApi) {
 	)
 	bot.save("uesio/crm.lead", [savedata])
 
-	// Send an email to the visitor
+	// Send an email to the user
 	const from = bot.getConfigValue("uesio/web.sales_email")
 	const templateId = bot.getConfigValue(
 		"uesio/web.email_template_leadresponse"
