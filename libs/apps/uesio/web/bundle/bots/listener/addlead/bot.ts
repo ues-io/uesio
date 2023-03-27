92d4f89442a68b4f09aefa654384ea70685a52f7
Wessel <wesselvdp@gmail.com>
2023-03-27 09:53:19 +0200

    PR feedback

--- libs/apps/uesio/web/bundle/bots/listener/addlead/bot.ts
+++ libs/apps/uesio/web/bundle/bots/listener/addlead/bot.ts
@@ -1,5 +1,5 @@
-import { AfterSaveBotApi } from "uesio/bots"
-function addlead(bot: AfterSaveBotApi) {
+import { ListenerBotApi } from "uesio/bots"
+function addlead(bot: ListenerBotApi) {
 	const fields = [
 		"uesio/crm.firstname",
 		"uesio/crm.lastname",
@@ -12,39 +12,39 @@ function addlead(bot: AfterSaveBotApi) {
 		{}
 	)
 
-	// // TODO: callbots can't throw errors yet. Uncomment when they can
-	// const labels = {
-	// 	"uesio/crm.firstname": "first name",
-	// 	"uesio/crm.lastname": "last name",
-	// 	"uesio/crm.email": "email",
-	// 	"uesio/web.role": "role",
-	// 	"uesio/crm.account": "company",
-	// }
-	// for (const key in values) {
-	// 	if (!values[key]) bot.addError(`missing ${labels[f]}`)
-	// }
+	// TODO: callbots can't throw errors yet. Uncomment when they can
+	const labels = {
+		"uesio/crm.firstname": "first name",
+		"uesio/crm.lastname": "last name",
+		"uesio/crm.email": "email",
+		"uesio/web.role": "role",
+		"uesio/crm.account": "company",
+	}
+	for (const key in values) {
+		if (!values[key]) throw new Error(`missing ${labels[key]}`)
+	}
 
 	// Save the lead in our leads collection
 	bot.save("uesio/crm.lead", [values])
 
 	// Send an email to the user
-	const salesEmail = bot.getConfigValue("uesio/web.sales_email")
+	const salesEmail = bot.getConfigValue("uesio/crm.sales_email")
 	const templateIdUser = bot.getConfigValue(
-		"uesio/web.email_template_leadresponse"
+		"email-template-lead-created-client"
 	)
 	const templateIdSales = bot.getConfigValue(
-		"uesio/web.email_template_lead_added_sales"
+		"email-template-lead-created-internal"
 	)
 
 	// Email to user
-	bot.runIntegrationAction("uesio/web.sendgrid", "sendEmail", {
+	bot.runIntegrationAction("uesio/crm.sendgrid", "sendEmail", {
 		to: [values["uesio/crm.email"]],
 		from: salesEmail,
 		templateId: templateIdUser,
 	})
 
 	// Email to us
-	bot.runIntegrationAction("uesio/web.sendgrid", "sendEmail", {
+	bot.runIntegrationAction("uesio/crm.sendgrid", "sendEmail", {
 		to: [salesEmail],
 		from: salesEmail,
 		templateId: templateIdSales,
