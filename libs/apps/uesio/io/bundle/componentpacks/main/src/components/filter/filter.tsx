644f4b6508613c4cf2b6586ff204f2c01fff4f25
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-03-28 15:06:26 +0200

    number filter & checkbox filter (#2179)
    * number filter
    
    * checkbox filter
    
    * clean

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/filter/filter.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/filter/filter.tsx
@@ -3,6 +3,8 @@ import FieldWrapper from "../../utilities/fieldwrapper/fieldwrapper"
 import MonthFilter from "../../utilities/monthfilter/monthfilter"
 import SelectFilter from "../../utilities/selectfilter/selectfilter"
 import WeekFilter from "../../utilities/weekfilter/weekfilter"
+import NumberFilter from "../../utilities/numberfilter/numberfilter"
+import CheckboxFilter from "../../utilities/checkboxfilter/checkboxfilter"
 import { LabelPosition } from "../field/field"
 
 type FilterDefinition = {
@@ -32,6 +34,10 @@ const getFilterContent = (
 	const type = fieldMetadata.getType()
 
 	switch (type) {
+		case "NUMBER":
+			return <NumberFilter {...common} />
+		case "CHECKBOX":
+			return <CheckboxFilter {...common} displayAs={displayAs} />
 		case "SELECT":
 			return <SelectFilter {...common} />
 		case "DATE": {
