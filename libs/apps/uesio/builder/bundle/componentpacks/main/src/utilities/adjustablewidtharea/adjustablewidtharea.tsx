e612e56a17955d9766dd3937469cc4f4fe0f50db
Zach McElrath <zach@ues.io>
2023-04-14 14:15:03 -0400

    move handler into useEffect

--- libs/apps/uesio/builder/bundle/componentpacks/main/src/utilities/adjustablewidtharea/adjustablewidtharea.tsx
+++ libs/apps/uesio/builder/bundle/componentpacks/main/src/utilities/adjustablewidtharea/adjustablewidtharea.tsx
@@ -9,32 +9,34 @@ const usePanels = (
 	const [dragging, setDragging] = useState(false)
 	const panelPosition = element?.getBoundingClientRect().left
 
-	// woah not soo fast
-	const throttledMouseHandler = throttle((e) => {
-		if (!dragging) return
-
-		const mouseX = e.clientX
-
-		if (!panelPosition || !mouseX) return
-		const change = panelPosition - mouseX
-		const x = width + change
-		const min = 250
-		const max = 600
-
-		if (x < min) {
-			setWidth(min)
-			return
-		}
-		if (x > max) {
-			setWidth(max)
-			return
-		}
-
-		setWidth(x)
-	}, 50)
-
 	useEffect(() => {
 		if (!dragging) return
+
+		// woah not soo fast
+		const throttledMouseHandler = throttle((e) => {
+			if (!dragging) return
+
+			const mouseX = e.clientX
+
+			if (!panelPosition || !mouseX) return
+
+			const change = panelPosition - mouseX
+			const x = width + change
+			const min = 250
+			const max = 600
+
+			if (x < min) {
+				setWidth(min)
+				return
+			}
+			if (x > max) {
+				setWidth(max)
+				return
+			}
+
+			setWidth(x)
+		}, 50)
+
 		document.addEventListener("mousemove", throttledMouseHandler)
 		document.addEventListener("mouseup", () => setDragging(false))
 
