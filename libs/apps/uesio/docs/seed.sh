c61fee0645e83fc956555465ea2c7d52881ff466
MarkoStrassl <123621483+MarkoStrassl@users.noreply.github.com>
2023-04-17 10:06:05 +0200

    feat: Remove uesio.styles from docs app (#2240)
    * Remove uesio.styles from docs app
    
    Removes all uesio.styles tags from the docs app.
    
    * fix styles in lesson.yaml
    
    * Update smallcallout.yaml
    
    * Update seed.sh
    
    ---------
    
    Co-authored-by: Abel Jimenez Molla <abel@ues.io>

--- libs/apps/uesio/docs/seed.sh
+++ libs/apps/uesio/docs/seed.sh
@@ -1,4 +1,4 @@
-npm run uesio upsert -- -f data/tracks/tracks.csv -s data/tracks/tracks.spec.json
-npm run uesio upsert -- -f data/articles/articles.csv -s data/articles/articles.spec.json
-npm run uesio upsert -- -f data/articles/docs -s data/articles/uploaddocs.spec.json
-npm run uesio upsert -- -f data/lessons/lessons.csv -s data/lessons/lessons.spec.json
+uesio upsert -f data/tracks/tracks.csv -s data/tracks/tracks.spec.json
+uesio upsert -f data/articles/articles.csv -s data/articles/articles.spec.json
+uesio upsert -f data/articles/docs -s data/articles/uploaddocs.spec.json
+uesio upsert -f data/lessons/lessons.csv -s data/lessons/lessons.spec.json
