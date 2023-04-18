f75cddf748480a2e4b23cc6c3748965ffafc0151
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-04-18 19:02:41 +0200

    Image component cleanup (#2249)
    * Image component cleanup
    
    * more fixes
    
    ---------
    
    Co-authored-by: humandad <55447225+humandad@users.noreply.github.com>

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/image/image.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/image/image.tsx
@@ -2,8 +2,6 @@ import { api, signal, definition, styles } from "@uesio/ui"
 
 type ImageDefinition = {
 	file?: string
-	height?: string
-	align?: "left" | "center" | "right"
 	signals?: signal.SignalDefinition[]
 	loading: "lazy" | "eager"
 	alt: string
@@ -15,40 +13,27 @@ const Image: definition.UC<ImageDefinition> = (props) => {
 
 	const classes = styles.useStyles(
 		{
-			root: {
-				display: "block",
-				textAlign: definition?.align || "left",
-				lineHeight: 0,
-				cursor: definition?.signals ? "pointer" : "",
-			},
-			inner: {
-				display: "inline-block",
-				height: definition?.height,
-			},
+			root: {},
 		},
 		props
 	)
 
 	return (
-		<div
-			className={classes.root}
+		<img
 			onClick={api.signal.getHandler(definition.signals, context)}
-		>
-			<img
-				id={api.component.getComponentIdFromProps(props)}
-				className={classes.inner}
-				src={
-					definition.file
-						? api.file.getURLFromFullName(
-								context,
-								context.mergeString(definition.file)
-						  )
-						: context.mergeString(definition.src)
-				}
-				loading={definition.loading}
-				alt={definition.alt}
-			/>
-		</div>
+			id={api.component.getComponentIdFromProps(props)}
+			className={classes.root}
+			src={
+				definition.file
+					? api.file.getURLFromFullName(
+							context,
+							context.mergeString(definition.file)
+					  )
+					: context.mergeString(definition.src)
+			}
+			loading={definition.loading}
+			alt={definition.alt}
+		/>
 	)
 }
 
