e12ab99d7c56131683b07a8f85e12ed97852cee3
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-04-19 15:21:14 +0200

    Vide component Cleanup (#2257)

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/video/video.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/video/video.tsx
@@ -3,7 +3,6 @@ import { api, styles, signal, definition } from "@uesio/ui"
 type VideoDefinition = {
 	file?: string
 	src?: string
-	height?: string
 	controls?: boolean
 	width?: string
 	muted?: boolean
@@ -18,49 +17,34 @@ const Video: definition.UC<VideoDefinition> = (props) => {
 
 	const classes = styles.useStyles(
 		{
-			root: {
-				display: "block",
-				lineHeight: 0,
-				cursor: definition?.signals ? "pointer" : "",
-			},
-			inner: {
-				display: "inline-block",
-				height: definition?.height,
-			},
+			root: {},
 		},
 		props
 	)
 
 	return (
-		<div
+		<video
 			className={classes.root}
+			loop={definition.loop || false}
+			controls={definition.controls || true}
+			width={definition.width}
+			autoPlay={definition.autoplay || false}
+			muted={definition.muted || false}
+			playsInline={definition.playsinline || false}
 			onClick={
 				definition?.signals &&
 				api.signal.getHandler(definition.signals, context)
 			}
 		>
-			<video
-				loop={definition.loop || false}
-				height={definition.height}
-				controls={definition.controls || true}
-				width={definition.width}
-				autoPlay={definition.autoplay || false}
-				muted={definition.muted || false}
-				playsInline={definition.playsinline || false}
-			>
-				<source
-					src={
-						definition.file
-							? api.file.getURLFromFullName(
-									context,
-									definition.file
-							  )
-							: context.mergeString(definition.src)
-					}
-				/>
-				Your browser does not support the video tag.
-			</video>
-		</div>
+			<source
+				src={
+					definition.file
+						? api.file.getURLFromFullName(context, definition.file)
+						: context.mergeString(definition.src)
+				}
+			/>
+			Your browser does not support the video tag.
+		</video>
 	)
 }
 
