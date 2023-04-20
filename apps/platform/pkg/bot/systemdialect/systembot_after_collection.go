5adae00cbd8cd4390c2c197dc648f0c1c2955713
Abel Jimenez Molla <abel@ues.io>
2023-04-20 16:08:31 +0200

    Update systembot_after_collection.go

--- apps/platform/pkg/bot/systemdialect/systembot_after_collection.go
+++ apps/platform/pkg/bot/systemdialect/systembot_after_collection.go
@@ -152,6 +152,7 @@ func validateUniqueKey(request *adapt.SaveOp, connection adapt.Connection, sessi
 
 		return nil
 	})
+
 	if err != nil {
 		return err
 	}
@@ -171,21 +172,27 @@ func validateUniqueKey(request *adapt.SaveOp, connection adapt.Connection, sessi
 
 	err = request.LoopChanges(func(change *adapt.ChangeItem) error {
 
-		collectionID, err := parseUniquekeyToCollectionKey(change.UniqueKey)
-		if err != nil {
-			return err
-		}
-
-		collectionMetadata, err := metadataResponse.GetCollection(collectionID)
-		if err != nil {
-			return err
-		}
-
 		oldUniquekey, err := change.GetOldField(STUDIO_UNIQUE_KEY_FIELD)
+		if err != nil {
+			return err
+		}
 		newUniquekey, err := change.GetField(STUDIO_UNIQUE_KEY_FIELD)
+		if err != nil {
+			return err
+		}
 		uniquekeyHasChanged := !reflect.DeepEqual(oldUniquekey, newUniquekey)
 
-		if err == nil {
+		if uniquekeyHasChanged {
+			collectionID, err := parseUniquekeyToCollectionKey(change.UniqueKey)
+			if err != nil {
+				return err
+			}
+
+			collectionMetadata, err := metadataResponse.GetCollection(collectionID)
+			if err != nil {
+				return err
+			}
+
 			uniquekeyFieldList := newUniquekey.([]interface{})
 			for _, fieldId := range uniquekeyFieldList {
 				stringVal, isString := fieldId.(string)
@@ -194,63 +201,12 @@ func validateUniqueKey(request *adapt.SaveOp, connection adapt.Connection, sessi
 				}
 				subField, err := collectionMetadata.GetField(stringVal)
 				if err != nil {
-					return errors.New("UniqueKey field " + stringVal + " not found:" + err.Error())
+					return errors.New("UniqueKey part " + stringVal + " not found:" + err.Error())
 				}
-				//TO-DO check what types are allowd for uniquekey int, string?
 				if subField.Type != "TEXT" || subField.GetFullName() == adapt.UNIQUE_KEY_FIELD {
 					return errors.New("UniqueKey can only be made up of fields of the text type")
 				}
 			}
-		}
-
-		//TO-DO remove || true
-		if uniquekeyHasChanged || true {
-			println("Trigger the DB update for this collection")
-
-			collection := &adapt.Collection{}
-
-			//LOAD all records of that collection
-			op := &adapt.LoadOp{
-				CollectionName: collectionID,
-				WireName:       "UniqueKeyWireLoad",
-				Collection:     collection,
-				Query:          true,
-				LoadAll:        true,
-			}
-
-			//TO-DO notice the wsSession
-			_, err := datasource.Load([]*adapt.LoadOp{op}, wsSession, &datasource.LoadOptions{
-				Metadata: connection.GetMetadata(),
-			})
-			if err != nil {
-				return err
-			}
-
-			//adapt.SetUniqueKey for each record
-			err = op.Collection.Loop(func(item meta.Item, index string) error {
-				test := item.SetField(adapt.UNIQUE_KEY_FIELD, "NEW_COOL_UNIQUEKEY"+index)
-				println(test)
-				return nil
-			})
-			if err != nil {
-				return err
-			}
-
-			//SAVE IT
-			err = datasource.Save([]datasource.SaveRequest{
-				{
-					Collection: collectionID,
-					Wire:       "UniqueKeyWireSave",
-					Changes:    collection,
-					Options: &adapt.SaveOptions{
-						Upsert: true,
-					},
-				},
-			}, wsSession)
-			if err != nil {
-				return err
-			}
-
 		}
 
 		return nil
