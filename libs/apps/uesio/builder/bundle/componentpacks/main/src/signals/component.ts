2cc574f1d217b408405c9008c6a95f564cb96ff1
Zach McElrath <zach@ues.io>
2023-04-26 14:30:23 -0400

    fixup

--- libs/apps/uesio/builder/bundle/componentpacks/main/src/signals/component.ts
+++ libs/apps/uesio/builder/bundle/componentpacks/main/src/signals/component.ts
@@ -10,6 +10,7 @@ import {
 	getComponentDef,
 	getComponentIdsOfType,
 } from "../api/stateapi"
+import { ComponentProperty } from "../properties/componentproperty"
 
 const getComponentTypesWithSignals = (context: context.Context) => {
 	const defs = Object.entries(getComponentDefs(context) || {}).filter(
@@ -85,76 +86,91 @@ const signals: SignalBandDefinition = {
 			label: "Run Component Signal",
 			description:
 				"Invokes a Component-defined Signal on one or more instances of that Component",
-			properties: (signal: ComponentSignalDefinition, context) => [
-				{
-					type: "SELECT",
-					name: "component",
-					label: "Component Type",
-					options: getComponentTypesWithSignals(context).map(
-						([type, def]) => ({
-							value: type,
-							label: def.title,
-						})
-					),
-					blankOptionLabel: "Select...",
-				},
-				{
-					type: "SELECT",
-					name: "componentsignal",
-					label: "Component Signal",
-					options: getComponentSignalOptions(
-						context,
-						signal.component as string
-					),
-					blankOptionLabel: "Select...",
-				},
-				// Once a specific Component Type is selected, you then get to choose a "Run Signal for which components?" option,
-				// which has two choices: (A) "All components whose id contains..." which then gives you a "Target" text box,
-				// OR (B) "Specific Component" , which then gives you a Select which lets you pick a specific Component by its Component Id .
-				// ONLY Components with a Component Id ("uesio.id" property) would be displayed here.
-				{
-					type: "SELECT",
-					options: [
-						{
-							value: "specific",
-							label: "Specific Component",
-						},
-						{
-							value: "multiple",
-							label: "All components whose id contains...",
-						},
-					],
-					name: "targettype",
-					label: "Run Signal for which components?",
-					blankOptionLabel: "Select...",
-				},
-				{
-					type: "SELECT",
-					options: getComponentIds(context, signal.component),
-					name: "componentid",
-					label: "Component Id",
-					blankOptionLabel: "Select Component...",
-					displayConditions: [
-						{
-							field: "targettype",
-							value: "specific",
-							type: "fieldValue",
-						},
-					],
-				},
-				{
-					type: "TEXT",
-					name: "target",
-					label: "Target Components",
-					displayConditions: [
-						{
-							field: "targettype",
-							value: "multiple",
-							type: "fieldValue",
-						},
-					],
-				},
-			],
+			properties: (signal: ComponentSignalDefinition, context) => {
+				const baseProperties = [
+					{
+						type: "SELECT",
+						name: "component",
+						label: "Component Type",
+						options: getComponentTypesWithSignals(context).map(
+							([type, def]) => ({
+								value: type,
+								label: def.title,
+							})
+						),
+						blankOptionLabel: "Select...",
+					},
+					{
+						type: "SELECT",
+						name: "componentsignal",
+						label: "Component Signal",
+						options: getComponentSignalOptions(
+							context,
+							signal.component as string
+						),
+						blankOptionLabel: "Select...",
+					},
+					// Once a specific Component Type is selected, you then get to choose a "Run Signal for which components?" option,
+					// which has two choices: (A) "All components whose id contains..." which then gives you a "Target" text box,
+					// OR (B) "Specific Component" , which then gives you a Select which lets you pick a specific Component by its Component Id .
+					// ONLY Components with a Component Id ("uesio.id" property) would be displayed here.
+					{
+						type: "SELECT",
+						options: [
+							{
+								value: "specific",
+								label: "Specific Component",
+							},
+							{
+								value: "multiple",
+								label: "All components whose id contains...",
+							},
+						],
+						name: "targettype",
+						label: "Run Signal for which components?",
+						blankOptionLabel: "Select...",
+					},
+					{
+						type: "SELECT",
+						options: getComponentIds(context, signal.component),
+						name: "componentid",
+						label: "Component Id",
+						blankOptionLabel: "Select Component...",
+						displayConditions: [
+							{
+								field: "targettype",
+								value: "specific",
+								type: "fieldValue",
+							},
+						],
+					},
+					{
+						type: "TEXT",
+						name: "target",
+						label: "Target Components",
+						displayConditions: [
+							{
+								field: "targettype",
+								value: "multiple",
+								type: "fieldValue",
+							},
+						],
+					},
+				] as ComponentProperty[]
+
+				// Append signal-specific properties, if there are any
+				const signalProperties = (
+					signal.component && signal.componentsignal
+						? getComponentDef(context, signal.component)?.signals?.[
+								signal.componentsignal
+						  ]?.properties
+						: []
+				) as ComponentProperty[]
+
+				return signalProperties
+					? baseProperties.concat(signalProperties)
+					: baseProperties
+			},
 			// Signal-defined outputs
 			// outputs: [{ name: "result", type: "MAP" }],
 		},
