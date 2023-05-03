2008b5957761a5f99f15087e10f5cf034aadfa7a
Zach McElrath <zach@ues.io>
2023-05-03 11:29:43 -0400

    remove dup line

--- libs/apps/uesio/tests/bundle/bots/listener/add_numbers/bot.ts
+++ libs/apps/uesio/tests/bundle/bots/listener/add_numbers/bot.ts
@@ -1,5 +1,4 @@
 import { ListenerBotApi } from "uesio/bots"
-import { ListenerBotApi } from "uesio/bots"
 function add_numbers(bot: ListenerBotApi) {
 	const a = (bot.params.get("a") as number) || 0
 	const b = (bot.params.get("b") as number) || 0
