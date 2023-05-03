bea7613d83910ce6ce23409bc8479bfd5167a07a
Zach McElrath <zmcelrath87@gmail.com>
2023-05-03 11:05:03 -0400

    feat: Improve the init, component, componentpack generators (#2334)

--- libs/apps/uesio/core/bundle/bots/generator/component/bot.js
+++ libs/apps/uesio/core/bundle/bots/generator/component/bot.js
@@ -1,5 +1,21 @@
 function collection(bot) {
+	var pack = bot.params.get("pack")
 	var name = bot.params.get("name")
-	var baseDir = "components/view/" + name + "/"
-	bot.generateFile(baseDir + name + ".tsx", {}, "templates/component.tsx")
+	var componentName = `${name.charAt(0).toUpperCase() + name.slice(1)}`
+	// Generate the component TSX file
+	bot.generateFile(
+		`componentpacks/${pack}/src/components/${name}/${name}.tsx`,
+		{},
+		"templates/component.template.tsx"
+	)
+	// Generate the component YAML file
+	bot.generateFile(
+		`components/${name}.yaml`,
+		{
+			componentName,
+			name,
+			pack,
+		},
+		"templates/component.template.yaml"
+	)
 }
