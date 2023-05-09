7beb01217fa4aad52c4b9a853d084f12c99c1b01
Zach McElrath <zach@ues.io>
2023-05-09 16:15:53 -0400

    restore code accidentally removed

--- libs/apps/uesio/builder/bundle/componentpacks/main/src/helpers/propertiesform.tsx
+++ libs/apps/uesio/builder/bundle/componentpacks/main/src/helpers/propertiesform.tsx
@@ -568,11 +568,17 @@ const parseProperties = (
 			// but we are rendering these using the Multiselect control,
 			// which works with a Record<string, boolean> where the keys are values which
 			// should be present in the YAML list
-			setter = (value: Record<string, boolean>) => {
-				if (viewOnly) return
-				const newValue = Object.keys(value)
-				// Update the YAML def
-				setDef(context, propPath, newValue)
+			setter = (value: Record<string, boolean>) =>
+				!viewOnly && setDef(context, propPath, Object.keys(value))
+			value = getDef(context, propPath) as string[]
+			if (value !== undefined) {
+				value = (value as string[]).reduce(
+					(acc, curr) => ({
+						...acc,
+						[curr]: true,
+					}),
+					{}
+				)
 			}
 		} else {
 			value = getDef(context, propPath) as string
