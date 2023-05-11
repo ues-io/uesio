44bdafaebd57d6eded6934d105fc1d85e8dbb855
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2023-05-11 17:34:41 +0200

    Google Analytics 4 Component for the web app (#2337)
    * added ga4 component
    
    * final v
    
    * Update ga4.tsx
    
    * remove log
    
    ---------
    
    Co-authored-by: Ben Hubbard <55447225+humandad@users.noreply.github.com>

---
+++ libs/apps/uesio/web/bundle/componentpacks/main/src/ga4.tsx
@@ -0,0 +1,25 @@
+import { FC, useEffect } from "react"
+import { definition, api } from "@uesio/ui"
+
+import ReactGA from "react-ga4"
+
+const Ga4: FC<definition.BaseProps> = ({ context }) => {
+	const MEASUREMENT_ID = api.view.useConfigValue(
+		"uesio/web.ga4_measurement_id"
+	)
+
+	useEffect(() => {
+		if (!MEASUREMENT_ID) {
+			return
+		}
+		ReactGA.initialize(MEASUREMENT_ID)
+		ReactGA.send({
+			hitType: "pageview",
+			page: context.getRoute()?.path,
+		})
+	}, [])
+
+	return null
+}
+
+export default Ga4
