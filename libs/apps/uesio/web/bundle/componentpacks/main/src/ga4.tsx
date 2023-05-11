c9d8109299236dbea3245cc85c83c5981fd07e28
humandad <55447225+humandad@users.noreply.github.com>
2023-05-11 12:21:30 -0400

    Fix google analytics build

--- libs/apps/uesio/web/bundle/componentpacks/main/src/ga4.tsx
+++ libs/apps/uesio/web/bundle/componentpacks/main/src/ga4.tsx
@@ -4,6 +4,8 @@ import { definition, api } from "@uesio/ui"
 import ReactGA from "react-ga4"
 
 const Ga4: FC<definition.BaseProps> = ({ context }) => {
+	const path = context.getRoute()?.path
+
 	const MEASUREMENT_ID = api.view.useConfigValue(
 		"uesio/web.ga4_measurement_id"
 	)
@@ -15,9 +17,9 @@ const Ga4: FC<definition.BaseProps> = ({ context }) => {
 		ReactGA.initialize(MEASUREMENT_ID)
 		ReactGA.send({
 			hitType: "pageview",
-			page: context.getRoute()?.path,
+			page: path,
 		})
-	}, [])
+	}, [path])
 
 	return null
 }
