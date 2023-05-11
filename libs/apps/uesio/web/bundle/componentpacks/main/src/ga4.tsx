8c2c1c562cb0db1dbf53c9f21d8965215fe5da28
Wessel <wesselvdp@gmail.com>
2023-05-11 16:57:58 +0200

    final v

--- libs/apps/uesio/web/bundle/componentpacks/main/src/ga4.tsx
+++ libs/apps/uesio/web/bundle/componentpacks/main/src/ga4.tsx
@@ -1,17 +1,25 @@
-import React, { FC } from "react"
+import { FC, useEffect } from "react"
 import { definition, api } from "@uesio/ui"
+
 import ReactGA from "react-ga4"
 
-const Ga4: FC<definition.BaseProps> = (props) => {
-	const MEASUREMENT_ID =
-		api.view.useConfigValue("uesio/web.ga4_measurement_id") ||
-		"G-1WLYF635CR"
+const Ga4: FC<definition.BaseProps> = ({ context }) => {
+	const MEASUREMENT_ID = api.view.useConfigValue(
+		"uesio/web.ga4_measurement_id"
+	)
+	// "G-1WLYF635CR"
 
-	ReactGA.initialize(MEASUREMENT_ID)
-	ReactGA.send({
-		hitType: "pageview",
-		page: props.context.getRoute()?.path,
-	})
+	useEffect(() => {
+		if (!MEASUREMENT_ID) {
+			console.log("missing measurement id")
+			return
+		}
+		ReactGA.initialize(MEASUREMENT_ID)
+		ReactGA.send({
+			hitType: "pageview",
+			page: context.getRoute()?.path,
+		})
+	}, [])
 
 	return null
 }
