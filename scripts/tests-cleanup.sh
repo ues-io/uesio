ab8bda6fd8203ef8d758fc03b69d3a71866ef3dc
Abel Jimenez Molla <abel@ues.io>
2023-05-23 15:23:45 +0200

    all test pass

--- scripts/tests-cleanup.sh
+++ scripts/tests-cleanup.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+export UESIO_CLI_LOGIN_METHOD=uesio/core.mock
+export UESIO_CLI_USERNAME=uesio
+export UESIO_CLI_HOST="https://studio.uesio-dev.com:3000"
+
 if [[ -z "${APP_IMAGE}" ]]; then
     if [[ -z "${GITSHA}" ]]; then
         export GITSHA=$(git rev-parse --short HEAD)
@@ -9,13 +13,25 @@ if [[ -z "${APP_IMAGE}" ]]; then
     export APP_IMAGE="$GITSHA"
 fi
 
+#Navigate
+cd libs/apps/uesio/tests
+
+echo "Logging in to Studio as uesio user..."
+uesio logout
+uesio sethost
+uesio login
+
 # Truncate dev workspace 
-uesio work -n dev
 echo "Truncate dev workspace."
+uesio work -n dev
 uesio workspace truncate
+echo "dev workspace should be clear"
+
+#Navigate back
+cd ../../../..
 
 # Run specs
-hurl --very-verbose -k --variable host=studio.uesio-dev.com --variable port=3000 --test libs/apps/uesio/tests/hurl_specs/truncate_tenant_data_cli.hurl
+hurl --very-verbose -k --variable host=studio.uesio-dev.com --variable port=3000 --test libs/apps/uesio/tests/hurl_specs_single_run/truncate_tenant_data_cli.hurl
 
 
 # Kill all Docker containers
