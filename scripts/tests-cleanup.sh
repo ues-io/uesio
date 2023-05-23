24506cfbb188322291edc25ae3bf3ff8c451a6b0
Abel Jimenez Molla <abel@ues.io>
2023-05-23 13:56:31 +0200

    close to the end

--- scripts/tests-cleanup.sh
+++ scripts/tests-cleanup.sh
@@ -9,5 +9,14 @@ if [[ -z "${APP_IMAGE}" ]]; then
     export APP_IMAGE="$GITSHA"
 fi
 
+# Truncate dev workspace 
+uesio work -n dev
+echo "Truncate dev workspace."
+uesio workspace truncate
+
+# Run specs
+hurl --very-verbose -k --variable host=studio.uesio-dev.com --variable port=3000 --test libs/apps/uesio/tests/hurl_specs/truncate_tenant_data_cli.hurl
+
+
 # Kill all Docker containers
 docker compose -f docker-compose-tests.yaml down
