cbd74b166ad2f6f977e6c00194c2b50a445cb7c6
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2023-05-30 14:46:50 -0400

    Documentation refactor (#2471)

---
+++ apps/cli/pkg/wire/site.go
@@ -0,0 +1,47 @@
+package wire
+
+import (
+	"github.com/thecloudmasters/uesio/pkg/adapt"
+)
+
+func GetAvailableSiteNames(appID string) ([]string, error) {
+	names := []string{}
+
+	sites, err := GetAvailableSites(appID)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(sites) == 0 {
+		return names, nil
+	}
+	for _, item := range sites {
+		siteName, err := item.GetFieldAsString("uesio/studio.name")
+		if err != nil {
+			return nil, err
+		}
+		names = append(names, siteName)
+	}
+
+	return names, nil
+}
+
+func GetAvailableSites(appID string) (adapt.Collection, error) {
+	return Load(
+		"uesio/studio.site",
+		&LoadOptions{
+			Fields: []adapt.LoadRequestField{
+				{
+					ID: "uesio/studio.name",
+				},
+			},
+			Conditions: []adapt.LoadRequestCondition{
+				{
+					Field:    "uesio/studio.app",
+					RawValue: appID,
+				},
+			},
+		},
+	)
+
+}
