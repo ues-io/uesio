16d66fa95741e72b4eace72b2dcb73233b7a5f81
Abel Jimenez Molla <abel@ues.io>
2023-06-14 08:58:34 +0200

    remove comment

--- apps/platform/pkg/adapt/postgresio/save.go
+++ apps/platform/pkg/adapt/postgresio/save.go
@@ -2,6 +2,7 @@ package postgresio
 
 import (
 	"context"
+
 	"github.com/francoispqt/gojay"
 	"github.com/jackc/pgx/v5"
 	"github.com/thecloudmasters/uesio/pkg/adapt"
@@ -130,7 +131,6 @@ func (c *Connection) Save(request *adapt.SaveOp, session *sess.Session) error {
 	for i := 0; i < execCount; i++ {
 		_, err := results.Exec()
 		if err != nil {
-			//fmt.Println("Error saving: " + request.Metadata.GetFullName())
 			results.Close()
 			return err
 		}
