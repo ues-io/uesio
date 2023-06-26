0fa2fb55285fb3f94857e937fbf840a74dc42161
humandad <55447225+humandad@users.noreply.github.com>
2023-06-26 14:57:30 -0400

    Add asadmin to aftersave

--- apps/platform/pkg/bot/jsdialect/aftersave.go
+++ apps/platform/pkg/bot/jsdialect/aftersave.go
@@ -13,6 +13,7 @@ type AfterSaveAPI struct {
 	op         *adapt.SaveOp
 	session    *sess.Session
 	connection adapt.Connection
+	AsAdmin    AdminCallBotAPI `bot:"asAdmin"`
 }
 
 func NewAfterSaveAPI(request *adapt.SaveOp, connection adapt.Connection, session *sess.Session) *AfterSaveAPI {
@@ -29,6 +30,10 @@ func NewAfterSaveAPI(request *adapt.SaveOp, connection adapt.Connection, session
 		session:    session,
 		op:         request,
 		connection: connection,
+		AsAdmin: AdminCallBotAPI{
+			Session:    session,
+			Connection: connection,
+		},
 	}
 }
 
