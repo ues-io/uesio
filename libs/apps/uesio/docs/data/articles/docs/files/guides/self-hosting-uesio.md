5df2040fef3200f2320cf85f8a0f76425aba16c7
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-07-13 16:18:03 +0200

    Deatach Uesio Docs app (#2703)
    * Deatach Uesio Docs app
    
    * Fix tests and prevent errors on home page

--- libs/apps/uesio/docs/data/articles/docs/files/guides/self-hosting-uesio.md
+++
@@ -1,1 +0,0 @@
-# Self-hosting ues.io
--- libs/apps/uesio/docs/seed.sh
+++
@@ -1,2 +0,0 @@
-uesio upsert -f data/articles/articles.csv -s data/articles/articles.spec.json
-uesio upsert -f data/articles/docs -s data/articles/uploaddocs.spec.json
