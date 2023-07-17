b5562d9bc162e4cdb891d41dd1e06f9476827524
Zach McElrath <zach@ues.io>
2023-07-17 15:41:53 -0600

    feat: add new "site deploy" CLI command for changing site's bundle

--- apps/cli/pkg/cmd/site.go
+++ apps/cli/pkg/cmd/site.go
@@ -2,9 +2,9 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/thecloudmasters/cli/pkg/command"
+	"github.com/thecloudmasters/cli/pkg/command/site"
 )
 
 func siteUpsert(cmd *cobra.Command, args []string) {
@@ -23,6 +23,27 @@ func siteUpsert(cmd *cobra.Command, args []string) {
 	}
 }
 
+func siteDeploy(cmd *cobra.Command, args []string) {
+	siteName, _ := cmd.Flags().GetString("site")
+	newBundleVersion, _ := cmd.Flags().GetString("bundle")
+
+	if siteName == "" {
+		fmt.Println("site name is required")
+		return
+	}
+	if newBundleVersion == "" {
+		fmt.Println("bundle is required")
+		return
+	}
+
+	err := site.SiteDeploy(siteName, newBundleVersion)
+	if err != nil {
+		fmt.Println("Unable to deploy site: " + err.Error())
+		return
+	}
+	fmt.Printf("Successfully updated site %s to bundle version: %s\n", siteName, newBundleVersion)
+}
+
 func init() {
 
 	// Add "site" prefix command
@@ -32,6 +53,16 @@ func init() {
 	}
 
 	// Add sub-commands
+
+	siteDeployCommand := &cobra.Command{
+		Use:   "deploy",
+		Short: "Update a site's associated bundle version",
+		Long:  "Updates a site's bundle to a specified version",
+		Run:   siteDeploy,
+	}
+	siteDeployCommand.PersistentFlags().StringP("bundle", "b", "", "The bundle version")
+	siteDeployCommand.PersistentFlags().StringP("site", "s", "", "The site name")
+
 	siteUpsertCmd := &cobra.Command{
 		Use:   "upsert",
 		Short: "Upserts data to a site",
@@ -42,7 +73,7 @@ func init() {
 	siteUpsertCmd.PersistentFlags().StringP("file", "f", "", "Filename of data to upsert")
 	siteUpsertCmd.PersistentFlags().StringP("collection", "c", "", "The collection to upsert")
 
-	siteCommand.AddCommand(siteUpsertCmd)
+	siteCommand.AddCommand(siteDeployCommand, siteUpsertCmd)
 	rootCmd.AddCommand(siteCommand)
 
 }
---
+++ apps/cli/pkg/command/site/siteDeploy.go
@@ -0,0 +1,40 @@
+package site
+
+import (
+	"errors"
+	"fmt"
+	"github.com/thecloudmasters/cli/pkg/wire"
+	"strings"
+)
+
+func SiteDeploy(siteName, newBundleVersion string) error {
+	app, err := wire.GetApp()
+	if err != nil {
+		return err
+	}
+
+	// Verify that the requested site exists
+	siteExists, err := wire.DoesSiteExist(app.ID, siteName)
+	if err != nil {
+		return errors.New("unable to verify that the requested site exists")
+	}
+	if !siteExists {
+		return errors.New("the requested site does not exist: " + siteName)
+	}
+
+	// The bundle version's unique key is appFullName:major:minor:patch
+	parts := strings.Split(strings.TrimPrefix(newBundleVersion, "v"), ".")
+	bundleUniqueKey := fmt.Sprintf("%s:%s:%s:%s", app.FullName, parts[0], parts[1], parts[2])
+	_, err = wire.Upsert("uesio/studio.site", []map[string]interface{}{
+		{
+			"uesio/studio.name": siteName,
+			"uesio/studio.bundle": map[string]interface{}{
+				"uesio/core.uniquekey": bundleUniqueKey,
+			},
+			"uesio/studio.app": map[string]interface{}{
+				"uesio/core.id": app.ID,
+			},
+		},
+	})
+	return err
+}
