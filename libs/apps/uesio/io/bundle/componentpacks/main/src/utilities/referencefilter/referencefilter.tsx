2a93857faf47318ed822b14116c16e4e905bdbc2
Zach McElrath <zach@ues.io>
2023-07-18 11:37:08 -0600

    remove redundant code

--- libs/apps/uesio/io/bundle/componentpacks/main/src/utilities/referencefilter/referencefilter.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/utilities/referencefilter/referencefilter.tsx
@@ -1,29 +1,19 @@
 import { FunctionComponent } from "react"
 import { definition, api, wire, collection } from "@uesio/ui"
 import ReferenceField, { ReferenceFieldOptions } from "../field/reference"
-import UserField, { UserFieldOptions } from "../field/user"
 
 interface ReferenceFilterProps extends definition.UtilityProps {
 	path: string
 	wire: wire.Wire
 	fieldMetadata: collection.Field
 	condition: wire.ValueConditionState
-	reference?: ReferenceFieldOptions
-	userreference?: UserFieldOptions
+	options?: ReferenceFieldOptions
 }
 
 const ReferenceFilter: FunctionComponent<ReferenceFilterProps> = (props) => {
-	const {
-		wire,
-		fieldMetadata,
-		context,
-		condition,
-		reference,
-		userreference,
-		path,
-	} = props
+	const { wire, fieldMetadata, context, condition, options, path } = props
 	const wireId = wire.getId()
-	return fieldMetadata.getType() === "REFERENCE" ? (
+	return (
 		<ReferenceField
 			path={path}
 			mode={"EDIT"}
@@ -31,37 +21,7 @@ const ReferenceFilter: FunctionComponent<ReferenceFilterProps> = (props) => {
 			fieldMetadata={fieldMetadata}
 			context={context}
 			variant={"uesio/io.filter"}
-			options={reference}
-			setValue={(value: string) => {
-				api.signal.runMany(
-					[
-						{
-							signal: "wire/SET_CONDITION",
-							wire: wireId,
-							condition: {
-								...condition,
-								value,
-								inactive: value === null,
-							},
-						},
-						{
-							signal: "wire/LOAD",
-							wires: [wireId],
-						},
-					],
-					context
-				)
-			}}
-		/>
-	) : (
-		<UserField
-			path={path}
-			mode={"EDIT"}
-			fieldId={fieldMetadata.getId()}
-			fieldMetadata={fieldMetadata}
-			context={context}
-			variant={"uesio/io.filter"}
-			options={userreference}
+			options={options}
 			setValue={(value: string) => {
 				api.signal.runMany(
 					[
