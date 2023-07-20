e80d6b2d99200de747b71b781d469f2c48ccaa1d
Zach McElrath <zach@ues.io>
2023-07-19 19:09:34 -0600

    fix(tests): Fix E2E test selector

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -186,7 +186,7 @@ function createWorkspaceInApp(workspaceName: string, appName: string) {
 }
 
 function createApp(appName: string) {
-	cy.clickButton("uesio/studio.home($root):uesio/io.button:add-app")
+	cy.clickButton("uesio/io.button:add-app")
 	cy.typeInInput("new-app-name", appName)
 	cy.typeInInput("new-app-description", "E2E Test App")
 	cy.clickButton("save-new-app")
