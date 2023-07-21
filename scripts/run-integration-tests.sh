25697cb0446da92bc4b81acb6d8a58ee5ee52066
Zach McElrath <zach@ues.io>
2023-07-21 12:50:54 -0600

    fix(conditions): Fix subquery condition metadata loading, add test case

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -44,6 +44,6 @@ hurl -k --variable host=studio.uesio-dev.com --variable port=3000 --test hurl_fi
 # FYI if you want to view the output of the request made by a specific hurl spec,
 # you can comment out the assertions of the last hurl request made in a hurl file, and then run the spec
 # without the "--test" flag, like this
-# hurl --very-verbose -k --variable host=studio.uesio-dev.com --variable port=3000 hurl_specs/wire_collection_dependencies.hurl
+# hurl --very-verbose -k --variable host=studio.uesio-dev.com --variable port=3000 hurl_specs/wire_load_group_condition.hurl
 
 cd ../../../..
