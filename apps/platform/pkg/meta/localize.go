c75cc0e7b5979f33b152fa6b0e9c9d6d4a296188
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2023-07-31 14:21:26 -0400

    local metadata prototype (#2828)
    * local metadata prototype
    
    * localize all studio routes
    
    * fix core routes too
    
    * oops
    
    * refactor and add tests
    
    * oops
    
    * better tests and roundtrip
    
    * add redirect tests

---
+++ apps/platform/pkg/meta/localize.go
@@ -0,0 +1,46 @@
+package meta
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+type SpecialYaml yaml.Node
+
+func isLocalNamespace(ns, localNamespace string) bool {
+	return ns == localNamespace || ns == "this/app"
+}
+
+// Takes a possibly localized namespace and turns it into a fully qualified,
+// (3rd person) namespace. If the namespace is not local, it is a no-op.
+func unLocalize(itemkey, localNamespace string) string {
+	if itemkey == "" {
+		return ""
+	}
+	namespace, name, err := ParseKeyWithDefault(itemkey, localNamespace)
+	if err != nil {
+		return ""
+	}
+
+	if isLocalNamespace(namespace, localNamespace) {
+		return fmt.Sprintf("%s.%s", localNamespace, name)
+	}
+
+	return itemkey
+}
+
+// Takes a possibly fully qualified namespace and turns it into a localized version
+func localize(itemkey, localNamespace string) string {
+	if itemkey == "" {
+		return ""
+	}
+	namespace, name, err := ParseKeyWithDefault(itemkey, localNamespace)
+	if err != nil {
+		return ""
+	}
+	if isLocalNamespace(namespace, localNamespace) {
+		return name
+	}
+	return itemkey
+}
