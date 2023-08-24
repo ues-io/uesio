d8a673c8dabccf1acbf38dd7a2ed10881267dbe4
Abel Jimenez Molla <abel@ues.io>
2023-08-24 11:12:02 +0200

    fix: field required property

--- apps/platform/pkg/datasource/fieldvalidations/required.go
+++ apps/platform/pkg/datasource/fieldvalidations/required.go
@@ -4,10 +4,28 @@ import "github.com/thecloudmasters/uesio/pkg/adapt"
 
 func ValidateRequiredField(field *adapt.FieldMetadata) ValidationFunc {
 	return func(change *adapt.ChangeItem) *adapt.SaveError {
+
+		isInsert := change.IsNew
 		val, err := change.FieldChanges.GetField(field.GetFullName())
-		if (change.IsNew && err != nil) || val == "" {
-			return adapt.NewSaveError(change.RecordKey, field.GetFullName(), "Field: "+field.Label+" is required")
+		fieldIsBeingUpdated := (err == nil)
+		//TO-DO we can add more types of empty values here
+		isEmpty := val == nil || val == ""
+
+		//It's an insert and the field is empty
+		if isInsert {
+			if isEmpty {
+				return adapt.NewSaveError(change.RecordKey, field.GetFullName(), "Field: "+field.Label+" is required")
+			}
+			return nil
 		}
+
+		//It's an update and the field is being updated
+		if fieldIsBeingUpdated {
+			if isEmpty {
+				return adapt.NewSaveError(change.RecordKey, field.GetFullName(), "Field: "+field.Label+" is required")
+			}
+		}
+
 		return nil
 	}
 }
