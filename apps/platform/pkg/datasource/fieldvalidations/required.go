c33f1bc1ef7c18b40e6a5796cf2b6fb6ace8fa8c
humandad <55447225+humandad@users.noreply.github.com>
2023-08-24 09:08:45 -0400

    make it easier to understand

--- apps/platform/pkg/datasource/fieldvalidations/required.go
+++ apps/platform/pkg/datasource/fieldvalidations/required.go
@@ -1,31 +1,22 @@
 package fieldvalidations
 
-import "github.com/thecloudmasters/uesio/pkg/adapt"
+import (
+	"github.com/thecloudmasters/uesio/pkg/adapt"
+)
 
 func ValidateRequiredField(field *adapt.FieldMetadata) ValidationFunc {
 	return func(change *adapt.ChangeItem) *adapt.SaveError {
-
-		isInsert := change.IsNew
+		valueIsUndefined := false
 		val, err := change.FieldChanges.GetField(field.GetFullName())
-		fieldIsBeingUpdated := (err == nil)
-		//TO-DO we can add more types of empty values here
-		isEmpty := val == nil || val == ""
-
-		//It's an insert and the field is empty
-		if isInsert {
-			if isEmpty {
-				return adapt.NewSaveError(change.RecordKey, field.GetFullName(), "Field: "+field.Label+" is required")
-			}
-			return nil
+		if err != nil {
+			valueIsUndefined = true
 		}
-
-		//It's an update and the field is being updated
-		if fieldIsBeingUpdated {
-			if isEmpty {
-				return adapt.NewSaveError(change.RecordKey, field.GetFullName(), "Field: "+field.Label+" is required")
-			}
+		valueIsEmpty := val == "" || val == nil
+		isMissingInsert := change.IsNew && (valueIsUndefined || valueIsEmpty)
+		isMissingUpdate := !change.IsNew && !valueIsUndefined && valueIsEmpty
+		if isMissingInsert || isMissingUpdate {
+			return adapt.NewSaveError(change.RecordKey, field.GetFullName(), "Field: "+field.Label+" is required")
 		}
-
 		return nil
 	}
 }
