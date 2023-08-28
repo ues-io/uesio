9855e1506877b06b3b376b24d3fd9154e1a54e96
Zach McElrath <zach@ues.io>
2023-08-28 10:35:16 -0400

    add tests for request caching

--- apps/platform/pkg/integ/web/web_test.go
+++ apps/platform/pkg/integ/web/web_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,9 +22,16 @@ func Test_RunAction(t *testing.T) {
 	var serveResponseBody, serveContentType string
 	var testInstance *testing.T
 	var requestAsserts func(t *testing.T, request *http.Request)
+	var countRequests map[string]uint32
 
 	// set up a mock server to handle our test requests
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqURL := r.URL.String()
+		if count, isPresent := countRequests[reqURL]; isPresent {
+			atomic.AddUint32(&count, 1)
+		} else {
+			countRequests[reqURL] = 1
+		}
 		if requestAsserts != nil && testInstance != nil {
 			requestAsserts(testInstance, r)
 		}
@@ -34,9 +42,7 @@ func Test_RunAction(t *testing.T) {
 		server.Close()
 	})()
 
-	tests := []struct {
-		name                string
-		integration         *meta.Integration
+	type args struct {
 		method              string
 		requestOptions      interface{}
 		response            string
@@ -44,6 +50,13 @@ func Test_RunAction(t *testing.T) {
 		requestAsserts      func(t *testing.T, request *http.Request)
 		responseAsserts     func(t *testing.T, responseData interface{})
 		wantErr             string
+		makeRequestNTimes   int
+	}
+
+	tests := []struct {
+		name        string
+		integration *meta.Integration
+		args        args
 	}{
 		{
 			"GET: it should handle JSON object responses",
@@ -52,24 +65,26 @@ func Test_RunAction(t *testing.T) {
 				Type:    "uesio/core.web",
 				Headers: map[string]string{},
 			},
-			"get",
-			&RequestOptions{
-				URL: "/test",
+			args{
+				method: "get",
+				requestOptions: &RequestOptions{
+					URL: "/test",
+				},
+				response:            `{"foo":"bar"}`,
+				responseContentType: "application/json",
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "GET", request.Method)
+					assert.Equal(t, "/test", request.URL.Path)
+					assert.EqualValues(t, uint32(1), countRequests[request.URL.String()])
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					if responseMap, ok := responseData.(*map[string]interface{}); ok {
+						assert.Equal(t, "bar", (*responseMap)["foo"])
+					} else {
+						assert.Fail(t, "response is not a map[string]interface{}")
+					}
+				},
 			},
-			`{"foo":"bar"}`,
-			"application/json",
-			func(t *testing.T, request *http.Request) {
-				assert.Equal(t, "GET", request.Method)
-				assert.Equal(t, "/test", request.URL.Path)
-			},
-			func(t *testing.T, responseData interface{}) {
-				if responseMap, ok := responseData.(*map[string]interface{}); ok {
-					assert.Equal(t, "bar", (*responseMap)["foo"])
-				} else {
-					assert.Fail(t, "response is not a map[string]interface{}")
-				}
-			},
-			"",
 		},
 		{
 			"GET: it should handle JSON array responses",
@@ -78,27 +93,57 @@ func Test_RunAction(t *testing.T) {
 				Type:    "uesio/core.web",
 				Headers: map[string]string{},
 			},
-			"get",
-			&RequestOptions{
-				URL: "/array",
-			},
-			`[{"foo":"bar"},{"hello":"world"}]`,
-			"text/json",
-			func(t *testing.T, request *http.Request) {
-				assert.Equal(t, "GET", request.Method)
-				assert.Equal(t, "/array", request.URL.Path)
-			},
-			func(t *testing.T, responseData interface{}) {
-				if responses, ok := responseData.(*[]interface{}); ok {
-					if response, isMap := (*responses)[1].(map[string]interface{}); isMap {
-						assert.Equal(t, "world", response["hello"])
+			args{
+				method: "get",
+				requestOptions: &RequestOptions{
+					URL: "/array",
+				},
+				response:            `[{"foo":"bar"},{"hello":"world"}]`,
+				responseContentType: "text/json",
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "GET", request.Method)
+					assert.Equal(t, "/array", request.URL.Path)
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					if responses, ok := responseData.(*[]interface{}); ok {
+						if response, isMap := (*responses)[1].(map[string]interface{}); isMap {
+							assert.Equal(t, "world", response["hello"])
+						}
+					} else {
+						assert.Fail(t, "response is not a valid array")
 					}
-				} else {
-					assert.Fail(t, "response is not a valid array")
-				}
-
+				},
 			},
-			"",
+		},
+		{
+			"GET: it should return a cached result if there was a previous request",
+			&meta.Integration{
+				BaseURL: server.URL,
+				Type:    "uesio/core.web",
+				Headers: map[string]string{},
+			},
+			args{
+				method: "get",
+				requestOptions: &RequestOptions{
+					URL:   "/test/cached",
+					Cache: true,
+				},
+				response:            `{"foo":"bar"}`,
+				responseContentType: "application/json",
+				makeRequestNTimes:   2,
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "GET", request.Method)
+					assert.Equal(t, "/test/cached", request.URL.Path)
+					assert.EqualValues(t, uint32(1), countRequests[request.URL.String()])
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					if responseMap, ok := responseData.(*map[string]interface{}); ok {
+						assert.Equal(t, "bar", (*responseMap)["foo"])
+					} else {
+						assert.Fail(t, "response is not a map[string]interface{}")
+					}
+				},
+			},
 		},
 		{
 			"GET: it should return other response types as raw data, and merge credentials into headers",
@@ -110,22 +155,23 @@ func Test_RunAction(t *testing.T) {
 					"Authorization": "${apikey}",
 				},
 			},
-			"get",
-			&RequestOptions{
-				URL: "/xml",
+			args{
+				method: "get",
+				requestOptions: &RequestOptions{
+					URL: "/xml",
+				},
+				response:            `<books/>`,
+				responseContentType: "text/xml",
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "GET", request.Method)
+					assert.Equal(t, "/xml", request.URL.Path)
+					assert.Equal(t, "text/xml", request.Header.Get("Accept"))
+					assert.Equal(t, "1234", request.Header.Get("Authorization"))
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					assert.Equal(t, `<books/>`, responseData)
+				},
 			},
-			`<books/>`,
-			"text/xml",
-			func(t *testing.T, request *http.Request) {
-				assert.Equal(t, "GET", request.Method)
-				assert.Equal(t, "/xml", request.URL.Path)
-				assert.Equal(t, "text/xml", request.Header.Get("Accept"))
-				assert.Equal(t, "1234", request.Header.Get("Authorization"))
-			},
-			func(t *testing.T, responseData interface{}) {
-				assert.Equal(t, `<books/>`, responseData)
-			},
-			"",
 		},
 		{
 			"POST: it should send a payload to the API",
@@ -138,26 +184,27 @@ func Test_RunAction(t *testing.T) {
 					"Authorization": "${apikey}",
 				},
 			},
-			"post",
-			&RequestOptions{
-				URL:  "/user/create",
-				Body: `{"first":"Luigi","last":"Vampa"}`,
+			args{
+				method: "post",
+				requestOptions: &RequestOptions{
+					URL:  "/user/create",
+					Body: `{"first":"Luigi","last":"Vampa"}`,
+				},
+				response:            `ok`,
+				responseContentType: "text/plain",
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "POST", request.Method)
+					assert.Equal(t, "/user/create", request.URL.Path)
+					assert.Equal(t, "text/plain", request.Header.Get("Accept"))
+					assert.Equal(t, "1234", request.Header.Get("Authorization"))
+					body, err := io.ReadAll(request.Body)
+					assert.Equal(t, nil, err)
+					assert.Equal(t, string(body), `{"first":"Luigi","last":"Vampa"}`)
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					assert.Equal(t, `ok`, responseData)
+				},
 			},
-			`ok`,
-			"text/plain",
-			func(t *testing.T, request *http.Request) {
-				assert.Equal(t, "POST", request.Method)
-				assert.Equal(t, "/user/create", request.URL.Path)
-				assert.Equal(t, "text/plain", request.Header.Get("Accept"))
-				assert.Equal(t, "1234", request.Header.Get("Authorization"))
-				body, err := io.ReadAll(request.Body)
-				assert.Equal(t, nil, err)
-				assert.Equal(t, string(body), `{"first":"Luigi","last":"Vampa"}`)
-			},
-			func(t *testing.T, responseData interface{}) {
-				assert.Equal(t, `ok`, responseData)
-			},
-			"",
 		},
 		{
 			"PUT: it should send a payload to the API",
@@ -169,25 +216,26 @@ func Test_RunAction(t *testing.T) {
 					"Content-Type": "text/json",
 				},
 			},
-			"put",
-			&RequestOptions{
-				URL:  "/user/111",
-				Body: `{"first":"Mario","last":"Vampa"}`,
+			args{
+				method: "put",
+				requestOptions: &RequestOptions{
+					URL:  "/user/111",
+					Body: `{"first":"Mario","last":"Vampa"}`,
+				},
+				response:            `ok`,
+				responseContentType: "text/plain",
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "PUT", request.Method)
+					assert.Equal(t, "/user/111", request.URL.Path)
+					assert.Equal(t, "text/plain", request.Header.Get("Accept"))
+					body, err := io.ReadAll(request.Body)
+					assert.Equal(t, nil, err)
+					assert.Equal(t, string(body), `{"first":"Mario","last":"Vampa"}`)
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					assert.Equal(t, `ok`, responseData)
+				},
 			},
-			`ok`,
-			"text/plain",
-			func(t *testing.T, request *http.Request) {
-				assert.Equal(t, "PUT", request.Method)
-				assert.Equal(t, "/user/111", request.URL.Path)
-				assert.Equal(t, "text/plain", request.Header.Get("Accept"))
-				body, err := io.ReadAll(request.Body)
-				assert.Equal(t, nil, err)
-				assert.Equal(t, string(body), `{"first":"Mario","last":"Vampa"}`)
-			},
-			func(t *testing.T, responseData interface{}) {
-				assert.Equal(t, `ok`, responseData)
-			},
-			"",
 		},
 		{
 			"PATCH: it should send a payload to the API",
@@ -199,27 +247,28 @@ func Test_RunAction(t *testing.T) {
 					"Content-Type": "text/json",
 				},
 			},
-			"patch",
-			&RequestOptions{
-				URL: "/user/111",
-				Body: map[string]interface{}{
-					"first": "Mario",
+			args{
+				method: "patch",
+				requestOptions: &RequestOptions{
+					URL: "/user/111",
+					Body: map[string]interface{}{
+						"first": "Mario",
+					},
+				},
+				response:            `ok`,
+				responseContentType: "text/plain",
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "PATCH", request.Method)
+					assert.Equal(t, "/user/111", request.URL.Path)
+					assert.Equal(t, "text/plain", request.Header.Get("Accept"))
+					body, err := io.ReadAll(request.Body)
+					assert.Equal(t, nil, err)
+					assert.Equal(t, string(body), `{"first":"Mario"}`)
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					assert.Equal(t, `ok`, responseData)
 				},
 			},
-			`ok`,
-			"text/plain",
-			func(t *testing.T, request *http.Request) {
-				assert.Equal(t, "PATCH", request.Method)
-				assert.Equal(t, "/user/111", request.URL.Path)
-				assert.Equal(t, "text/plain", request.Header.Get("Accept"))
-				body, err := io.ReadAll(request.Body)
-				assert.Equal(t, nil, err)
-				assert.Equal(t, string(body), `{"first":"Mario"}`)
-			},
-			func(t *testing.T, responseData interface{}) {
-				assert.Equal(t, `ok`, responseData)
-			},
-			"",
 		},
 		{
 			"DELETE: it should send a payload to the API",
@@ -231,25 +280,26 @@ func Test_RunAction(t *testing.T) {
 					"Content-Type": "text/json",
 				},
 			},
-			"delete",
-			&RequestOptions{
-				URL:  "/user/111",
-				Body: `{"__delete__":true}`,
+			args{
+				method: "delete",
+				requestOptions: &RequestOptions{
+					URL:  "/user/111",
+					Body: `{"__delete__":true}`,
+				},
+				response:            `deleted`,
+				responseContentType: "text/plain",
+				requestAsserts: func(t *testing.T, request *http.Request) {
+					assert.Equal(t, "DELETE", request.Method)
+					assert.Equal(t, "/user/111", request.URL.Path)
+					assert.Equal(t, "text/plain", request.Header.Get("Accept"))
+					body, err := io.ReadAll(request.Body)
+					assert.Equal(t, nil, err)
+					assert.Equal(t, string(body), `{"__delete__":true}`)
+				},
+				responseAsserts: func(t *testing.T, responseData interface{}) {
+					assert.Equal(t, `deleted`, responseData)
+				},
 			},
-			`deleted`,
-			"text/plain",
-			func(t *testing.T, request *http.Request) {
-				assert.Equal(t, "DELETE", request.Method)
-				assert.Equal(t, "/user/111", request.URL.Path)
-				assert.Equal(t, "text/plain", request.Header.Get("Accept"))
-				body, err := io.ReadAll(request.Body)
-				assert.Equal(t, nil, err)
-				assert.Equal(t, string(body), `{"__delete__":true}`)
-			},
-			func(t *testing.T, responseData interface{}) {
-				assert.Equal(t, `deleted`, responseData)
-			},
-			"",
 		},
 		{
 			"it should reject unknown action names",
@@ -258,30 +308,41 @@ func Test_RunAction(t *testing.T) {
 				Type:    "uesio/core.web",
 				Headers: map[string]string{},
 			},
-			"foooo",
-			&RequestOptions{
-				URL: "/users",
+			args{
+				method: "foooo",
+				requestOptions: &RequestOptions{
+					URL: "/users",
+				},
+				wantErr: "invalid action name for web integration",
 			},
-			"",
-			"",
-			nil,
-			nil,
-			"invalid action name for web integration",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			wi := &WebIntegration{}
-			serveResponseBody = tt.response
-			serveContentType = tt.responseContentType
-			requestAsserts = tt.requestAsserts
+			serveResponseBody = tt.args.response
+			serveContentType = tt.args.responseContentType
+			requestAsserts = tt.args.requestAsserts
 			testInstance = t
+			countRequests = map[string]uint32{}
 			conn, _ := wi.GetIntegrationConnection(tt.integration, session, creds)
-			actualResponse, err := conn.RunAction(tt.method, tt.requestOptions)
-			if tt.wantErr != "" {
-				assert.EqualError(t, err, tt.wantErr, "expected error for test case: "+tt.name)
-			} else if tt.responseAsserts != nil {
-				tt.responseAsserts(t, actualResponse)
+			reqsToMake := 1
+			if tt.args.makeRequestNTimes > 1 {
+				reqsToMake = tt.args.makeRequestNTimes
+			}
+			var actualResponse interface{}
+			var err error
+			for {
+				reqsToMake = reqsToMake - 1
+				actualResponse, err = conn.RunAction(tt.args.method, tt.args.requestOptions)
+				if reqsToMake == 0 {
+					break
+				}
+			}
+			if tt.args.wantErr != "" {
+				assert.EqualError(t, err, tt.args.wantErr, "expected error for test case: "+tt.name)
+			} else if tt.args.responseAsserts != nil {
+				tt.args.responseAsserts(t, actualResponse)
 			}
 		})
 	}
