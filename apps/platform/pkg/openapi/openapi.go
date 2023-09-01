41b20d440a206adbe66285ac97fc7c03ca032071
Zach McElrath <zach@ues.io>
2023-09-01 08:06:41 -0400

    more more more

--- apps/platform/pkg/openapi/openapi.go
+++ apps/platform/pkg/openapi/openapi.go
@@ -1,9 +1,12 @@
 package openapi
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/pb33f/libopenapi"
+	"github.com/pb33f/libopenapi/datamodel/high/base"
 	v3 "github.com/pb33f/libopenapi/datamodel/high/v3"
 	"github.com/thecloudmasters/uesio/pkg/meta"
 	"strings"
@@ -45,4 +48,186 @@ func LoadModelFromDataSource(ds *meta.DataSource) (*v3.Document, error) {
 	//v3Model.Index.
 
 	return &v3Model.Model, nil
+}
+
+func ParseResponseBodyUsingApiMetadata(collection meta.Group, data []byte, contentType string, statusCode int, responses *v3.Responses, spec *v3.Document) error {
+	if responses == nil {
+		return errors.New("no response schemas defined in API metadata")
+	}
+
+	// If the type is NOT JSON --- we don't support it right now
+	if !strings.Contains(contentType, "/json") {
+		return errors.New("unsupported response content type: " + contentType)
+	}
+
+	// First look for a code-specific response
+	response := responses.FindResponseByCode(statusCode)
+	if response == nil {
+		// Fallback to default response if no code-specific response is provided
+		response = responses.Default
+	}
+	if response == nil {
+		return errors.New("no response schemas defined in API metadata")
+	}
+	// If we have a valid response schema, and it is a reference, resolve the reference
+	if response.Content == nil {
+		// We have no schema to use. Dang
+		return errors.New("no content types defined for API response metadata")
+	}
+	mediaType := response.Content[contentType]
+	if mediaType == nil {
+		return errors.New("no content type match found for API response metadata")
+	}
+	schemaProxy := mediaType.Schema
+	if schemaProxy == nil {
+		return errors.New("no schema defined for API response metadata")
+	}
+	schema := ResolveSchemaProxy(schemaProxy, spec)
+	if schema == nil {
+		return errors.New("could not find schema for API response metadata")
+	}
+	// For now --- only handle one schema type, although in OpenAPI 3.1 Type is an array...
+	// so we'd need to introspect response / use a discriminator to figure it out anyway
+	schemaType := schema.Type[0]
+
+	//
+	// Cases:
+	// - Primitive (e.g. String)
+	// - Struct: there will be a well-defined schema in "properties",
+	//		with "required" array indicating which properties are required.
+	// - Map: keys are strings, and "additionalProperties.type" defines the value type:
+	//		- Value: Primitive
+	//		- Value: other Schema ($ref)
+	// - Polymorphic Structs
+	//		- "allOf", "oneOf", "anyOf" arrays should be provided to define the composite types
+	//		- discriminator used to distinguish which Model we are looking at
+	// - Array
+	//		- items object will define the Schema of the items
+	// parse the response as the object schema...
+
+	if schemaType == "object" {
+		// Deserialize into a map[string]interface{}
+		responseBody := map[string]interface{}{}
+		if err := json.NewDecoder(bytes.NewReader(data)).Decode(&responseBody); err != nil {
+			return errors.New("unable to deserialize response body into a map")
+		}
+		return AddCollectionItemFromObject(collection, responseBody, schema, spec)
+	} else if schemaType == "array" && schema.Items != nil {
+		// OpenAPI 3.0 schema value
+		var itemSchema *base.Schema
+		if schema.Items.IsA() {
+			itemSchema = ResolveSchemaProxy(schema.Items.A, spec)
+			if itemSchema == nil {
+				return errors.New("unable to resolve array items schema")
+			}
+			// If the item schema type is an object...
+			if itemSchema.Type[0] == "object" {
+				// Deserialize into a []map[string]object{}
+				var responseBody []map[string]interface{}
+				if err := json.NewDecoder(bytes.NewReader(data)).Decode(&responseBody); err != nil {
+					return errors.New("unable to deserialize response body into a an array of maps")
+				}
+				for _, rawItem := range responseBody {
+					if err := AddCollectionItemFromObject(collection, rawItem, itemSchema, spec); err != nil {
+						return err
+					}
+				}
+				return nil
+			} else {
+				return errors.New("unsupported response body schema type: " + itemSchema.Type[0])
+			}
+		} else {
+			// items is a boolean, there's nothing we can do with it
+			return errors.New("unable to deserialize array response due to unparseable items schema")
+		}
+	}
+	return nil
+}
+
+func AddCollectionItemFromObject(collection meta.Group, object map[string]interface{}, objectSchema *base.Schema, spec *v3.Document) error {
+	item := collection.NewItem()
+	// Iterate over the object and process it against the object schema
+	for propName, propValue := range object {
+		// See if we have a matching property in the object schema
+		property := objectSchema.Properties[propName]
+		var propertySchema *base.Schema
+		fieldId := propName
+		if property != nil {
+			propertySchema = ResolveSchemaProxy(property, spec)
+		}
+		if propertySchema != nil {
+			// Get the Uesio field id from extensions
+			uesioFieldId := propertySchema.Extensions["x-uesio-field-id"]
+			if uesioFieldId != nil && uesioFieldId != "" {
+				if stringValue, isString := uesioFieldId.(string); isString {
+					fieldId = stringValue
+				}
+			}
+		}
+		// Dump the value over. TODO: maybe do a better job of creating sub items?
+		if err := item.SetField(fieldId, propValue); err != nil {
+			return fmt.Errorf("could not set value of field %s to %v", fieldId, propValue)
+		}
+	}
+	if err := collection.AddItem(item); err != nil {
+		return errors.New("unable to add item to collection: " + err.Error())
+	}
+	return nil
+}
+
+func ResolveSchemaProxy(schemaProxy *base.SchemaProxy, spec *v3.Document) *base.Schema {
+	// If this is an inline schema, fetch it directly
+	if !schemaProxy.IsReference() {
+		return schemaProxy.Schema()
+	}
+	// Fetch the schema from elsewhere in the spec
+	if spec.Components == nil || spec.Components.Schemas == nil {
+		return nil
+	}
+	match := spec.Components.Schemas[schemaProxy.GetReference()]
+	if match == nil {
+		return nil
+	}
+	return match.Schema()
+}
+
+func ParseResponseBodyUsingBestGuess(collection meta.Group, data []byte, contentType string, ds *meta.DataSource) error {
+	var responseBody interface{}
+	if strings.Contains(contentType, "/json") {
+		// If it starts with a curly brace, treat it as JSON object
+		first := string(data[0])
+		if first == "{" {
+			responseBody = &map[string]interface{}{}
+		} else if first == "[" {
+			// Otherwise, assume it's a JSON array
+			second := string(data[1])
+			if second == "{" {
+				responseBody = &[]map[string]interface{}{}
+			} else {
+				responseBody = &[]interface{}{}
+			}
+		}
+		err := json.NewDecoder(bytes.NewReader(data)).Decode(responseBody)
+		if err != nil {
+			return err
+		}
+	}
+
+	switch _ := responseBody.(type) {
+	case map[string]interface{}:
+		// We only have one Item. Assume that all values correspond to the data source field metadata
+		//for fieldName, fieldValue := range val {
+		//
+		//}
+		return nil
+	case []map[string]interface{}:
+		// We have multiple Items. Assume that all values correspond to the data source field metadata
+		//for _, rawItem := range val {
+		//	for fieldName, fieldValue := range rawItem {
+		//
+		//	}
+		//}
+		return nil
+	}
+	return nil
 }
