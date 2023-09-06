c1e6fc374d048a74051216e0dd1d8d7a176a979d
Zach McElrath <zach@ues.io>
2023-09-06 11:31:46 -0400

    fix: Bug fixes and UX improvements for Data Manager

--- libs/ui/src/bands/collection/types.ts
+++ libs/ui/src/bands/collection/types.ts
@@ -12,6 +12,7 @@ export type PlainCollection = {
 	hasAllFields?: boolean
 	label: string
 	pluralLabel: string
+	uniqueKey?: string[]
 }
 
 export type PlainCollectionMap = {
@@ -21,5 +22,17 @@ export type PlainCollectionMap = {
 const ID_FIELD = "uesio/core.id"
 const UNIQUE_KEY_FIELD = "uesio/core.uniquekey"
 const UPDATED_AT_FIELD = "uesio/core.updatedat"
+const CREATED_AT_FIELD = "uesio/core.createdat"
+const UPDATED_BY_FIELD = "uesio/core.updatedby"
+const CREATED_BY_FIELD = "uesio/core.createdby"
+const OWNER_FIELD = "uesio/core.owner"
 
-export { ID_FIELD, UNIQUE_KEY_FIELD, UPDATED_AT_FIELD }
+export {
+	ID_FIELD,
+	UNIQUE_KEY_FIELD,
+	OWNER_FIELD,
+	UPDATED_AT_FIELD,
+	UPDATED_BY_FIELD,
+	CREATED_AT_FIELD,
+	CREATED_BY_FIELD,
+}
