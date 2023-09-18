c682287e78e84a4547037a52030d08a70fa5cbde
Abel Jimenez Molla <abel@ues.io>
2023-09-18 11:50:56 +0200

    Clone action creates a new uesio id

--- libs/apps/uesio/builder/bundle/componentpacks/main/src/api/defapi.ts
+++ libs/apps/uesio/builder/bundle/componentpacks/main/src/api/defapi.ts
@@ -217,7 +217,25 @@ const clone = (context: ctx.Context, path: FullPath) => {
 	const parentNode = getNodeAtPath(parentPath, yamlDoc.contents)
 	if (!yaml.isSeq(parentNode)) return
 	const items = parentNode.items
-	items.splice(index, 0, items[index])
+	const itemToClone = items[index] as Node
+	//
+	if (!yaml.isMap(itemToClone)) {
+		return
+	}
+
+	console.log("GET GOT a NODE")
+	const jsonItem = itemToClone.toJSON()
+	const itemToCloneComponentType = Object.keys(jsonItem)[0]
+	console.log(itemToCloneComponentType)
+	const itemToCloneProps = jsonItem[itemToCloneComponentType]
+	console.log({ itemToCloneProps })
+	const uesioID = itemToCloneProps["uesio.id"]
+	const newKey = (uesioID || "") + (Math.floor(Math.random() * 60) + 1)
+	console.log({ newKey })
+	const test = itemToClone.clone()
+	test.setIn(["uesio.id"], newKey)
+	items.splice(index, 0, test)
+	//
 
 	setMetadataValue(context, path, yamlDoc)
 }
