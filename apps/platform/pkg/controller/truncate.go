bc9afd3ef9568c86b2b57e1bd3afa9471106f018
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2023-09-20 17:09:06 +0200

    fix(studio): Remove all Workspace data bot does not work (#3150)

--- apps/platform/pkg/controller/truncate.go
+++ apps/platform/pkg/controller/truncate.go
@@ -1,22 +1,19 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/thecloudmasters/uesio/pkg/controller/file"
-	"github.com/thecloudmasters/uesio/pkg/datasource"
 	"github.com/thecloudmasters/uesio/pkg/logger"
 	"github.com/thecloudmasters/uesio/pkg/meta"
 	"github.com/thecloudmasters/uesio/pkg/truncate"
-	"net/http"
 
 	"github.com/thecloudmasters/uesio/pkg/middleware"
 )
 
 func Truncate(w http.ResponseWriter, r *http.Request) {
 	session := middleware.GetSession(r)
-	connection, err := datasource.GetPlatformConnection(nil, session, nil)
-	if err == nil {
-		err = truncate.TruncateWorkspaceData(session, connection)
-	}
+	err := truncate.TruncateWorkspaceData(session)
 	if err != nil {
 		var responseCode int
 		switch err.(type) {
