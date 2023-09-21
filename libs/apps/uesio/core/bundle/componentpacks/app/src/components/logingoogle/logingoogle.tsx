dc028c3cc3e4c91da081a77439b92aed3d7b2cf3
humandad <55447225+humandad@users.noreply.github.com>
2023-09-21 14:42:26 -0400

    Allow admin create login for google auth

--- libs/apps/uesio/core/bundle/componentpacks/app/src/components/logingoogle/logingoogle.tsx
+++ libs/apps/uesio/core/bundle/componentpacks/app/src/components/logingoogle/logingoogle.tsx
@@ -1,8 +1,30 @@
-import { component, definition } from "@uesio/ui"
+import { definition, api, signal } from "@uesio/ui"
+import LoginGoogleUtility from "../../utilities/logingoogle/logingoogle"
 
-const LoginGoogle: definition.UC = ({ context, definition }) => {
-	const LoginGoogleUtility = component.getUtility("uesio/core.logingoogle")
-	return <LoginGoogleUtility context={context} mode={definition.mode} />
+type ButtonDefinition = {
+	onLoginSignals?: signal.SignalDefinition[]
+}
+
+type GoogleLoginResponse = {
+	credential: string
+	client_id: string
+}
+
+const LoginGoogle: definition.UC<ButtonDefinition> = ({
+	context,
+	definition,
+}) => {
+	const handler = (loginresponse: GoogleLoginResponse) => {
+		if (!definition.onLoginSignals) return
+		api.signal.runMany(
+			definition.onLoginSignals,
+			context.addComponentFrame("uesio/core.logingoogle", {
+				credential: loginresponse.credential,
+				client_id: loginresponse.client_id,
+			})
+		)
+	}
+	return <LoginGoogleUtility context={context} onLogin={handler} />
 }
 
 export default LoginGoogle
