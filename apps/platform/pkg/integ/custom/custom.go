a0f4ed535761b073346cb4af8e5a56eb089e3e57
Zach McElrath <zach@ues.io>
2023-09-25 10:42:38 -0400

    feat: Custom Integration Actions

--- apps/platform/pkg/integ/custom/custom.go
+++ apps/platform/pkg/integ/custom/custom.go
@@ -1,9 +1,11 @@
 package custom
 
 import (
-	"errors"
+	"fmt"
 
 	"github.com/thecloudmasters/uesio/pkg/adapt"
+	"github.com/thecloudmasters/uesio/pkg/bundle"
+	"github.com/thecloudmasters/uesio/pkg/datasource"
 	"github.com/thecloudmasters/uesio/pkg/meta"
 	"github.com/thecloudmasters/uesio/pkg/sess"
 )
@@ -34,5 +36,28 @@ func (c CustomIntegrationConnection) GetIntegration() *meta.Integration {
 }
 
 func (c CustomIntegrationConnection) RunAction(actionName string, requestOptions interface{}) (interface{}, error) {
-	return nil, errors.New("no actions implemented")
+	// Load the integration action bundle with this action name
+	action, err := meta.NewIntegrationAction(c.integration.GetKey(), actionName)
+	if err != nil {
+		return nil, err
+	}
+	err = bundle.Load(action, c.session, nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not find integration action with name %s for integration %s", actionName, c.integration.GetKey())
+	}
+	// Load the bundle's associated bot
+	botNamespace, botName, err := meta.ParseKey(action.BotRef)
+	if err != nil {
+		return nil, fmt.Errorf("invalid bot name for integration action %s", actionName)
+	}
+	
+	// convert requestOptions into a params map
+	params := make(map[string]interface{})
+
+	outputs, err := datasource.CallListenerBot(botNamespace, botName, params, nil, c.session)
+	if err != nil {
+		return nil, err
+	}
+
+	return outputs, nil
 }
---
+++ apps/platform/pkg/meta/integrationaction.go
@@ -0,0 +1,87 @@
+package meta
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+func NewIntegrationAction(integrationName, actionName string) (*IntegrationAction, error) {
+	namespace, name, err := ParseKey(actionName)
+	if err != nil {
+		return nil, errors.New("Bad Key for IntegrationAction: " + integrationName + ": " + actionName)
+	}
+	return NewBaseIntegrationAction(integrationName, namespace, name), nil
+}
+
+func NewBaseIntegrationAction(integrationName, namespace, name string) *IntegrationAction {
+	return &IntegrationAction{
+		BundleableBase: NewBase(namespace, name),
+		IntegrationRef: integrationName,
+	}
+}
+
+type IntegrationAction struct {
+	BuiltIn        `yaml:",inline"`
+	BundleableBase `yaml:",inline"`
+	IntegrationRef string `yaml:"-" json:"uesio/studio.integration"`
+	BotRef         string `yaml:"-" json:"uesio/studio.bot"`
+}
+
+type IntegrationActionWrapper IntegrationAction
+
+func (ia *IntegrationAction) GetCollectionName() string {
+	return INTEGRATION_ACTION_COLLECTION_NAME
+}
+
+func (ia *IntegrationAction) GetBundleFolderName() string {
+	return INTEGRATION_ACTION_FOLDER_NAME
+}
+
+func (ia *IntegrationAction) GetDBID(workspace string) string {
+	return fmt.Sprintf("%s:%s:%s", workspace, ia.IntegrationRef, ia.Name)
+}
+
+func (ia *IntegrationAction) GetKey() string {
+	return fmt.Sprintf("%s:%s.%s", ia.IntegrationRef, ia.Namespace, ia.Name)
+}
+
+func (ia *IntegrationAction) GetPath() string {
+	integrationNamespace, integrationName, _ := ParseKey(ia.IntegrationRef)
+	nsUser, appName, _ := ParseNamespace(integrationNamespace)
+	return filepath.Join(nsUser, appName, integrationName, ia.Name) + ".yaml"
+}
+
+func (ia *IntegrationAction) SetField(actionName string, value interface{}) error {
+	return StandardFieldSet(ia, actionName, value)
+}
+
+func (ia *IntegrationAction) GetField(actionName string) (interface{}, error) {
+	return StandardFieldGet(ia, actionName)
+}
+
+func (ia *IntegrationAction) Loop(iter func(string, interface{}) error) error {
+	return StandardItemLoop(ia, iter)
+}
+
+func (ia *IntegrationAction) Len() int {
+	return StandardItemLen(ia)
+}
+
+func (ia *IntegrationAction) UnmarshalYAML(node *yaml.Node) error {
+	err := validateNodeName(node, ia.Name)
+	if err != nil {
+		return err
+	}
+	return node.Decode((*IntegrationActionWrapper)(ia))
+}
+
+func (ia *IntegrationAction) MarshalYAML() (interface{}, error) {
+	return (*IntegrationActionWrapper)(ia), nil
+}
+
+func (c *IntegrationAction) IsPublic() bool {
+	return true
+}
