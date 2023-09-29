218fae5e3b16ce81a2fa2ae31340d825c14a9215
Zach McElrath <zach@ues.io>
2023-09-29 15:18:20 -0400

    implement Permissions for Integration Actions

--- apps/platform/pkg/meta/permissionset.go
+++ apps/platform/pkg/meta/permissionset.go
@@ -30,6 +30,13 @@ type CollectionPermission struct {
 
 type CollectionPermissionMap map[string]CollectionPermission
 
+type IntegrationPermission struct {
+	AllowAll    bool            `yaml:"allowAllActions" json:"allowAllActions"`
+	ActionPerms map[string]bool `yaml:"actions" json:"actions"`
+}
+
+type IntegrationPermissionMap map[string]IntegrationPermission
+
 type FieldPermission struct {
 	Read bool `yaml:"read" json:"read"`
 	Edit bool `yaml:"edit" json:"edit"`
@@ -75,21 +82,23 @@ func (cpm *CollectionPermissionMap) UnmarshalYAML(node *yaml.Node) error {
 }
 
 type PermissionSet struct {
-	BuiltIn             `yaml:",inline"`
-	BundleableBase      `yaml:",inline"`
-	NamedRefs           map[string]bool         `yaml:"named" json:"uesio/studio.namedrefs"`
-	ViewRefs            map[string]bool         `yaml:"views" json:"uesio/studio.viewrefs"`
-	CollectionRefs      CollectionPermissionMap `yaml:"collections" json:"uesio/studio.collectionrefs"`
-	RouteRefs           map[string]bool         `yaml:"routes" json:"uesio/studio.routerefs"`
-	FileRefs            map[string]bool         `yaml:"files" json:"uesio/studio.filerefs"`
-	BotRefs             map[string]bool         `yaml:"bots" json:"uesio/studio.botrefs"`
-	AllowAllBots        bool                    `yaml:"allowallbots" json:"uesio/studio.allowallbots"`
-	AllowAllCollections bool                    `yaml:"allowallcollections" json:"uesio/studio.allowallcollections"`
-	AllowAllViews       bool                    `yaml:"allowallviews" json:"uesio/studio.allowallviews"`
-	AllowAllRoutes      bool                    `yaml:"allowallroutes" json:"uesio/studio.allowallroutes"`
-	AllowAllFiles       bool                    `yaml:"allowallfiles" json:"uesio/studio.allowallfiles"`
-	ModifyAllRecords    bool                    `yaml:"modifyallrecords" json:"uesio/studio.modifyallrecords"`
-	ViewAllRecords      bool                    `yaml:"viewallrecords" json:"uesio/studio.viewallrecords"`
+	BuiltIn                    `yaml:",inline"`
+	BundleableBase             `yaml:",inline"`
+	NamedRefs                  map[string]bool          `yaml:"named" json:"uesio/studio.namedrefs"`
+	ViewRefs                   map[string]bool          `yaml:"views" json:"uesio/studio.viewrefs"`
+	CollectionRefs             CollectionPermissionMap  `yaml:"collections" json:"uesio/studio.collectionrefs"`
+	RouteRefs                  map[string]bool          `yaml:"routes" json:"uesio/studio.routerefs"`
+	FileRefs                   map[string]bool          `yaml:"files" json:"uesio/studio.filerefs"`
+	BotRefs                    map[string]bool          `yaml:"bots" json:"uesio/studio.botrefs"`
+	IntegrationActionRefs      IntegrationPermissionMap `yaml:"integrationActions" json:"uesio/studio.integrationactionrefs"`
+	AllowAllBots               bool                     `yaml:"allowallbots" json:"uesio/studio.allowallbots"`
+	AllowAllCollections        bool                     `yaml:"allowallcollections" json:"uesio/studio.allowallcollections"`
+	AllowAllViews              bool                     `yaml:"allowallviews" json:"uesio/studio.allowallviews"`
+	AllowAllRoutes             bool                     `yaml:"allowallroutes" json:"uesio/studio.allowallroutes"`
+	AllowAllFiles              bool                     `yaml:"allowallfiles" json:"uesio/studio.allowallfiles"`
+	AllowAllIntegrationActions bool                     `yaml:"allowallintegrationactions" json:"uesio/studio.allowallintegrationactions"`
+	ModifyAllRecords           bool                     `yaml:"modifyallrecords" json:"uesio/studio.modifyallrecords"`
+	ViewAllRecords             bool                     `yaml:"viewallrecords" json:"uesio/studio.viewallrecords"`
 }
 
 type PermissionSetWrapper PermissionSet
@@ -148,6 +157,15 @@ func (ps *PermissionSet) HasPermission(check *PermissionSet) bool {
 		}
 	}
 
+	if !ps.AllowAllIntegrationActions {
+		for checkIntegrationName, _ := range check.IntegrationActionRefs {
+			_, hasEntry := ps.IntegrationActionRefs[checkIntegrationName]
+			if !hasEntry {
+				return false
+			}
+		}
+	}
+
 	if !ps.AllowAllViews {
 		for key, value := range check.ViewRefs {
 			if value {
@@ -187,6 +205,23 @@ func (ps *PermissionSet) HasPermission(check *PermissionSet) bool {
 	}
 
 	return true
+}
+
+func (ps *PermissionSet) CanRunIntegrationAction(integrationName, actionName string) bool {
+	if ps.AllowAllIntegrationActions {
+		return true
+	}
+	integrationPerms, hasIntegrationPerms := ps.IntegrationActionRefs[integrationName]
+	if !hasIntegrationPerms {
+		return false
+	}
+	if integrationPerms.AllowAll {
+		return true
+	}
+	if integrationPerms.ActionPerms == nil {
+		return false
+	}
+	return integrationPerms.ActionPerms[actionName]
 }
 
 func (ps *PermissionSet) HasCollectionReadPermission(key string) bool {
@@ -258,13 +293,9 @@ func (ps *PermissionSet) HasNamedPermission(namedPermission string) bool {
 }
 
 func FlattenPermissions(permissionSets []PermissionSet) *PermissionSet {
-	botPerms := map[string]bool{}
-	namedPerms := map[string]bool{}
-	viewPerms := map[string]bool{}
-	routePerms := map[string]bool{}
-	filePerms := map[string]bool{}
-	collectionPerms := CollectionPermissionMap{}
+
 	allowAllBots := false
+	allowAllIntegrationActions := false
 	allowAllViews := false
 	allowAllRoutes := false
 	allowAllFiles := false
@@ -272,47 +303,15 @@ func FlattenPermissions(permissionSets []PermissionSet) *PermissionSet {
 	modifyAllRecords := false
 	viewAllRecords := false
 
+	namedPerms := map[string]bool{}
+
+	// Do the "AllowAll" checks first so that we can bypass other checks
 	for _, permissionSet := range permissionSets {
-		for key, value := range permissionSet.NamedRefs {
-			if value {
-				namedPerms[key] = true
-			}
-		}
-		for key, value := range permissionSet.BotRefs {
-			if value {
-				botPerms[key] = true
-			}
-		}
-		for key, value := range permissionSet.ViewRefs {
-			if value {
-				viewPerms[key] = true
-			}
-		}
-		for key, value := range permissionSet.RouteRefs {
-			if value {
-				routePerms[key] = true
-			}
-		}
-		for key, value := range permissionSet.FileRefs {
-			if value {
-				filePerms[key] = true
-			}
-		}
-		for key, value := range permissionSet.CollectionRefs {
-			if existingVal, ok := collectionPerms[key]; !ok {
-				collectionPerms[key] = value
-			} else {
-				existingVal.ModifyAll = existingVal.ModifyAll || value.ModifyAll
-				existingVal.ViewAll = existingVal.ViewAll || value.ViewAll
-				existingVal.Create = existingVal.Create || value.Create
-				existingVal.Delete = existingVal.Delete || value.Delete
-				existingVal.Edit = existingVal.Edit || value.Edit
-				existingVal.Read = existingVal.Read || value.Read
-				collectionPerms[key] = existingVal
-			}
-		}
 		if permissionSet.AllowAllBots {
 			allowAllBots = true
+		}
+		if permissionSet.AllowAllIntegrationActions {
+			allowAllIntegrationActions = true
 		}
 		if permissionSet.AllowAllViews {
 			allowAllViews = true
@@ -332,34 +331,140 @@ func FlattenPermissions(permissionSets []PermissionSet) *PermissionSet {
 		if permissionSet.ViewAllRecords {
 			viewAllRecords = true
 		}
+		// Also do the named perm checks because there's no "Allow All" equivalent
+		for key, value := range permissionSet.NamedRefs {
+			if value {
+				namedPerms[key] = true
+			}
+		}
+	}
+
+	var botPerms, filePerms, routePerms, viewPerms map[string]bool
+	var integrationActionPerms IntegrationPermissionMap
+	var collectionPerms CollectionPermissionMap
+
+	// Only initialize the maps if the "AllowAll" props are false
+	if !allowAllBots {
+		botPerms = map[string]bool{}
+	}
+	if !allowAllCollections {
+		collectionPerms = CollectionPermissionMap{}
+	}
+	if !allowAllIntegrationActions {
+		integrationActionPerms = IntegrationPermissionMap{}
+	}
+	if !allowAllFiles {
+		filePerms = map[string]bool{}
+	}
+	if !allowAllRoutes {
+		routePerms = map[string]bool{}
+	}
+	if !allowAllViews {
+		viewPerms = map[string]bool{}
+	}
+
+	for _, permissionSet := range permissionSets {
+		// Don't bother with Bot-specific checks if we can run all
+		if !allowAllBots {
+			for key, value := range permissionSet.BotRefs {
+				if value {
+					botPerms[key] = true
+				}
+			}
+		}
+		// Don't bother with integration-specific checks if we can run all
+		if !allowAllIntegrationActions {
+			for integrationName, integrationPerms := range permissionSet.IntegrationActionRefs {
+				if existingVal, ok := integrationActionPerms[integrationName]; !ok {
+					integrationActionPerms[integrationName] = integrationPerms
+				} else {
+					existingVal.AllowAll = existingVal.AllowAll || integrationPerms.AllowAll
+					// If AllowAll, we don't need to track actions-specific perms anymore, so clear them out
+					if existingVal.AllowAll {
+						existingVal.ActionPerms = nil
+						continue
+					}
+					// Otherwise we need to keep track of action-specific perms
+					if existingVal.ActionPerms == nil {
+						existingVal.ActionPerms = integrationPerms.ActionPerms
+					} else if integrationActionPerms != nil {
+						for actionName, isAllowed := range integrationPerms.ActionPerms {
+							// Only add an entry if it is allowed
+							if isAllowed {
+								existingVal.ActionPerms[actionName] = existingVal.ActionPerms[actionName] || isAllowed
+							}
+						}
+					}
+				}
+			}
+		}
+		if !allowAllViews {
+			for key, value := range permissionSet.ViewRefs {
+				if value {
+					viewPerms[key] = true
+				}
+			}
+		}
+		if !allowAllRoutes {
+			for key, value := range permissionSet.RouteRefs {
+				if value {
+					routePerms[key] = true
+				}
+			}
+		}
+		if !allowAllFiles {
+			for key, value := range permissionSet.FileRefs {
+				if value {
+					filePerms[key] = true
+				}
+			}
+		}
+		if !allowAllCollections {
+			for key, value := range permissionSet.CollectionRefs {
+				if existingVal, ok := collectionPerms[key]; !ok {
+					collectionPerms[key] = value
+				} else {
+					existingVal.ModifyAll = existingVal.ModifyAll || value.ModifyAll
+					existingVal.ViewAll = existingVal.ViewAll || value.ViewAll
+					existingVal.Create = existingVal.Create || value.Create
+					existingVal.Delete = existingVal.Delete || value.Delete
+					existingVal.Edit = existingVal.Edit || value.Edit
+					existingVal.Read = existingVal.Read || value.Read
+					collectionPerms[key] = existingVal
+				}
+			}
+		}
 	}
 
 	return &PermissionSet{
-		NamedRefs:           namedPerms,
-		ViewRefs:            viewPerms,
-		BotRefs:             botPerms,
-		RouteRefs:           routePerms,
-		FileRefs:            filePerms,
-		CollectionRefs:      collectionPerms,
-		AllowAllBots:        allowAllBots,
-		AllowAllViews:       allowAllViews,
-		AllowAllRoutes:      allowAllRoutes,
-		AllowAllFiles:       allowAllFiles,
-		AllowAllCollections: allowAllCollections,
-		ModifyAllRecords:    modifyAllRecords,
-		ViewAllRecords:      viewAllRecords,
+		NamedRefs:                  namedPerms,
+		ViewRefs:                   viewPerms,
+		BotRefs:                    botPerms,
+		IntegrationActionRefs:      integrationActionPerms,
+		RouteRefs:                  routePerms,
+		FileRefs:                   filePerms,
+		CollectionRefs:             collectionPerms,
+		AllowAllBots:               allowAllBots,
+		AllowAllIntegrationActions: allowAllIntegrationActions,
+		AllowAllViews:              allowAllViews,
+		AllowAllRoutes:             allowAllRoutes,
+		AllowAllFiles:              allowAllFiles,
+		AllowAllCollections:        allowAllCollections,
+		ModifyAllRecords:           modifyAllRecords,
+		ViewAllRecords:             viewAllRecords,
 	}
 }
 
-// Returns a permissionset that has the maximum permissions possible
+// GetAdminPermissionSet Returns a PermissionSet that has the maximum permissions possible
 func GetAdminPermissionSet() *PermissionSet {
 	return &PermissionSet{
-		AllowAllBots:        true,
-		AllowAllViews:       true,
-		AllowAllRoutes:      true,
-		AllowAllFiles:       true,
-		AllowAllCollections: true,
-		ModifyAllRecords:    true,
-		ViewAllRecords:      true,
+		AllowAllBots:               true,
+		AllowAllIntegrationActions: true,
+		AllowAllViews:              true,
+		AllowAllRoutes:             true,
+		AllowAllFiles:              true,
+		AllowAllCollections:        true,
+		ModifyAllRecords:           true,
+		ViewAllRecords:             true,
 	}
 }
