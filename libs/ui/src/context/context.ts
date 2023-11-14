843e5d6a283799da70e89dc501ba5ac288ffe428
Wessel <38309266+Wesselvdp@users.noreply.github.com>
2023-11-14 17:08:21 +0100

    Update libs/ui/src/context/context.ts
    Co-authored-by: Zach McElrath <zach@ues.io>

--- libs/ui/src/context/context.ts
+++ libs/ui/src/context/context.ts
@@ -629,14 +629,16 @@ class Context {
 		return `${result ?? ""}`
 	}
 
-	mergeBoolean = (template: Mergeable) => {
+	mergeBoolean = (template: Mergeable, defaultValue?: boolean) => {
 		const result = this.merge(template)
-		if (typeof result !== "boolean") {
-			throw new Error(
-				`Merge failed: result is of type ${typeof result} and cannot be returned as a boolean, please check your merge.`
-			)
+		if (typeof result === "boolean") {
+			return result
 		}
-		return result
+		// If a defaultValue is provided, return that instead of throwing an error
+		if (typeof defaultValue === "boolean") return defaultValue;
+		throw new Error(
+			`Merge failed: result is of type ${typeof result} and cannot be returned as a boolean, please check your merge.`
+		)
 	}
 
 	mergeDeep = (value: DeepMergeable) => {
