fcd609f150fbc2952185f96d1cddc8e947c70686
Zach McElrath <zach@ues.io>
2023-11-21 07:58:52 -0500

    fix E2E tests, cleanup Test authentication panel

--- cypress/e2e/oauth2_authentication.cy.ts
+++ cypress/e2e/oauth2_authentication.cy.ts
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
 
-import Sinon from "cypress/types/sinon"
 import { getBaseUrl, getWorkspaceBasePath } from "../support/paths"
 
 describe("OAuth 2 Authorization Code flow", () => {
@@ -28,16 +27,15 @@ describe("OAuth 2 Authorization Code flow", () => {
 	context("Workspace mode", () => {
 		it("should perform the OAuth authorization code flow to get credentials", () => {
 			// Go to the Integration detail page
-			let stub: Sinon.SinonStub | undefined
 			cy.visit(
 				getWorkspaceBasePath(appName, workspaceName) +
 					integrationDetailRoute,
 				{
 					headers: {},
-					// Stub window.open, we need to verify that it was invoked,
-					// and Cypress doesn't actually support navigation to other windows
-					onBeforeLoad(win) {
-						stub = cy.stub(win, "open")
+					onBeforeLoad: (window) => {
+						cy.stub(window, "open")
+							.as("open")
+							.callsFake(cy.stub().as("openFunc"))
 					},
 				}
 			)
@@ -55,57 +53,50 @@ describe("OAuth 2 Authorization Code flow", () => {
 			// Click the Authenticate button. Verify that the OAuth authorize URL was opened
 			authBtn().should("exist").click()
 			cy.window().its("open").should("be.called")
-			const callbackUrl = `${getBaseUrl()}/site/oauth2/callback`
-			if (stub) {
-				const openedUrl = stub.firstCall.args[0]
-				expect(openedUrl).to.contain(
-					`${getWorkspaceBasePath(
-						appName,
-						workspaceName
-					)}/routes/path/uesio/tests/mock_oauth_authorize_url`
+			cy.get("@open").should("have.been.calledOnce")
+			cy.get("@openFunc").then((func) => {
+				const theFunc = func as unknown as sinon.SinonStub
+				expect(theFunc).to.have.been.calledOnce
+				const [url, queryString] = theFunc.firstCall.args[0].split("?")
+				cy.log("url", url)
+				// Verify that the URL is correct
+				expect(url).to.contain(
+					"/workspace/uesio/tests/dev/routes/path/uesio/tests/mock_oauth_authorize_url"
 				)
-			}
-			// Now open the callback URL manually, to simulate what should have happened by opening the window
-			// This state is base64 encoded, and contains the integration ID, a nonce, the workspace name, and app name.
-			// You can generate something like this using state_test.go, marshalling a state and then grabbing the encoded string
-			const state =
-				"eyJuIjoiMTIzIiwiaSI6InVlc2lvL3Rlc3RzLm9hdXRoMl9hdXRob3JpemF0aW9uX2NvZGVfMiIsImMiOiJ3IiwiYSI6InVlc2lvL3Rlc3RzIiwidyI6ImRldiJ9"
-			const fakeAuthCode = "authcode123"
-			let closeStub: Sinon.SinonStub | undefined
-			cy.visit(`${callbackUrl}?state=${state}&code=${fakeAuthCode}`, {
-				headers: {},
-				// Stub window.close, we need to verify that it was invoked
-				onBeforeLoad(win) {
-					closeStub = cy.stub(win, "close")
-				},
+				const parsed = new URLSearchParams(queryString)
+				expect(parsed.get("state")).to.have.length.greaterThan(0)
+				const state = parsed.get("state") || ""
+				const callbackUrl = `${getBaseUrl()}/site/oauth2/callback`
+				// Now open the callback URL manually, to simulate what should have happened by opening the window
+				const fakeAuthCode = "authcode123"
+				cy.visit(`${callbackUrl}?state=${state}&code=${fakeAuthCode}`, {
+					headers: {},
+				})
+				cy.url().should("include", "/site/oauth2/callback")
+				cy.getByIdFragment("span", "auth-message").should(
+					"contain",
+					"Authentication successful!"
+				)
+				// Now go back to the integration page and reopen the Test Auth dialog
+				cy.visit(
+					getWorkspaceBasePath(appName, workspaceName) +
+						integrationDetailRoute
+				)
+				cy.url().should("include", integrationDetailRoute)
+				testAuthBtn().should("exist")
+				testAuthBtn().click()
+				hasAccessToken()
+					.should("exist")
+					.and("have.attr", "checked", "checked")
+				hasRefreshToken()
+					.should("exist")
+					.and("have.attr", "checked", "checked")
+				// Now, clear credentials
+				clearCredsBtn().should("exist")
+				clearCredsBtn().click()
+				hasAccessToken().should("exist").and("be.disabled")
+				hasRefreshToken().should("not.exist")
 			})
-			cy.url().should("include", "/site/oauth2/callback")
-			cy.getByIdFragment("span", "auth-message").should(
-				"contain",
-				"Authentication successful!"
-			)
-			if (closeStub) {
-				expect(closeStub.calledOnce).to.be.true
-			}
-			// Now go back to the integration page and reopen the Test Auth dialog
-			cy.visit(
-				getWorkspaceBasePath(appName, workspaceName) +
-					integrationDetailRoute
-			)
-			cy.url().should("include", integrationDetailRoute)
-			testAuthBtn().should("exist")
-			testAuthBtn().click()
-			hasAccessToken()
-				.should("exist")
-				.and("have.attr", "checked", "checked")
-			hasRefreshToken()
-				.should("exist")
-				.and("have.attr", "checked", "checked")
-			// Now, clear credentials
-			clearCredsBtn().should("exist")
-			clearCredsBtn().click()
-			hasAccessToken().should("exist").and("be.disabled")
-			hasRefreshToken().should("not.exist")
 		})
 	})
 })
