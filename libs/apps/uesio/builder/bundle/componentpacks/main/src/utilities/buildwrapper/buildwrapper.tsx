898626dc2dfe8947008dbf66244b2589fd00f119
humandad <55447225+humandad@users.noreply.github.com>
2023-11-28 17:54:05 -0500

    better invisible component handling and styling

--- libs/apps/uesio/builder/bundle/componentpacks/main/src/utilities/buildwrapper/buildwrapper.tsx
+++ libs/apps/uesio/builder/bundle/componentpacks/main/src/utilities/buildwrapper/buildwrapper.tsx
@@ -1,7 +1,37 @@
-import { definition, context } from "@uesio/ui"
+import { definition, context, styles } from "@uesio/ui"
 import PlaceHolder from "../placeholder/placeholder"
 import { useDropPath } from "../../api/stateapi"
 import { FullPath } from "../../api/path"
+
+const StyleDefaults = Object.freeze({
+	root: [
+		"contents",
+		"relative",
+		"empty:block",
+		"empty:bg-blue-50",
+		"empty:py-2",
+		"empty:px-4",
+		"empty:my-1",
+		"empty:border-1",
+		"empty:border-blue-400",
+		"empty:text-blue-400",
+		"empty:text-[8pt]",
+		"empty:font-light",
+		"empty:rounded",
+		"empty:uppercase",
+		"empty:before:block",
+		"empty:before:pl-6",
+		"empty:before:content-[attr(data-component)]",
+		"empty:after:absolute",
+		"empty:after:inset-0",
+		"empty:after:pointer-events-none",
+		"empty:after:content-['visibility\\_off']",
+		"empty:after:py-1",
+		"empty:after:px-3",
+		"empty:after:font-[Material_Icons]",
+		"empty:after:text-base",
+	],
+})
 
 const usePlaceHolders = (
 	context: context.Context,
@@ -34,13 +64,14 @@ const BuildWrapper: definition.UC = (props) => {
 
 	const [addBefore, addAfter, index] = usePlaceHolders(context, path)
 
+	const classes = styles.useStyleTokens(StyleDefaults, props)
+
 	return (
 		<div
-			className="contents"
+			className={classes.root}
 			data-placeholder="true"
 			data-index={index}
 			data-component={componentType}
-			data-empty-label={"Invisible Component: " + componentType}
 		>
 			{addBefore && (
 				<PlaceHolder label="0" isHovering={true} context={context} />
