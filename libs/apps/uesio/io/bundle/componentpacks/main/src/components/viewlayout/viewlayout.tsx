da2d6b9952539ea04e2fb5cc4e71fa0afc164158
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2023-12-01 17:11:27 -0500

    feat: Add view layout component (#3535)

---
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/viewlayout/viewlayout.tsx
@@ -0,0 +1,88 @@
+import { component, styles, definition } from "@uesio/ui"
+
+const StyleDefaults = Object.freeze({
+	root: [],
+	header: [],
+	left: [],
+	content: [],
+	right: [],
+	footer: [],
+})
+
+type ViewLayoutDefinition = {
+	header?: definition.DefinitionList
+	left?: definition.DefinitionList
+	content?: definition.DefinitionList
+	right?: definition.DefinitionList
+	footer?: definition.DefinitionList
+}
+
+const ViewLayout: definition.UC<ViewLayoutDefinition> = (props) => {
+	const { definition, context, componentType } = props
+	const { header, left, content, right, footer } = definition
+
+	const classes = styles.useStyleTokens(StyleDefaults, props)
+	return (
+		<div className={classes.root}>
+			{header && (
+				<div className={classes.header}>
+					<component.Slot
+						definition={definition}
+						listName="header"
+						path={props.path}
+						context={context}
+						componentType={componentType}
+					/>
+				</div>
+			)}
+			{left && (
+				<div className={classes.left}>
+					<component.Slot
+						definition={definition}
+						listName="left"
+						path={props.path}
+						context={context}
+						componentType={componentType}
+					/>
+				</div>
+			)}
+			{content && (
+				<div className={classes.content}>
+					<component.Slot
+						definition={definition}
+						listName="content"
+						path={props.path}
+						context={context}
+						componentType={componentType}
+					/>
+				</div>
+			)}
+			{right && (
+				<div className={classes.right}>
+					<component.Slot
+						definition={definition}
+						listName="right"
+						path={props.path}
+						context={context}
+						componentType={componentType}
+					/>
+				</div>
+			)}
+			{footer && (
+				<div className={classes.footer}>
+					<component.Slot
+						definition={definition}
+						listName="footer"
+						path={props.path}
+						context={context}
+						componentType={componentType}
+					/>
+				</div>
+			)}
+		</div>
+	)
+}
+
+ViewLayout.displayName = "ViewLayout"
+
+export default ViewLayout
