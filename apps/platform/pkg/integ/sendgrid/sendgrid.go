e9e4333834f429c3c58ade7045b3314ff08d596b
Zach McElrath <zach@ues.io>
2023-12-08 15:09:10 -0500

    feat(SendGrid): Support single/multiple emails, as well as display names

--- apps/platform/pkg/integ/sendgrid/sendgrid.go
+++ apps/platform/pkg/integ/sendgrid/sendgrid.go
@@ -1,29 +1,16 @@
 package sendgrid
 
 import (
-	"errors"
 	"fmt"
 	"strings"
 
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 
-	"github.com/thecloudmasters/uesio/pkg/datasource"
 	"github.com/thecloudmasters/uesio/pkg/meta"
+	"github.com/thecloudmasters/uesio/pkg/types/exceptions"
 	"github.com/thecloudmasters/uesio/pkg/types/wire"
 )
-
-type SendEmailOptions struct {
-	To                  []string               `json:"to"`
-	CC                  []string               `json:"cc"`
-	BCC                 []string               `json:"bcc"`
-	Subject             string                 `json:"subject"`
-	PlainBody           string                 `json:"plainBody"`
-	ContentType         string                 `json:"contentType"`
-	From                string                 `json:"from"`
-	TemplateId          string                 `json:"templateId"`
-	DynamicTemplateData map[string]interface{} `json:"dynamicTemplateData"`
-}
 
 type connection struct {
 	client      *sendgrid.Client
@@ -33,7 +20,7 @@ type connection struct {
 func newSendGridConnection(ic *wire.IntegrationConnection) (*connection, error) {
 	apikey, err := ic.GetCredentials().GetRequiredEntry("apikey")
 	if err != nil || apikey == "" {
-		return nil, errors.New("SendGrid API Key not provided")
+		return nil, exceptions.NewUnauthorizedException("SendGrid API Key not provided")
 	}
 	return &connection{
 		client:      sendgrid.NewSendClient(apikey),
@@ -43,7 +30,6 @@ func newSendGridConnection(ic *wire.IntegrationConnection) (*connection, error) 
 
 // RunAction implements the system bot interface
 func RunAction(bot *meta.Bot, ic *wire.IntegrationConnection, actionName string, params map[string]interface{}) (interface{}, error) {
-
 	sgic, err := newSendGridConnection(ic)
 	if err != nil {
 		return nil, err
@@ -52,55 +38,126 @@ func RunAction(bot *meta.Bot, ic *wire.IntegrationConnection, actionName string,
 	case "sendemail":
 		return nil, sgic.sendEmail(params)
 	}
+	return nil, exceptions.NewBadRequestException("invalid action name for SendGrid integration")
+}
 
-	return nil, errors.New("invalid action name for SendGrid integration")
+func getEmailsList(addresses interface{}, names interface{}) []*mail.Email {
 
+	addressesSlice, isAddressesSlice := addresses.([]string)
+	namesSlice, isNamesSlice := names.([]string)
+
+	// Try to parse the emails as slices
+	if isAddressesSlice {
+		// First, check if the slice lengths are the same
+		haveMatchingNamesSlice := isNamesSlice && (len(addressesSlice) == len(namesSlice))
+		var emails []*mail.Email
+		for i := range addressesSlice {
+			var name string
+			address := addressesSlice[i]
+			if haveMatchingNamesSlice {
+				name = namesSlice[i]
+			}
+			if name == "" {
+				name = address
+			}
+			emails = append(emails, &mail.Email{
+				Name:    name,
+				Address: address,
+			})
+		}
+		return emails
+	}
+	// Next check if the address/name is a single slice
+	addressString, isAddressString := addresses.(string)
+	nameString, isNamesString := names.(string)
+
+	if isAddressString && isNamesString {
+		return []*mail.Email{{
+			Name:    nameString,
+			Address: addressString,
+		}}
+	} else if isAddressString {
+		return []*mail.Email{{
+			Name:    addressString,
+			Address: addressString,
+		}}
+	}
+	return []*mail.Email{}
+}
+
+func asString(v interface{}) string {
+	if stringVal, isString := v.(string); isString {
+		return stringVal
+	}
+	return ""
+}
+
+func createMessage(requestOptions map[string]interface{}) *mail.SGMailV3 {
+
+	message := mail.NewV3Mail()
+	p := mail.NewPersonalization()
+	var fromAddress, fromName, plainBody, contentType, templateId string
+	var dynamicTemplateData map[string]interface{}
+	var toAddresses, toNames, ccAddresses, ccNames, bccAddresses, bccNames interface{}
+
+	for k, v := range requestOptions {
+		switch k {
+		case "to":
+			toAddresses = v
+		case "toNames":
+			toNames = v
+		case "cc":
+			ccAddresses = v
+		case "ccNames":
+			ccNames = v
+		case "bcc":
+			bccAddresses = v
+		case "bccNames":
+			bccNames = v
+		case "subject":
+			message.Subject = asString(v)
+		case "plainBody":
+			plainBody = asString(v)
+		case "contentType":
+			contentType = asString(v)
+		case "from":
+			fromAddress = asString(v)
+		case "fromName":
+			fromName = asString(v)
+		case "templateId":
+			templateId = asString(v)
+		case "dynamicTemplateData":
+			if mapVal, isMap := v.(map[string]interface{}); isMap {
+				dynamicTemplateData = mapVal
+			}
+		}
+	}
+	p.AddTos(getEmailsList(toAddresses, toNames)...)
+	p.AddCCs(getEmailsList(ccAddresses, ccNames)...)
+	p.AddBCCs(getEmailsList(bccAddresses, bccNames)...)
+
+	// Default FromName to From address
+	if fromName == "" && fromAddress != "" {
+		fromName = fromAddress
+	}
+	if templateId != "" {
+		message.SetTemplateID(templateId)
+		p.DynamicTemplateData = dynamicTemplateData
+	}
+	if plainBody != "" {
+		if contentType == "" {
+			contentType = "text/plain"
+		}
+		message.AddContent(mail.NewContent(contentType, plainBody))
+	}
+	message.AddPersonalizations(p)
+	message.SetFrom(mail.NewEmail(fromName, fromAddress))
+
+	return message
 }
 
 func (sgic *connection) sendEmail(requestOptions map[string]interface{}) error {
-
-	options := &SendEmailOptions{}
-	err := datasource.HydrateOptions(requestOptions, options)
-	if err != nil {
-		return err
-	}
-
-	var toUsers []*mail.Email
-	for _, toRecipient := range options.To {
-		toUsers = append(toUsers, mail.NewEmail(toRecipient, toRecipient))
-	}
-
-	var ccUsers []*mail.Email
-	for _, ccRecipient := range options.CC {
-		ccUsers = append(ccUsers, mail.NewEmail(ccRecipient, ccRecipient))
-	}
-
-	var bccUsers []*mail.Email
-	for _, bccRecipient := range options.BCC {
-		bccUsers = append(bccUsers, mail.NewEmail(bccRecipient, bccRecipient))
-	}
-
-	message := mail.NewV3Mail()
-	message.Subject = options.Subject
-	p := mail.NewPersonalization()
-	p.AddTos(toUsers...)
-	p.AddCCs(ccUsers...)
-	p.AddBCCs(bccUsers...)
-
-	if options.TemplateId != "" {
-		message.SetTemplateID(options.TemplateId)
-		p.DynamicTemplateData = options.DynamicTemplateData
-	}
-
-	message.AddPersonalizations(p)
-	if options.PlainBody != "" {
-		contentType := "text/plain"
-		if options.ContentType != "" {
-			contentType = options.ContentType
-		}
-		message.AddContent(mail.NewContent(contentType, options.PlainBody))
-	}
-	message.SetFrom(mail.NewEmail(options.From, options.From))
+	message := createMessage(requestOptions)
 	response, err := sgic.client.Send(message)
 	if err != nil {
 		return err
@@ -108,6 +165,5 @@ func (sgic *connection) sendEmail(requestOptions map[string]interface{}) error {
 	if response.StatusCode != 202 {
 		return fmt.Errorf("%v %s", response.StatusCode, response.Body)
 	}
-
 	return nil
 }
