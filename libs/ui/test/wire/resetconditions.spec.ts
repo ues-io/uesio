c37219a56435b1b8b426436d2354041f07797c56
Zach McElrath <zach@ues.io>
2023-12-13 15:24:40 -0500

    add unit tests for RESET_CONDITION

---
+++ libs/ui/test/wire/resetconditions.spec.ts
@@ -0,0 +1,155 @@
+import testWireSignal, { WireSignalTest } from "./utils"
+import { ValueConditionState, WireDefinition } from "../../src/wireexports"
+import { Context } from "../../src/context/context"
+import { getCollectionSlice } from "../utils/defaults"
+import { InitialState } from "../../src/store/store"
+import { PlainViewDef } from "../../src/definition/view"
+
+const wireId = "mywire"
+const collectionId = "ben/planets.exoplanet"
+
+const getInitialState = (wireDef: WireDefinition) =>
+	({
+		route: {
+			dependencies: {
+				collection: getCollectionSlice(),
+				viewdef: [
+					{
+						definition: {
+							wires: {
+								[wireId]: wireDef,
+							},
+							components: [],
+						},
+						name: "myview",
+					} as PlainViewDef,
+				],
+			},
+		},
+	} as InitialState)
+
+const wireDef1 = {
+	collection: collectionId,
+	fields: {},
+	conditions: [
+		{
+			id: "123",
+			field: "ben/planets.name",
+			valueSource: "VALUE",
+			value: "kepler",
+		},
+		{
+			id: "456",
+			field: "ben/planets.galaxy",
+			valueSource: "VALUE",
+			values: [1, 2],
+		},
+		{
+			field: "uesio/core.id",
+			valueSource: "VALUE",
+			operator: "IN",
+			values: ["abc", "defg"],
+		},
+	] as ValueConditionState[],
+}
+
+const wireDef2 = {
+	collection: collectionId,
+	conditions: [
+		{
+			id: "999",
+			field: "ben/planets.name",
+			valueSource: "VALUE",
+			value: "$Param{first} $Param{last}",
+		} as ValueConditionState,
+	],
+	fields: {},
+}
+
+const tests: WireSignalTest[] = [
+	{
+		name: "it should reset named conditions with static values, but ignore unnamed conditions",
+		initialState: getInitialState(wireDef1),
+		wireId,
+		wireDef: wireDef1,
+		signals: [
+			// First modify the condition values
+			{
+				signal: "wire/SET_CONDITION_VALUE",
+				wire: wireId,
+				conditionId: "123",
+				value: "Foobar",
+			},
+			{
+				signal: "wire/SET_CONDITION_VALUE",
+				wire: wireId,
+				conditionId: "456",
+				values: [3],
+			},
+			// Then reset them
+			{
+				signal: "wire/RESET_CONDITIONS",
+				wire: wireId,
+			},
+		],
+		run: () => (wire) => () => {
+			expect(wire).toMatchObject({
+				conditions: [
+					{ id: "123", field: "ben/planets.name", value: "kepler" },
+					{ id: "456", field: "ben/planets.galaxy", values: [1, 2] },
+					{
+						field: "uesio/core.id",
+						operator: "IN",
+						values: ["abc", "defg"],
+					},
+				],
+			})
+		},
+	},
+	{
+		name: "should evaluate merges when resetting condition values",
+		initialState: getInitialState(wireDef2),
+		wireId,
+		wireDef: wireDef2,
+		context: new Context()
+			.addViewFrame({
+				view: "myview",
+				viewDef: "myview",
+				params: {
+					first: "Luigi",
+					last: "Vampa",
+				},
+			})
+			.addSignalOutputFrame("ageCalculation", {
+				age: 37,
+				conditionName: "999",
+				wirename: wireId,
+			}),
+		signals: [
+			{
+				signal: "wire/SET_CONDITION_VALUE",
+				wire: "$SignalOutput{ageCalculation:wirename}",
+				conditionId: "$SignalOutput{ageCalculation:conditionName}",
+				value: "Alice Perkins (Age: $SignalOutput{ageCalculation:age})",
+			},
+			{
+				signal: "wire/RESET_CONDITIONS",
+				wire: "$SignalOutput{ageCalculation:wirename}",
+			},
+		],
+		run: () => (wire) => {
+			expect(wire.conditions).toEqual([
+				{
+					id: "999",
+					field: "ben/planets.name",
+					valueSource: "VALUE",
+					value: "Luigi Vampa",
+				},
+			])
+		},
+	},
+]
+
+describe("signals: wire/RESET_CONDITIONS", () => {
+	tests.map((el) => test(el.name, () => testWireSignal(el)))
+})
