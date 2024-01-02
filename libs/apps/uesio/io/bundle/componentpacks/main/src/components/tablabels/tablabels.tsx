413ae3bfc84c826c59800a2d0c2e688c9e58eb81
Zach McElrath <zach@ues.io>
2024-01-02 15:02:14 -0500

    get integration test working

---
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/tablabels/tablabels.tsx
@@ -0,0 +1,34 @@
+import { component, definition, api, styles } from "@uesio/ui"
+import { useState } from "react"
+import IOTabLabels from "../../utilities/tablabels/tablabels"
+import { TabDefinition } from "../tabs/tabs"
+
+type TabLabelsDefinition = {
+	tabs: definition.DefinitionList
+}
+
+const StyleDefaults = Object.freeze({
+	root: [],
+})
+
+// This component is not used or exposed anywhere currently,
+// it just exists to enable us to have a utility component that uses the tablabels variant.
+// TODO: Remove this component once we have a better way to do this.
+const TabLabels: definition.UC<TabLabelsDefinition> = (props) => {
+	const { definition, context } = props
+	const classes = styles.useStyleTokens(StyleDefaults, props)
+	const [selectedTab, setSelectedTab] = useState<string>("")
+	return (
+		<IOTabLabels
+			id={api.component.getComponentIdFromProps(props)}
+			variant={definition[component.STYLE_VARIANT]}
+			context={context}
+			classes={classes}
+			tabs={definition.tabs as TabDefinition[]}
+			selectedTab={selectedTab}
+			setSelectedTab={setSelectedTab}
+		/>
+	)
+}
+
+export default TabLabels
