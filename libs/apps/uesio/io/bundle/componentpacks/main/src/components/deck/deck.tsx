903f1ca3d94df5c00fce90e2035d024415fe3cd4
Zach McElrath <zach@ues.io>
2024-01-02 13:52:04 -0500

    process variant definition dependencies

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/deck/deck.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/deck/deck.tsx
@@ -1,4 +1,4 @@
-import { api, definition, metadata, styles } from "@uesio/ui"
+import { api, component, definition, metadata, styles } from "@uesio/ui"
 import List, { ListDefinition } from "../list/list"
 
 import { default as IOGrid } from "../../utilities/grid/grid"
@@ -8,16 +8,18 @@ const StyleDefaults = Object.freeze({
 })
 
 type DeckDefinition = {
-	gridVariant?: metadata.MetadataType
+	gridVariant?: metadata.MetadataKey
 } & ListDefinition
 
 const Deck: definition.UC<DeckDefinition> = (props) => {
 	const { definition, context } = props
-
+	// Backwards compatibility: Prior to 1/1/2024, "gridVariant" didn't exist,
+	// and the "uesio.variant" property was expected to be a Grid variant.
+	const { gridVariant = definition[component.STYLE_VARIANT] } = definition
 	const classes = styles.useStyleTokens(StyleDefaults, props)
 	return (
 		<IOGrid
-			variant={definition.gridVariant}
+			variant={gridVariant}
 			classes={classes}
 			context={context}
 			id={api.component.getComponentIdFromProps(props)}
