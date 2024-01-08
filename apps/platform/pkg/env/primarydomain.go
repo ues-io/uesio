b8f82e1ba5efa05a8540cb0e18b7ff1bec7f808a
Abel Jimenez Molla <abel@ues.io>
2024-01-08 09:34:02 +0100

    feedback

--- apps/platform/pkg/env/primarydomain.go
+++ apps/platform/pkg/env/primarydomain.go
@@ -5,8 +5,11 @@ import "os"
 func GetPrimaryDomain() string {
 
 	var primaryDomain, isSet = os.LookupEnv("UESIO_PRIMARY_DOMAIN")
-	if !isSet  && InDevMode() {
-		primaryDomain = "uesio-dev.com"
+	if !isSet {
+		primaryDomain = "ues.io"
+		if InDevMode() {
+			primaryDomain = "uesio-dev.com"
+		}
 	}
 
 	return primaryDomain
