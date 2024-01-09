84d5165a79c0f9aa275bf96d310c7c7e388f58da
Abel Jimenez Molla <abel@ues.io>
2024-01-09 14:28:12 +0100

    Update 5_recompute_uniquekeys_collection_bundle.down.sql

--- apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.down.sql
+++ apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.down.sql
@@ -1,5 +1,15 @@
 
 begin;
 
+UPDATE public.data
+SET fields = jsonb_delete(fields, 'uesio/studio.repository')
+WHERE collection = 'uesio/studio.bundle';
+
+UPDATE public.data SET 
+uniquekey = split_part(uniquekey, ':', 1) || ':' ||  
+split_part(uniquekey, ':', 2) || ':' || 
+split_part(uniquekey, ':', 3) || ':' || 
+split_part(uniquekey, ':', 4)
+WHERE collection = 'uesio/studio.bundle'
 
 commit;
