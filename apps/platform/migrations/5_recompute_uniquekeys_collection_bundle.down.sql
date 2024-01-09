bfdbefa5a48a59b12ee5b7cf2c7ccd5008c59cec
Abel Jimenez Molla <abel@ues.io>
2024-01-09 14:21:46 +0100

    move all to up

--- apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.down.sql
+++ apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.down.sql
@@ -1,8 +1,5 @@
 
 begin;
 
-UPDATE public.data 
-SET fields = jsonb_set(fields,'{uesio/studio.repository}', '"ues.io"') 
-WHERE collection = 'uesio/studio.bundle'
 
 commit;
--- apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.up.sql
+++ apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.up.sql
@@ -1,12 +1,15 @@
 
 begin;
 
+UPDATE public.data 
+SET fields = jsonb_set(fields,'{uesio/studio.repository}', '"ues.io"') 
+WHERE collection = 'uesio/studio.bundle'
+
 UPDATE public.data SET 
 uniquekey = split_part(uniquekey, ':', 1) || ':' ||  
 split_part(uniquekey, ':', 2) || ':' || 
 split_part(uniquekey, ':', 3) || ':' || 
-split_part(uniquekey, ':', 4) || ':' || 
-jsonb_extract_path_text(fields, 'uesio/studio.repository')  
+split_part(uniquekey, ':', 4) || ':ues.io'
 WHERE collection = 'uesio/studio.bundle'
 
 commit;
