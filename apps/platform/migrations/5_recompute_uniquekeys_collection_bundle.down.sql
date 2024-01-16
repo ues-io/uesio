38ea455f62601aeba9c487ef6baf1993cdc14686
Zach McElrath <zach@ues.io>
2024-01-16 10:10:05 -0500

    simplify migrations

--- apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.down.sql
+++ apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.down.sql
@@ -2,14 +2,7 @@
 begin;
 
 UPDATE public.data
-SET fields = jsonb_delete(fields, 'uesio/studio.repository')
-WHERE collection = 'uesio/studio.bundle';
-
-UPDATE public.data SET 
-uniquekey = split_part(uniquekey, ':', 1) || ':' ||  
-split_part(uniquekey, ':', 2) || ':' || 
-split_part(uniquekey, ':', 3) || ':' || 
-split_part(uniquekey, ':', 4)
+SET fields = jsonb_delete(fields, 'uesio/studio.repository'), uniquekey = replace(uniquekey, ':ues.io', '')
 WHERE collection = 'uesio/studio.bundle'
 
 commit;
--- apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.up.sql
+++ apps/platform/migrations/5_recompute_uniquekeys_collection_bundle.up.sql
@@ -2,14 +2,7 @@
 begin;
 
 UPDATE public.data 
-SET fields = jsonb_set(fields,'{uesio/studio.repository}', '"ues.io"') 
-WHERE collection = 'uesio/studio.bundle'
-
-UPDATE public.data SET 
-uniquekey = split_part(uniquekey, ':', 1) || ':' ||  
-split_part(uniquekey, ':', 2) || ':' || 
-split_part(uniquekey, ':', 3) || ':' || 
-split_part(uniquekey, ':', 4) || ':ues.io'
+SET fields = jsonb_set(fields,'{uesio/studio.repository}', '"ues.io"'), uniquekey = concat(uniquekey, ':ues.io')
 WHERE collection = 'uesio/studio.bundle'
 
 commit;
