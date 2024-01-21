c4f4935e3b43a0a225fbf1495500a49393e96a08
Zach McElrath <zach@ues.io>
2024-01-21 16:56:41 -0500

    refactor: Extract AsAdminAPI

---
+++ apps/platform/pkg/bot/jsdialect/asadminapi.go
@@ -0,0 +1,37 @@
+package jsdialect
+
+import (
+	"github.com/thecloudmasters/uesio/pkg/configstore"
+	"github.com/thecloudmasters/uesio/pkg/datasource"
+	"github.com/thecloudmasters/uesio/pkg/sess"
+	"github.com/thecloudmasters/uesio/pkg/types/wire"
+)
+
+type AsAdminApi struct {
+	session    *sess.Session
+	connection wire.Connection
+}
+
+func (aaa *AsAdminApi) Save(collection string, changes wire.Collection) error {
+	return botSave(collection, changes, datasource.GetSiteAdminSession(aaa.session), aaa.connection)
+}
+
+func (aaa *AsAdminApi) Delete(collection string, deletes wire.Collection) error {
+	return botDelete(collection, deletes, datasource.GetSiteAdminSession(aaa.session), aaa.connection)
+}
+
+func (aaa *AsAdminApi) RunIntegrationAction(integrationID string, action string, options interface{}) (interface{}, error) {
+	return runIntegrationAction(integrationID, action, options, datasource.GetSiteAdminSession(aaa.session), aaa.connection)
+}
+
+func (aaa *AsAdminApi) CallBot(botKey string, params map[string]interface{}) (interface{}, error) {
+	return botCall(botKey, params, aaa.session, aaa.connection)
+}
+
+func (aaa *AsAdminApi) GetConfigValue(configValueKey string) (string, error) {
+	return configstore.GetValueFromKey(configValueKey, datasource.GetSiteAdminSession(aaa.session))
+}
+
+func (aaa *AsAdminApi) Load(request BotLoadOp) (*wire.Collection, error) {
+	return botLoad(request, datasource.GetSiteAdminSession(aaa.session), aaa.connection)
+}
