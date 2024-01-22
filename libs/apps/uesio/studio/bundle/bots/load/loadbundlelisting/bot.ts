0bbea3218983d147aca993bfd56ec4a0253af1cf
Abel Jimenez Molla <abel@ues.io>
2024-01-22 09:29:09 +0100

    add filter

--- libs/apps/uesio/studio/bundle/bots/load/loadbundlelisting/bot.ts
+++ libs/apps/uesio/studio/bundle/bots/load/loadbundlelisting/bot.ts
@@ -11,14 +11,6 @@ export default function loadbundlelisting(bot: LoadBotApi) {
 	const bundleStoreDomain = bot.getConfigValue(
 		"uesio/core.bundle_store_domain"
 	)
-
-	bot.log.info("bundleStoreDomain --> ", bundleStoreDomain)
-
-	if (bundleStoreDomain != "") {
-		//API call
-	}
-
-	//READ from DB
 	const results = bot.callBot("getbundlelisting", {})
 
 	Object.entries(results).map(([key, fieldValue], index) => {
@@ -33,12 +25,12 @@ export default function loadbundlelisting(bot: LoadBotApi) {
 	})
 
 	const apiResponse = response.body as unknown as ApiResponse[]
-	bot.log.info("Got Data", apiResponse)
 	apiResponse.forEach((record) =>
 		bot.addRecord({
 			"uesio/studio.uniquekey": record.app,
 			"uesio/studio.description": record.description,
 			"uesio/studio.approved": true,
+			"uesio/studio.prodbundle": true,
 			"uesio/studio.app": {
 				"uesio/core.uniquekey": record.app,
 				"uesio/studio.color": record.color,
