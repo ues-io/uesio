f9d2235069c13137a5dc253a51e78083e7202d47
Abel Jimenez Molla <65349612+abelcloudmasters@users.noreply.github.com>
2024-01-22 15:24:04 +0100

    feat: allow bot load in Integration Load bots (#3740)
    * feat: allow bot load in Integration Load bots
    
    * remove unneeded code
    
    ---------
    
    Co-authored-by: Zach McElrath <zach@ues.io>

--- apps/platform/pkg/bot/jsdialect/jsdialect.go
+++ apps/platform/pkg/bot/jsdialect/jsdialect.go
@@ -254,7 +254,7 @@ func (b *JSDialect) LoadBot(bot *meta.Bot, op *wire.LoadOp, connection wire.Conn
 	if err != nil {
 		return err
 	}
-	botAPI := NewLoadBotAPI(bot, connection, op, integrationConnection)
+	botAPI := NewLoadBotAPI(bot, op, integrationConnection)
 	if err = RunBot(bot, botAPI, session, b.hydrateBot, nil); err != nil {
 		return err
 	}
