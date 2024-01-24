4f80bfcbcc22b9040a29510502db54299c8ca0dc
Abel Jimenez Molla <abel@ues.io>
2024-01-24 11:26:30 +0100

    stores in separated views

--- libs/apps/uesio/studio/bundle/bots/load/loadbundlelisting/bot.ts
+++ libs/apps/uesio/studio/bundle/bots/load/loadbundlelisting/bot.ts
@@ -10,62 +10,39 @@ type ApiResponse = {
 export default function loadbundlelisting(bot: LoadBotApi) {
 	const { conditions } = bot.loadRequest
 
-	const localBundleOnlyCondition = conditions?.find(
-		(condition) => condition.id === "localBundleOnly"
+	const bundleStoreDomain = bot.getConfigValue(
+		"uesio/core.bundle_store_domain"
+	)
+	const url = "https://studio." + bundleStoreDomain + "/site/bundles/v1/list"
+
+	const response = bot.http.request({
+		method: "GET",
+		url,
+	})
+
+	let apiResponse = response.body as unknown as ApiResponse[]
+
+	const bundleListingWrapperUniquekey = conditions?.find(
+		(condition) => condition.id === "bundleListingWrapperUniquekey"
 	)
 
-	if (
-		localBundleOnlyCondition &&
-		localBundleOnlyCondition.inactive === false
-	) {
-		//LOCAL Bundels (shall we filter by approved & PUBLISHED ??)
-		const results = bot.load({
-			collection: `uesio/studio.bundlelisting`,
-			fields: [
-				{
-					id: "uesio/studio.app",
-					fields: [
-						{ id: "uesio/studio.fullname" },
-						{ id: "uesio/studio.color" },
-						{ id: "uesio/studio.icon" },
-					],
-				},
-				{ id: "uesio/studio.description" },
-				{ id: "uesio/studio.approved" },
-			],
-		})
-
-		results.map((fieldValue, index) => {
-			bot.addRecord({
-				...(fieldValue as Object),
-				"uesio/studio.localbundle": true,
-			})
-		})
-	} else {
-		const bundleStoreDomain = bot.getConfigValue(
-			"uesio/core.bundle_store_domain"
-		)
-		const url =
-			"https://studio." + bundleStoreDomain + "/site/bundles/v1/list"
-
-		const response = bot.http.request({
-			method: "GET",
-			url,
-		})
-
-		const apiResponse = response.body as unknown as ApiResponse[]
-		apiResponse.forEach((record) =>
-			bot.addRecord({
-				"uesio/studio.uniquekey": record.app,
-				"uesio/studio.description": record.description,
-				"uesio/studio.approved": true,
-				"uesio/studio.app": {
-					"uesio/core.uniquekey": record.app,
-					"uesio/studio.color": record.color,
-					"uesio/studio.icon": record.icon,
-					"uesio/studio.fullname": record.app,
-				},
-			})
+	if (bundleListingWrapperUniquekey) {
+		apiResponse = apiResponse.filter(
+			(record) => record.app === bundleListingWrapperUniquekey.value
 		)
 	}
+
+	apiResponse.forEach((record) =>
+		bot.addRecord({
+			"uesio/studio.uniquekey": record.app,
+			"uesio/studio.description": record.description,
+			"uesio/studio.approved": true,
+			"uesio/studio.app": {
+				"uesio/core.uniquekey": record.app,
+				"uesio/studio.color": record.color,
+				"uesio/studio.icon": record.icon,
+				"uesio/studio.fullname": record.app,
+			},
+		})
+	)
 }
