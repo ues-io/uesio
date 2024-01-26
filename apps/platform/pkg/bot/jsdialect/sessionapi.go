4ce5102b23e1677299e32e641c86a5c6f6c8b30b
Zach McElrath <zach@ues.io>
2024-01-26 12:18:18 -0500

    feat: Add APIs for accessing Workspace metadata to Session API (#3751)

--- apps/platform/pkg/bot/jsdialect/sessionapi.go
+++ apps/platform/pkg/bot/jsdialect/sessionapi.go
@@ -1,6 +1,9 @@
 package jsdialect
 
 import (
+	"fmt"
+
+	"github.com/thecloudmasters/uesio/pkg/meta"
 	"github.com/thecloudmasters/uesio/pkg/sess"
 )
 
@@ -8,14 +11,43 @@ import (
 
 type SessionAPI struct {
 	session *sess.Session
+	wsApi   *WorkspaceAPI
+}
+type WorkspaceAPI struct {
+	workspace *meta.Workspace
+}
+
+func (w *WorkspaceAPI) GetAppFullName() string {
+	return w.workspace.GetAppFullName()
+}
+func (w *WorkspaceAPI) GetName() string {
+	return w.workspace.Name
+}
+func (w *WorkspaceAPI) GetUrlPrefix() string {
+	return fmt.Sprintf("/workspace/%s/%s", w.workspace.GetAppFullName(), w.workspace.Name)
 }
 
 func NewSessionAPI(session *sess.Session) *SessionAPI {
 	return &SessionAPI{
-		session,
+		session: session,
 	}
 }
 
 func (s *SessionAPI) GetId() string {
 	return s.session.GetSessionId()
+}
+
+func (s *SessionAPI) InWorkspaceContext() bool {
+	return s.session.GetWorkspace() != nil
+}
+
+func (s *SessionAPI) GetWorkspace() *WorkspaceAPI {
+	if s.wsApi != nil {
+		return s.wsApi
+	}
+	if s.session.GetWorkspace() != nil {
+		s.wsApi = &WorkspaceAPI{workspace: s.session.GetWorkspace()}
+		return s.wsApi
+	}
+	return nil
 }
