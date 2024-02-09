2b9638b15c41efa9936a5147be8542eae656c3d4
humandad <55447225+humandad@users.noreply.github.com>
2024-02-09 13:05:01 -0500

    Improve sitenav with console and createnew view types

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/sitenav/sitenav.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/sitenav/sitenav.tsx
@@ -1,6 +1,35 @@
 import { component, definition } from "@uesio/ui"
 import groupBy from "lodash/groupBy"
 
+const getAssignmentButton = (
+	collection: string,
+	icon: string,
+	viewtype: string
+) => ({
+	"uesio/io.tile": {
+		"uesio.variant": "uesio/io.nav",
+		"uesio.styleTokens": {
+			avatar: ["text-sm"],
+			root: ["p-1.5"],
+		},
+		avatar: [
+			{
+				"uesio/io.text": {
+					"uesio.variant": "uesio/io.icon",
+					text: icon,
+				},
+			},
+		],
+		signals: [
+			{
+				signal: "route/NAVIGATE_TO_ASSIGNMENT",
+				collection,
+				viewtype,
+			},
+		],
+	},
+})
+
 const SiteNav: definition.UC = (props) => {
 	const { context, path } = props
 
@@ -11,39 +40,77 @@ const SiteNav: definition.UC = (props) => {
 	return (
 		<>
 			{Object.entries(assignmentsByCollection).map(
-				([collection, assignments]) => (
-					<component.Component
-						path={path}
-						context={context}
-						componentType="uesio/io.tile"
-						definition={{
-							"uesio.variant": "uesio/io.nav",
-							content: [
-								{
-									"uesio/io.text": {
-										text: assignments[0]
-											.collectionPluralLabel,
+				([collection, assignments]) => {
+					const assignmentsByType = groupBy(assignments, "type")
+					const listAssignment = assignmentsByType.list?.[0]
+					const createNewAssignment = assignmentsByType.createnew?.[0]
+					const consoleAssignment = assignmentsByType.console?.[0]
+					const primaryAssignment = listAssignment || assignments[0]
+
+					return (
+						<component.Component
+							path={path}
+							context={context}
+							componentType="uesio/io.box"
+							definition={{
+								"uesio.styleTokens": {
+									root: ["flex", "gap-2", "items-center"],
+								},
+								components: [
+									{
+										"uesio/io.tile": {
+											"uesio.variant": "uesio/io.nav",
+											"uesio.styleTokens": {
+												root: ["flex", "grow"],
+											},
+											content: [
+												{
+													"uesio/io.text": {
+														text: primaryAssignment.collectionPluralLabel,
+													},
+												},
+											],
+											avatar: [
+												{
+													"uesio/io.text": {
+														"uesio.variant":
+															"uesio/io.icon",
+														text: "view_list",
+													},
+												},
+											],
+											signals: listAssignment
+												? [
+														{
+															signal: "route/NAVIGATE_TO_ASSIGNMENT",
+															collection,
+														},
+													]
+												: undefined,
+										},
 									},
-								},
-							],
-							avatar: [
-								{
-									"uesio/io.text": {
-										"uesio.variant": "uesio/io.icon",
-										text: "view_list",
+									{
+										...(consoleAssignment &&
+											getAssignmentButton(
+												collection,
+												"dock_to_right",
+												"console"
+											)),
 									},
-								},
-							],
-							signals: [
-								{
-									signal: "route/NAVIGATE_TO_ASSIGNMENT",
-									collection,
-								},
-							],
-						}}
-						key={collection}
-					/>
-				)
+									{
+										...(createNewAssignment &&
+											getAssignmentButton(
+												collection,
+												"add",
+												"createnew"
+											)),
+									},
+								],
+							}}
+							key={collection}
+						/>
+					)
+				}
 			)}
 		</>
 	)
