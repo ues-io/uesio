58177ed7ce9c57c26cba38c9ef42e6a90b1b0936
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2024-04-01 14:18:38 -0400

    Better Query stats on integration tests (#3934)
    * Better Query stats on integration tests
    
    * oops

--- apps/platform/pkg/controller/filejson/respondjson.go
+++ apps/platform/pkg/controller/filejson/respondjson.go
@@ -14,3 +14,13 @@ func RespondJSON(w http.ResponseWriter, r *http.Request, v interface{}) {
 		return
 	}
 }
+
+func RespondJSONPretty(w http.ResponseWriter, r *http.Request, v interface{}) {
+	w.Header().Set("content-type", "text/json")
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "    ")
+	if err := enc.Encode(v); err != nil {
+		ctlutil.HandleError(w, err)
+		return
+	}
+}
