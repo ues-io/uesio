bf758a8187da3f69f8b95ce582d5f813070117de
humandad <55447225+humandad@users.noreply.github.com>
2024-05-06 13:10:52 -0400

    Fix ui tests

--- libs/ui/src/bands/wire/operations/resetconditions.ts
+++ libs/ui/src/bands/wire/operations/resetconditions.ts
@@ -1,12 +1,40 @@
-import { Context } from "../../../context/context"
-import setConditions from "./setconditions"
+import { Context, Mergeable } from "../../../context/context"
+import { dispatch } from "../../../store/store"
+import { setConditions, getFullWireId } from ".."
+import { isValueCondition } from "../conditions/conditions"
+import { PlainFieldValue } from "../../wirerecord/types"
 
 export default (context: Context, wireName: string) => {
 	//this returns the original wire definition
 	const viewDef = context.getViewDef()
 	const wireDef = viewDef?.wires?.[wireName]
 	if (wireDef && !wireDef.viewOnly && wireDef.conditions) {
-		setConditions(context, wireName, wireDef.conditions)
+		const viewId = context.getViewId()
+		const conditions = wireDef.conditions.map((condition) => {
+			if (condition.id && isValueCondition(condition)) {
+				const { value, values, inactive } = condition
+				const newCondition = {
+					...condition,
+					inactive: context.mergeBoolean(inactive, false),
+				}
+				if (value ?? false)
+					newCondition.value = context.merge(
+						value as Mergeable
+					) as PlainFieldValue
+				if (values ?? false)
+					newCondition.values = context.merge(
+						values as Mergeable
+					) as PlainFieldValue[]
+				return newCondition
+			}
+			return condition
+		})
+		dispatch(
+			setConditions({
+				entity: getFullWireId(viewId, wireName),
+				conditions,
+			})
+		)
 	}
 	return context
 }
