ccae9590ead8d23cf038bda27e1983cbab1c95c2
humandad <55447225+humandad@users.noreply.github.com>
2024-06-12 15:27:44 -0400

    Add public bundlestore listings

---
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/emptystate/emptystate.tsx
@@ -0,0 +1,43 @@
+import { styles, definition } from "@uesio/ui"
+
+import Icon from "../../utilities/icon/icon"
+
+type EmptyStateDefinition = {
+	title?: string
+	subtitle?: string
+	icon?: string
+	iconFill?: boolean
+}
+
+const StyleDefaults = Object.freeze({
+	root: [],
+	title: [],
+	subtitle: [],
+	icon: [],
+})
+
+const EmptyState: definition.UC<EmptyStateDefinition> = (props) => {
+	const { definition, context } = props
+	const classes = styles.useStyleTokens(StyleDefaults, props)
+
+	const { title, subtitle, icon, iconFill } = definition
+
+	return (
+		<div className={classes.root}>
+			<Icon
+				classes={{
+					root: classes.icon,
+				}}
+				fill={iconFill}
+				context={context}
+				icon={context.mergeString(icon)}
+			/>
+			<div className={classes.title}>{context.mergeString(title)}</div>
+			<div className={classes.subtitle}>
+				{context.mergeString(subtitle)}
+			</div>
+		</div>
+	)
+}
+
+export default EmptyState
