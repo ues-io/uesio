882617b245707e21922b209c610d159419200817
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2024-08-09 16:54:33 -0400

    add reference fields to appkit starter (#4131)

--- libs/apps/uesio/appkit/bundle/bots/generator/collection/bot.js
+++ libs/apps/uesio/appkit/bundle/bots/generator/collection/bot.js
@@ -2,6 +2,7 @@ function collection(bot) {
 	var collectionName = bot.params.get("collection")
 	var collectionLabel = bot.params.get("label")
 	var collectionPluralLabel = bot.params.get("pluralLabel")
+	var existingCollections = bot.params.get("existingCollections")
 
 	const namespace = bot.getAppName()
 
@@ -13,57 +14,107 @@ function collection(bot) {
 		pluralLabel: collectionPluralLabel,
 	})
 
+	const modelID = "anthropic.claude-3-haiku-20240307-v1:0"
+
+	const systemPrompt = `
+		You are an assistant who specializes in creating data models for databases.
+		You deeply understand relational databases and seek to complete the task in
+		the simplest and most straightforward way possible.
+	`
+
+	const prompt = `
+		Use the tool provided to create between 5 and 10 fields for
+		a database table called: ${collectionName}. The primary key for this table as well
+		as audit fields such as created date, updated date, created by and updated by already
+		exist and should not be created again.
+	`
+
+	const nameParam = {
+		type: "string",
+		description: `
+			The field name. This should be snake case. No uppercase characters are allowed.
+			For fields of type REFERENCE, do not include "id" in the name.
+		`,
+	}
+
+	const typeParam = {
+		type: "string",
+		description: `
+			The data type of the field. The only valid values for this type are
+			TEXT, NUMBER, CHECKBOX, DATE, FILE or REFERENCE.
+			Fields of type REFERENCE mean that they provide a foreign-key relationship
+			to another table.
+		`,
+	}
+
+	const labelParam = {
+		type: "string",
+		description: `
+			This is the human-readable label for this field. It should start with a
+			capital letter and spaces are allowed.
+			For fields of type REFERENCE, do not put "ID" in the label.
+		`,
+	}
+
+	const referencedCollectionParam = {
+		type: "string",
+		description: `
+			Only provide a value for this parameter if the field's type is REFERENCE.
+			If it is reference, chose the table that this field is a foreign key
+			reference to from one of the following tables. And use that table's name
+			as the value for this parameter.
+			${existingCollections.join("\n")}
+			The only valid values for this parameter are in the list above.
+		`,
+	}
+
+	const acceptTypeParam = {
+		type: "string",
+		description: `
+			Only provide a value for this parameter if the field's type is FILE.
+			If set, the valid values for this parameter are IMAGE, DOCUMENT, or ANY.
+		`,
+	}
+
+	const createFieldsTool = {
+		name: "create_fields",
+		description: "Create fields for a database table",
+		input_schema: {
+			type: "object",
+			properties: {
+				fields: {
+					type: "array",
+					description: "The fields to create",
+					items: {
+						type: "object",
+						properties: {
+							name: nameParam,
+							type: typeParam,
+							label: labelParam,
+							referencedCollection: referencedCollectionParam,
+							accept: acceptTypeParam,
+						},
+						required: ["name", "type", "label"],
+					},
+				},
+			},
+			required: ["fields"],
+		},
+	}
+
 	const result = bot.runIntegrationAction(
 		"uesio/aikit.bedrock",
 		"invokemodel",
 		{
-			model: "anthropic.claude-3-haiku-20240307-v1:0",
+			model: modelID,
 			messages: [
 				{
 					role: "user",
-					content:
-						"Please use the tool provided to create 5 fields for this database object called: " +
-						collectionName,
+					content: prompt,
 				},
 			],
-			system: "You are an assistant who creates data models for databases.",
-			tools: [
-				{
-					name: "create_fields",
-					description: "Create fields for a database table",
-					input_schema: {
-						type: "object",
-						properties: {
-							fields: {
-								type: "array",
-								description: "The fields to create",
-								items: {
-									type: "object",
-									properties: {
-										name: {
-											type: "string",
-											description:
-												"The field name. This should be snake case. No uppercase characters are allowed.",
-										},
-										type: {
-											type: "string",
-											description:
-												"The data type of the field. The only valid values for this type are TEXT, NUMBER, or CHECKBOX",
-										},
-										label: {
-											type: "string",
-											description:
-												"This is the nice, human-readable label for this field. It should start with a capital letter and spaces are allowed.",
-										},
-									},
-									required: ["name"],
-								},
-							},
-						},
-						required: ["fields"],
-					},
-				},
-			],
+			system: systemPrompt,
+			tools: [createFieldsTool],
 			tool_choice: {
 				type: "tool",
 				name: "create_fields",
@@ -85,6 +136,8 @@ function collection(bot) {
 			name: field.name,
 			label: field.label,
 			type: field.type,
+			ref_collection: field.referencedCollection,
+			accept: field.accept,
 		})
 		return namespace + "." + field.name
 	})
