17f5d90afac33a7150140db563233fca49da1f3a
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2024-09-13 13:51:39 -0400

    Sitekit starter gen improvements (#4194)

--- libs/apps/uesio/sitekit/bundle/bots/generator/starter/bot.js
+++ libs/apps/uesio/sitekit/bundle/bots/generator/starter/bot.js
@@ -1,9 +1,279 @@
 function starter(bot) {
 	const appInfo = bot.getApp()
 	const appName = appInfo.getName()
+	const appDescription = appInfo.getDescription()
+
+	const modelID = "anthropic.claude-3-haiku-20240307-v1:0"
+
+	let tagline = "Hello World."
+	let tagline_sub = "Generate websites quickly and easily with SiteKit."
+	let testimonials = [
+		{
+			quote: "By far the best thing since sliced bagels.",
+			name: "Barbara Mills",
+			title: "CTO Acme Widget Co.",
+		},
+	]
+	let pages = [
+		{
+			name: "Product",
+			type: "INFO",
+			description: "",
+		},
+		{
+			name: "About",
+			type: "INFO",
+			description: "",
+		},
+		{
+			name: "Contact",
+			type: "CALL_TO_ACTION",
+			description: "",
+		},
+	]
+	let footerCategories = [
+		{
+			name: "Company",
+			links: [
+				{
+					name: "Contact",
+					relative_url: "/contact",
+				},
+				{
+					name: "Services",
+					relative_url: "/services",
+				},
+			],
+		},
+		{
+			name: "Legal",
+			links: [
+				{
+					name: "Privacy Policy",
+					relative_url: "/privacy-policy",
+				},
+				{
+					name: "Terms of Service",
+					relative_url: "/terms-of-service",
+				},
+			],
+		},
+		{
+			name: "Resources",
+			links: [
+				{
+					name: "Book Appointment",
+					relative_url: "/book-appointment",
+				},
+				{
+					name: "Testimonials",
+					relative_url: "/testimonials",
+				},
+				{
+					name: "FAQs",
+					relative_url: "/faqs",
+				},
+			],
+		},
+	]
 
 	const doContentAndCopy = bot.params.get("use_ai_for_content_and_copy")
 	if (doContentAndCopy && doContentAndCopy !== "false") {
+		const systemPrompt = `
+			You are an assistant who specializes in creating professional marketing websites.
+			You deeply understand marketing best practices and are a creative copywriter. You
+			seek to complete the task in the simplest and most straightforward way possible.
+		`
+
+		const prompt = `
+			Use the tool provided to create a website for a company called "${appName}". The
+			desciption of the website is "${appDescription}". Create at least 4 pages for the website.
+		`
+
+		const pageTypes = [
+			"INFO",
+			"PERSONA",
+			"PRICING",
+			"CALL_TO_ACTION",
+			"DATA_COLLECTION_FORM",
+		]
+
+		const createWebsiteTool = {
+			name: "create_website",
+			description: "Create a website",
+			input_schema: {
+				type: "object",
+				properties: {
+					pages: {
+						type: "array",
+						description:
+							"The pages to create. There is already a home page, so you don't need to create that page.",
+						items: {
+							type: "object",
+							properties: {
+								name: {
+									type: "string",
+									description: "The page name",
+								},
+								type: {
+									type: "string",
+									description: `
+										The type of website page.
+										Only use a type from the list below.
+										${pageTypes.join("\n")}
+									`,
+								},
+								description: {
+									type: "string",
+									description: `
+										A detailed description of this page of the website. This should describe
+										which sections and content it will have. As well as the purpose of the page.
+									`,
+								},
+							},
+							required: ["name", "type", "description"],
+						},
+					},
+					footer_links_category: {
+						type: "array",
+						description:
+							"A heading in the footer for links to less-used parts of the site. Create at least 3 categories.",
+						items: {
+							type: "object",
+							properties: {
+								name: {
+									type: "string",
+									description:
+										"The name of the link category.",
+								},
+								links: {
+									type: "array",
+									description:
+										"The footer links for this category",
+									items: {
+										type: "object",
+										properties: {
+											name: {
+												type: "string",
+												description:
+													"The name of the link",
+											},
+											relative_url: {
+												type: "string",
+												description: "The relative url",
+											},
+										},
+										required: ["name", "relative_url"],
+									},
+								},
+							},
+						},
+					},
+					testimonials: {
+						type: "array",
+						description:
+							"The testimonials of satisfied users of this company. Create 4 testimonials.",
+						items: {
+							type: "object",
+							properties: {
+								quote: {
+									type: "string",
+									description: `
+										The positive and funny quote from the person giving the testimonial.
+									`,
+								},
+								name: {
+									type: "string",
+									description: `
+										The name of the person giving the testimonial.
+									`,
+								},
+								title: {
+									type: "string",
+									description: `
+										The job title of the person giving the testimonial.
+									`,
+								},
+							},
+							required: ["quote", "name", "title"],
+						},
+					},
+					logo_prompt: {
+						type: "string",
+						description:
+							"a prompt for a text-to-image ai model for generating a compelling and professional logo for this company.",
+					},
+					tagline: {
+						type: "string",
+						description:
+							"A catchy marketing tagline for this website. It should be between 3 and 10 words.",
+					},
+					tagline_secondary_text: {
+						type: "string",
+						description:
+							"The secondary text for the main tagline. This should be related to the tagline and be clever and engaging. It should be between 6 and 14 words.",
+					},
+					key_features: {
+						type: "array",
+						description:
+							"Key features or communication points that this website should make. Provide 3 key benefits or talking points for the subject of this website.",
+						items: {
+							type: "object",
+							properties: {
+								title: {
+									type: "string",
+									description: `
+										The title or short description of the key feature.
+									`,
+								},
+								description: {
+									type: "string",
+									description: `
+										The longer description or explanation of the key feature.
+									`,
+								},
+							},
+							required: ["title", "description"],
+						},
+					},
+				},
+				required: ["pages", "logo_prompt"],
+			},
+		}
+
+		const result = bot.runIntegrationAction(
+			"uesio/aikit.bedrock",
+			"invokemodel",
+			{
+				model: modelID,
+				messages: [
+					{
+						role: "user",
+						content: prompt,
+					},
+				],
+				system: systemPrompt,
+				tools: [createWebsiteTool],
+				tool_choice: {
+					type: "tool",
+					name: createWebsiteTool.name,
+				},
+			}
+		)
+
+		if (!result.length) {
+			throw new Error("Invalid Result")
+		}
+
+		const input = result[0].input
+
+		tagline = input.tagline
+		tagline_sub = input.tagline_secondary_text
+		testimonials = input.testimonials
+		pages = input.pages
+		footerCategories = input.footer_links_category
+
+		bot.log.info("result", input)
 		/*
 		bot.runGenerators([
 			{
@@ -39,19 +309,27 @@ function starter(bot) {
 			// Create a header view
 			namespace: "uesio/sitekit",
 			name: "view_header",
-			params: {},
+			params: {
+				pages,
+			},
 		},
 		{
 			// Create a footer view
 			namespace: "uesio/sitekit",
 			name: "view_footer",
-			params: {},
+			params: {
+				categories: footerCategories,
+			},
 		},
 		{
 			// Create a home view/route
 			namespace: "uesio/sitekit",
 			name: "view_home",
-			params: {},
+			params: {
+				tagline,
+				tagline_sub,
+				testimonials,
+			},
 		},
 		{
 			// Create public profile/permission set
--- libs/apps/uesio/sitekit/bundle/bots/generator/view_home/bot.js
+++ libs/apps/uesio/sitekit/bundle/bots/generator/view_home/bot.js
@@ -1,8 +1,40 @@
 function view_home(bot) {
 	const namespace = bot.getAppName()
+	const tagline = bot.params.get("tagline")
+	const tagline_sub = bot.params.get("tagline_sub")
+	const testimonials = bot.params.get("testimonials")
+
+	const mainTestimonial = testimonials.pop()
+
+	const mainTestimonialYaml = bot.mergeYamlTemplate(
+		{
+			quote: mainTestimonial.quote,
+			name: mainTestimonial.name,
+			title: mainTestimonial.title,
+		},
+		"templates/maintestimonial.yaml"
+	)
+
+	const testimonialsYaml = testimonials
+		.map((testimonial) =>
+			bot.mergeYamlTemplate(
+				{
+					quote: testimonial.quote,
+					name: testimonial.name,
+					title: testimonial.title,
+				},
+				"templates/testimonial.yaml"
+			)
+		)
+		.join("")
+
 	var definition = bot.mergeYamlTemplate(
 		{
 			namespace,
+			tagline,
+			tagline_sub,
+			mainTestimonialYaml,
+			testimonialsYaml,
 		},
 		"templates/home.yaml"
 	)
