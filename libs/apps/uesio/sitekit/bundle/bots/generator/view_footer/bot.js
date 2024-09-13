8d5b29cfcb61553e5e34bc9b28ca81322da4dd5d
humandad <55447225+humandad@users.noreply.github.com>
2024-09-13 13:47:39 -0400

    Sitekit starter gen improvements

--- libs/apps/uesio/sitekit/bundle/bots/generator/view_footer/bot.js
+++ libs/apps/uesio/sitekit/bundle/bots/generator/view_footer/bot.js
@@ -1,8 +1,35 @@
 function view_footer(bot) {
 	const namespace = bot.getAppName()
+	const categories = bot.params.get("categories")
+
+	const categoriesYaml = categories
+		.map((category) => {
+			const buttonsYaml = category.links
+				.map((link) => {
+					return bot.mergeYamlTemplate(
+						{
+							name: link.name,
+							variant: "uesio/sitekit.footer_link",
+						},
+						"templates/button.yaml"
+					)
+				})
+				.join("")
+
+			return bot.mergeYamlTemplate(
+				{
+					title: category.name,
+					buttonsYaml,
+				},
+				"templates/category.yaml"
+			)
+		})
+		.join("")
+
 	var definition = bot.mergeYamlTemplate(
 		{
 			namespace,
+			categoriesYaml,
 		},
 		"templates/footer.yaml"
 	)
--- libs/apps/uesio/sitekit/bundle/bots/generator/view_header/bot.js
+++ libs/apps/uesio/sitekit/bundle/bots/generator/view_header/bot.js
@@ -1,8 +1,40 @@
 function view_header(bot) {
 	const namespace = bot.getAppName()
+	const pages = bot.params.get("pages")
+
+	// Find the first "CALL_TO_ACTION" page and remove it.
+	const infoPages = pages.filter((page) => page.type !== "CALL_TO_ACTION")
+	const actionPages = pages.filter((page) => page.type === "CALL_TO_ACTION")
+
+	const infoButtonYaml = infoPages
+		.map((page) =>
+			bot.mergeYamlTemplate(
+				{
+					name: page.name,
+					variant: "uesio/sitekit.secondary_dark",
+				},
+				"templates/button.yaml"
+			)
+		)
+		.join("")
+
+	const actionButtonYaml = actionPages
+		.map((page) =>
+			bot.mergeYamlTemplate(
+				{
+					name: page.name,
+					variant: "uesio/sitekit.primary_dark",
+				},
+				"templates/button.yaml"
+			)
+		)
+		.join("")
+
 	var definition = bot.mergeYamlTemplate(
 		{
 			namespace,
+			infoButtonYaml,
+			actionButtonYaml,
 		},
 		"templates/header.yaml"
 	)
