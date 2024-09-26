6ee4fbe86d55d83ad2911b5f1b7e0d56447266f5
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2024-09-26 11:28:10 -0400

    Fix file permissions sitekit (#4220)

--- libs/apps/uesio/io/bundle/componentpacks/main/src/components/deck/deck.tsx
+++ libs/apps/uesio/io/bundle/componentpacks/main/src/components/deck/deck.tsx
@@ -1,11 +1,7 @@
-import { api, component, definition, metadata, styles } from "@uesio/ui"
+import { api, component, definition, metadata } from "@uesio/ui"
 import List, { ListDefinition } from "../list/list"
 
 import { default as IOGrid } from "../../utilities/grid/grid"
-
-const StyleDefaults = Object.freeze({
-	root: [],
-})
 
 type DeckDefinition = {
 	gridVariant?: metadata.MetadataKey
@@ -17,11 +13,11 @@ const Deck: definition.UC<DeckDefinition> = (props) => {
 	// Backwards compatibility: Prior to 1/1/2024, "gridVariant" didn't exist,
 	// and the "uesio.variant" property was expected to be a Grid variant.
 	const { gridVariant = definition[component.STYLE_VARIANT] } = definition
-	const classes = styles.useStyleTokens(StyleDefaults, props)
+
 	return (
 		<IOGrid
 			variant={gridVariant}
-			classes={classes}
+			styleTokens={definition[component.STYLE_TOKENS]}
 			context={context}
 			id={api.component.getComponentIdFromProps(props)}
 		>
