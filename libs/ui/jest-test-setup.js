36a0c5a45ca5d0737e3f580cc41bcba9fdd6b339
humandad <55447225+humandad@users.noreply.github.com>
2025-01-23 11:26:21 -0500

    fix lint

--- libs/ui/jest-test-setup.js
+++ libs/ui/jest-test-setup.js
@@ -1,3 +1,4 @@
 // https://stackoverflow.com/a/57312218
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {}
 Object.defineProperty(window, "scrollTo", { value: noop, writable: true })
