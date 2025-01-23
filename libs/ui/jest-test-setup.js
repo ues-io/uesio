ee8ca2e16869e6651840b905c7c0885158002b42
techfg <barry@techfg.com>
2025-01-23 13:15:37 -0800

    chore: use spy and setupFilesAfterEnv

--- libs/ui/jest-test-setup.js
+++ libs/ui/jest-test-setup.js
@@ -1,4 +1,2 @@
-// https://stackoverflow.com/a/57312218
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop = () => {}
-Object.defineProperty(window, "scrollTo", { value: noop, writable: true })
+// jsdom does not implement scrollTo - See https://stackoverflow.com/a/57312218 & https://github.com/jsdom/jsdom/blob/b1c0072e306e4e9ad83a5e1ddf0e356ba044bd25/lib/jsdom/browser/Window.js#L904
+Object.defineProperty(window, "scrollTo", { value: jest.fn(), writable: true })
--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -7,5 +7,5 @@ module.exports = {
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   coverageDirectory: "../../coverage/libs/ui",
-  setupFiles: ["./jest-test-setup.js"],
+  setupFilesAfterEnv: ["./jest-test-setup.js"],
 }
