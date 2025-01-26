a420c061f087ca523a6314c3efa9c1dcaa169f5f
techfg <barry@techfg.com>
2025-01-25 18:40:52 -0800

    chore: clean-up public types (#4498)

--- libs/ui/build.sh
+++ libs/ui/build.sh
@@ -12,7 +12,6 @@ echo -e "declare module \"@uesio/bots\" {\n$(cat src/public_types/server/index.d
 echo -e "declare module \"@uesio/bots\" {\n$(cat src/public_types/server/index.d.ts)\n}" > ../../dist/ui/types/client/index.d.ts
 # @uesio/ui API
 echo -e "declare module \"@uesio/ui\" {\n$(cat src/public_types/client/index.d.ts)\n}" >> ../../dist/ui/types/client/index.d.ts
-cp src/public_types/client/package.json ../../dist/ui/types/client/package.json
 
 npx tsc --noEmit --project ./tsconfig.lib.json
 
