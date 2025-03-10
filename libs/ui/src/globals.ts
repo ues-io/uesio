3812f5e2ba3f2f41d549ae00ee3d702309260e18
techfg <barry@techfg.com>
2025-03-10 09:56:38 -0700

    chore: add comment

--- libs/ui/src/globals.ts
+++ libs/ui/src/globals.ts
@@ -11,6 +11,11 @@ declare global {
   var ReactJsxRuntime: typeof _ReactJsxRuntime
 }
 
+// Component Packs are loaded dynamically and rely on React being available in global scope.
+// We could load React* in its own script tags using a UMD loader but React 19 does not ship
+// UMD versions so we would have to build them ourselves or pull from a CDN.  Since we only
+// need browser loads we handle it directly here instead of via separate script files.  This
+// increases bundle size for uesio.js itself but not the overall "platform bundle" size.
 globalThis.React = _React
 globalThis.ReactDOM = _ReactDOM
 globalThis.ReactDOMClient = _ReactDOMClient
