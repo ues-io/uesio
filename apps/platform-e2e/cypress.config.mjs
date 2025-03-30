ba994d483c85c04f6b7a28a18ebe0732c75c24f9
techfg <barry@techfg.com>
2025-03-29 23:51:55 -0700

    chore: refactor test scripts

--- apps/platform-e2e/cypress.config.mjs
+++ apps/platform-e2e/cypress.config.mjs
@@ -1,8 +1,17 @@
 import { nxE2EPreset } from "@nx/cypress/plugins/cypress-preset.js"
 import { defineConfig } from "cypress"
 
-const studioBaseUrl =
-  process.env.UESIO_APP_URL || `https://studio.uesio-dev.com:3000`
+// legacy behavior default to https if not set or empty
+const scheme =
+  !process.env.UESIO_USE_HTTPS || process.env.UESIO_USE_HTTPS === "true"
+    ? "https"
+    : "http"
+// legacy behavior default to uesio-dev.com if not set or empty
+const primaryDomain = process.env.UESIO_PRIMARY_DOMAIN || "uesio-dev.com"
+// legacy behavior default to 3000 if not set or empty
+// TODO: PORT variable should change to have UESIO prefix
+const port = process.env.PORT || "3000"
+const studioBaseUrl = `${scheme}://studio.${primaryDomain}:${port}`
 const automationUsername = process.env.UESIO_AUTOMATION_USERNAME || "uesio"
 const automationPassword = process.env.UESIO_AUTOMATION_PASSWORD
 const useMockLogin = !automationPassword
--- scripts/tests/setup-common.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# Common environment variables
-source scripts/tests/setup-env.sh
