3612c6113fd04fbf31290f50039aeb5645fa08b9
humandad <55447225+humandad@users.noreply.github.com>
2025-04-03 13:13:42 -0400

    use const for default model id

--- apps/platform/pkg/bot/systemdialect/systembot_listener_runagent.go
+++ apps/platform/pkg/bot/systemdialect/systembot_listener_runagent.go
@@ -31,7 +31,7 @@ func runAgentListenerBot(params map[string]any, connection wire.Connection, sess
 		return nil, err
 	}
 
-	modelID := param.GetOptionalString(params, "model", "anthropic.claude-3-5-sonnet-20241022-v2:0")
+	modelID := param.GetOptionalString(params, "model", bedrock.CLAUDE_3_5_SONNET_MODEL_ID)
 
 	hiddenInputPrefix := param.GetOptionalString(params, "hiddenInputPrefix", "")
 
