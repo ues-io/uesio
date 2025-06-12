6a4114c93c784d79d6132ee2f9753800a1823d15
humandad <55447225+humandad@users.noreply.github.com>
2025-06-12 14:42:34 -0400

    script cleanup

--- libs/vendor/vendor.sh
+++ libs/vendor/vendor.sh
@@ -1,12 +1,10 @@
 source="../../node_modules/monaco-editor"
 dest="../../dist/vendor/monaco-editor"
+rm -rf $dest
+mkdir -p $dest/src
 if [ "$NODE_ENV" = "development" ]; then
-    rm -rf $dest
-    mkdir -p $dest/src
     cp -R $source/dev/ $dest/src
 else
-    rm -rf $dest
-    mkdir -p $dest/src
     mkdir -p $dest/min-maps
     cp -R $source/min/ $dest/src
     cp -R $source/min-maps/ $dest/min-maps
