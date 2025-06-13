5b70291dfcad38d2587daef9f8dd5a5452d94e2e
humandad <55447225+humandad@users.noreply.github.com>
2025-06-13 13:43:57 -0400

    fix vendor script

--- libs/vendor/vendor.sh
+++ libs/vendor/vendor.sh
@@ -1,11 +1,11 @@
 source="../../node_modules/monaco-editor"
 dest="../../dist/vendor/monaco-editor"
-rm -rf $dest
-mkdir -p $dest/src
+rm -rf "$dest"
+mkdir -p "$dest/src"
 if [ "$NODE_ENV" = "development" ]; then
-    cp -R $source/dev/ $dest/src
+    cp -R "$source/dev/." "$dest/src"
 else
-    mkdir -p $dest/min-maps
-    cp -R $source/min/ $dest/src
-    cp -R $source/min-maps/ $dest/min-maps
+    mkdir -p "$dest/min-maps"
+    cp -R "$source/min/." "$dest/src"
+    cp -R "$source/min-maps/." "$dest/min-maps"
 fi
