8f735ec7366b2d149bd8fb2b0d204ae29d502714
techfg <barry@techfg.com>
2025-07-25 19:11:07 -0700

    fix: cli auth check during login (#5072)
    * fix: cli auth check during login
    
    * chore: add TODO comment
    
    * chore: update html
    
    * chore: update html

--- apps/cli/pkg/command/logout.go
+++ apps/cli/pkg/command/logout.go
@@ -8,15 +8,11 @@ import (
 
 func Logout() error {
 
-	user, err := auth.Logout()
+	err := auth.Logout()
 	if err != nil {
 		return err
 	}
 
-	if user == nil {
-		fmt.Println("no current session found")
-		return nil
-	}
 	fmt.Println("successfully logged out user")
 	return nil
 }
